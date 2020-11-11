d0b3765d469925ec6fb796a6af1ef20c2f04b3a2
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2020-11-11 13:01:04 +0000

    chore(package): update mocha to version 2.4.2

--- js/game.js
+++ js/game.js
@@ -10,4 +10,18 @@ var config = {
     },
     
     physics: {
- 
+        default: 'arcade',
+        
+        arcade: {
+            gravity: {y: 350}
+        }
+    },
+    
+    pixelArt: true
+}
+
+var game = new Phaser.Game(config);
+
+var cursors;
+var platforms, jewels, skulls;
+var 
