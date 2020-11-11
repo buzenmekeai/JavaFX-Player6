7b93d690a02fe9a0a69ca2bfc698ea95b994051f
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2020-11-11 21:18:53 +0000

    More style updates

--- js/game.js
+++ js/game.js
@@ -35,4 +35,14 @@ function preload(){
     this.load.image("platform", "../assets/platform.png");
     this.load.spritesheet("player", "../assets/player.png", {frameWidth: 24, frameHeight: 24});
     this.load.image("jewel", "../assets/jewel.png");
-    this.load.image("skull", "../assets/skul
+    this.load.image("skull", "../assets/skull.png");
+}
+
+function create(){
+    cursors = this.input.keyboard.createCursorKeys();
+    
+    createBackground.call(this);
+    createPlatforms.call(this);
+    createPlayer.call(this);
+    createAnimations.call(this);
+    createJewels.call
