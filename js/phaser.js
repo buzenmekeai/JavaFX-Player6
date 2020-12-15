f40e6fa8f10bbd1345d7db8c69ed8a345aabdaf2
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2020-12-15 05:30:37 +0000

    Updated Gujarati Translations

--- js/phaser.js
+++ js/phaser.js
@@ -54,3 +54,220 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 			});
 /******/ 		}
 /******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1127);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+//  Taken from klasse by mattdesl https://github.com/mattdesl/klasse
+
+function hasGetterOrSetter (def)
+{
+    return (!!def.get && typeof def.get === 'function') || (!!def.set && typeof def.set === 'function');
+}
+
+function getProperty (definition, k, isClassDescriptor)
+{
+    //  This may be a lightweight object, OR it might be a property that was defined previously.
+
+    //  For simple class descriptors we can just assume its NOT previously defined.
+    var def = (isClassDescriptor) ? definition[k] : Object.getOwnPropertyDescriptor(definition, k);
+
+    if (!isClassDescriptor && def.value && typeof def.value === 'object')
+    {
+        def = def.value;
+    }
+
+    //  This might be a regular property, or it may be a getter/setter the user defined in a class.
+    if (def && hasGetterOrSetter(def))
+    {
+        if (typeof def.enumerable === 'undefined')
+        {
+            def.enumerable = true;
+        }
+
+        if (typeof def.configurable === 'undefined')
+        {
+            def.configurable = true;
+        }
+
+        return def;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function hasNonConfigurable (obj, k)
+{
+    var prop = Object.getOwnPropertyDescriptor(obj, k);
+
+    if (!prop)
+    {
+        return false;
+    }
+
+    if (prop.value && typeof prop.value === 'object')
+    {
+        prop = prop.value;
+    }
+
+    if (prop.configurable === false)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+function extend (ctor, definition, isClassDescriptor, extend)
+{
+    for (var k in definition)
+    {
+        if (!definition.hasOwnProperty(k))
+        {
+            continue;
+        }
+
+        var def = getProperty(definition, k, isClassDescriptor);
+
+        if (def !== false)
+        {
+            //  If Extends is used, we will check its prototype to see if the final variable exists.
+
+            var parent = extend || ctor;
+
+            if (hasNonConfigurable(parent.prototype, k))
+            {
+                //  Just skip the final property
+                if (Class.ignoreFinals)
+                {
+                    continue;
+                }
+
+                //  We cannot re-define a property that is configurable=false.
+                //  So we will consider them final and throw an error. This is by
+                //  default so it is clear to the developer what is happening.
+                //  You can set ignoreFinals to true if you need to extend a class
+                //  which has configurable=false; it will simply not re-define final properties.
+                throw new Error('cannot override final property \'' + k + '\', set Class.ignoreFinals = true to skip');
+            }
+
+            Object.defineProperty(ctor.prototype, k, def);
+        }
+        else
+        {
+            ctor.prototype[k] = definition[k];
+        }
+    }
+}
+
+function mixin (myClass, mixins)
+{
+    if (!mixins)
+    {
+        return;
+    }
+
+    if (!Array.isArray(mixins))
+    {
+        mixins = [ mixins ];
+    }
+
+    for (var i = 0; i < mixins.length; i++)
+    {
+        extend(myClass, mixins[i].prototype || mixins[i]);
+    }
+}
+
+/**
+ * Creates a new class with the given descriptor.
+ * The constructor, defined by the name `initialize`,
+ * is an optional function. If unspecified, an anonymous
+ * function will be used which calls the parent class (if
+ * one exists).
+ *
+ * You can also use `Extends` and `Mixins` to provide subclassing
+ * and inheritance.
+ *
+ * @class  Class
+ * @constructor
+ * @param {Object} definition a dictionary of functions for the class
+ * @example
+ *
+ *      var MyClass = new Phaser.Class({
+ *
+ *          initialize: function() {
+ *              this.foo = 2.0;
+ *          },
+ *
+ *          bar: function() {
+ *              return this.foo + 5;
+ *          }
+ *      });
+ */
+function Class (definition)
+{
+    if (!definition)
+    {
+        definition = {};
+    }
+
+    //  The variable name here dictates what we see in Chrome debugger
+    var initialize;
+    var Extends;
+
+    if (definition.initialize)
+    {
+        if (typeof definition.initialize !== 'function')
+        {
+            throw new Error('initialize must be a function');
+        }
+
+        initialize = definition.initialize;
+
+        //  Usually we should avoid 'delete' in V8 at all costs.
+        //  However, its unlikely to make any performance difference
+        //  here since we only call this on class creation (i.e. not object creation).
+        delete definition.initialize;
+    }
+    else if (definition.Extends)
+    {
+        var base = definition.Extends;
+
+        initialize = function ()
+        {
+            base.apply(this, arguments);
+        };
