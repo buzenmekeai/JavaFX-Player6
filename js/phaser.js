b21da725bd33731bd093fc6dc919fbd12fe7823f
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-01-04 15:13:58 +0000

    update sbt version

--- js/phaser.js
+++ js/phaser.js
@@ -2917,3 +2917,223 @@ module.exports = PluginCache;
 
 
 /***/ }),
+/* 16 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var RND = __webpack_require__(404);
+
+var MATH_CONST = {
+
+    /**
+     * The value of PI * 2.
+     * 
+     * @name Phaser.Math.PI2
+     * @type {number}
+     * @since 3.0.0
+     */
+    PI2: Math.PI * 2,
+
+    /**
+     * The value of PI * 0.5.
+     * 
+     * @name Phaser.Math.TAU
+     * @type {number}
+     * @since 3.0.0
+     */
+    TAU: Math.PI * 0.5,
+
+    /**
+     * An epsilon value (1.0e-6)
+     * 
+     * @name Phaser.Math.EPSILON
+     * @type {number}
+     * @since 3.0.0
+     */
+    EPSILON: 1.0e-6,
+
+    /**
+     * For converting degrees to radians (PI / 180)
+     * 
+     * @name Phaser.Math.DEG_TO_RAD
+     * @type {number}
+     * @since 3.0.0
+     */
+    DEG_TO_RAD: Math.PI / 180,
+
+    /**
+     * For converting radians to degrees (180 / PI)
+     * 
+     * @name Phaser.Math.RAD_TO_DEG
+     * @type {number}
+     * @since 3.0.0
+     */
+    RAD_TO_DEG: 180 / Math.PI,
+
+    /**
+     * An instance of the Random Number Generator.
+     * 
+     * @name Phaser.Math.RND
+     * @type {Phaser.Math.RandomDataGenerator}
+     * @since 3.0.0
+     */
+    RND: new RND()
+
+};
+
+module.exports = MATH_CONST;
+
+
+/***/ }),
+/* 17 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetFastValue = __webpack_require__(2);
+
+/**
+ * @typedef {object} GetTilesWithinFilteringOptions
+ *
+ * @property {boolean} [isNotEmpty=false] - If true, only return tiles that don't have -1 for an index.
+ * @property {boolean} [isColliding=false] - If true, only return tiles that collide on at least one side.
+ * @property {boolean} [hasInterestingFace=false] - If true, only return tiles that have at least one interesting face.
+ */
+
+/**
+ * Gets the tiles in the given rectangular area (in tile coordinates) of the layer.
+ *
+ * @function Phaser.Tilemaps.Components.GetTilesWithin
+ * @private
+ * @since 3.0.0
+ *
+ * @param {integer} tileX - The left most tile index (in tile coordinates) to use as the origin of the area.
+ * @param {integer} tileY - The top most tile index (in tile coordinates) to use as the origin of the area.
+ * @param {integer} width - How many tiles wide from the `tileX` index the area will be.
+ * @param {integer} height - How many tiles tall from the `tileY` index the area will be.
+ * @param {object} GetTilesWithinFilteringOptions - Optional filters to apply when getting the tiles.
+ * @param {Phaser.Tilemaps.LayerData} layer - The Tilemap Layer to act upon.
+ * 
+ * @return {Phaser.Tilemaps.Tile[]} Array of Tile objects.
+ */
+var GetTilesWithin = function (tileX, tileY, width, height, filteringOptions, layer)
+{
+    if (tileX === undefined) { tileX = 0; }
+    if (tileY === undefined) { tileY = 0; }
+    if (width === undefined) { width = layer.width; }
+    if (height === undefined) { height = layer.height; }
+
+    var isNotEmpty = GetFastValue(filteringOptions, 'isNotEmpty', false);
+    var isColliding = GetFastValue(filteringOptions, 'isColliding', false);
+    var hasInterestingFace = GetFastValue(filteringOptions, 'hasInterestingFace', false);
+
+    // Clip x, y to top left of map, while shrinking width/height to match.
+    if (tileX < 0)
+    {
+        width += tileX;
+        tileX = 0;
+    }
+    if (tileY < 0)
+    {
+        height += tileY;
+        tileY = 0;
+    }
+
+    // Clip width and height to bottom right of map.
+    if (tileX + width > layer.width)
+    {
+        width = Math.max(layer.width - tileX, 0);
+    }
+    if (tileY + height > layer.height)
+    {
+        height = Math.max(layer.height - tileY, 0);
+    }
+
+    var results = [];
+
+    for (var ty = tileY; ty < tileY + height; ty++)
+    {
+        for (var tx = tileX; tx < tileX + width; tx++)
+        {
+            var tile = layer.data[ty][tx];
+            if (tile !== null)
+            {
+                if (isNotEmpty && tile.index === -1) { continue; }
+                if (isColliding && !tile.collides) { continue; }
+                if (hasInterestingFace && !tile.hasInterestingFace) { continue; }
+                results.push(tile);
+            }
+        }
+    }
+
+    return results;
+};
+
+module.exports = GetTilesWithin;
+
+
+/***/ }),
+/* 18 */
+/***/ (function(module, exports) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var FILE_CONST = {
+
+    /**
+     * The Loader is idle.
+     * 
+     * @name Phaser.Loader.LOADER_IDLE
+     * @type {integer}
+     * @since 3.0.0
+     */
+    LOADER_IDLE: 0,
+
+    /**
+     * The Loader is actively loading.
+     * 
+     * @name Phaser.Loader.LOADER_LOADING
+     * @type {integer}
+     * @since 3.0.0
+     */
+    LOADER_LOADING: 1,
+
+    /**
+     * The Loader is processing files is has loaded.
+     * 
+     * @name Phaser.Loader.LOADER_PROCESSING
+     * @type {integer}
+     * @since 3.0.0
+     */
+    LOADER_PROCESSING: 2,
+
+    /**
+     * The Loader has completed loading and processing.
+     * 
+     * @name Phaser.Loader.LOADER_COMPLETE
+     * @type {integer}
+     * @since 3.0.0
+     */
+    LOADER_COMPLETE: 3,
+
+    /**
+     * The Loader is shutting down.
+     * 
+     * @name Phaser.Loader.LOADER_SHUTDOWN
+     * @type {integer}
+     * @since 3.0.0
+     */
+    LOADER_SHUTDOWN: 4,
