9696fbb2e3344b95427b4a5da937082614caeea1
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-01-07 22:50:09 +0000

    Update system.js

--- js/phaser.js
+++ js/phaser.js
@@ -5131,3 +5131,205 @@ var Line = __webpack_require__(54);
  *
  * @class Shape
  * @extends Phaser.GameObjects.GameObject
+ * @memberof Phaser.GameObjects
+ * @constructor
+ * @since 3.13.0
+ *
+ * @extends Phaser.GameObjects.Components.Alpha
+ * @extends Phaser.GameObjects.Components.BlendMode
+ * @extends Phaser.GameObjects.Components.ComputedSize
+ * @extends Phaser.GameObjects.Components.Depth
+ * @extends Phaser.GameObjects.Components.GetBounds
+ * @extends Phaser.GameObjects.Components.Mask
+ * @extends Phaser.GameObjects.Components.Origin
+ * @extends Phaser.GameObjects.Components.Pipeline
+ * @extends Phaser.GameObjects.Components.ScaleMode
+ * @extends Phaser.GameObjects.Components.ScrollFactor
+ * @extends Phaser.GameObjects.Components.Transform
+ * @extends Phaser.GameObjects.Components.Visible
+ *
+ * @param {Phaser.Scene} scene - The Scene to which this Game Object belongs. A Game Object can only belong to one Scene at a time.
+ * @param {string} [type] - The internal type of the Shape.
+ * @param {any} [data] - The data of the source shape geometry, if any.
+ */
+var Shape = new Class({
+
+    Extends: GameObject,
+
+    Mixins: [
+        Components.Alpha,
+        Components.BlendMode,
+        Components.ComputedSize,
+        Components.Depth,
+        Components.GetBounds,
+        Components.Mask,
+        Components.Origin,
+        Components.Pipeline,
+        Components.ScaleMode,
+        Components.ScrollFactor,
+        Components.Transform,
+        Components.Visible
+    ],
+
+    initialize:
+
+    function Shape (scene, type, data)
+    {
+        if (type === undefined) { type = 'Shape'; }
+
+        GameObject.call(this, scene, type);
+
+        /**
+         * The source Shape data. Typically a geometry object.
+         * You should not manipulate this directly.
+         *
+         * @name Phaser.GameObjects.Shape#data
+         * @type {any}
+         * @readonly
+         * @since 3.13.0
+         */
+        this.geom = data;
+
+        /**
+         * Holds the polygon path data for filled rendering.
+         *
+         * @name Phaser.GameObjects.Shape#pathData
+         * @type {number[]}
+         * @readonly
+         * @since 3.13.0
+         */
+        this.pathData = [];
+
+        /**
+         * Holds the earcut polygon path index data for filled rendering.
+         *
+         * @name Phaser.GameObjects.Shape#pathIndexes
+         * @type {integer[]}
+         * @readonly
+         * @since 3.13.0
+         */
+        this.pathIndexes = [];
+
+        /**
+         * The fill color used by this Shape.
+         *
+         * @name Phaser.GameObjects.Shape#fillColor
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.fillColor = 0xffffff;
+
+        /**
+         * The fill alpha value used by this Shape.
+         *
+         * @name Phaser.GameObjects.Shape#fillAlpha
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.fillAlpha = 1;
+
+        /**
+         * The stroke color used by this Shape.
+         *
+         * @name Phaser.GameObjects.Shape#strokeColor
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.strokeColor = 0xffffff;
+
+        /**
+         * The stroke alpha value used by this Shape.
+         *
+         * @name Phaser.GameObjects.Shape#strokeAlpha
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.strokeAlpha = 1;
+
+        /**
+         * The stroke line width used by this Shape.
+         *
+         * @name Phaser.GameObjects.Shape#lineWidth
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.lineWidth = 1;
+
+        /**
+         * Controls if this Shape is filled or not.
+         * Note that some Shapes do not support being filled (such as Line shapes)
+         *
+         * @name Phaser.GameObjects.Shape#isFilled
+         * @type {boolean}
+         * @since 3.13.0
+         */
+        this.isFilled = false;
+
+        /**
+         * Controls if this Shape is stroked or not.
+         * Note that some Shapes do not support being stroked (such as Iso Box shapes)
+         *
+         * @name Phaser.GameObjects.Shape#isStroked
+         * @type {boolean}
+         * @since 3.13.0
+         */
+        this.isStroked = false;
+
+        /**
+         * Controls if this Shape path is closed during rendering when stroked.
+         * Note that some Shapes are always closed when stroked (such as Ellipse shapes)
+         *
+         * @name Phaser.GameObjects.Shape#closePath
+         * @type {boolean}
+         * @since 3.13.0
+         */
+        this.closePath = true;
+
+        /**
+         * Private internal value.
+         * A Line used when parsing internal path data to avoid constant object re-creation.
+         *
+         * @name Phaser.GameObjects.Curve#_tempLine
+         * @type {Phaser.Geom.Line}
+         * @private
+         * @since 3.13.0
+         */
+        this._tempLine = new Line();
+
+        this.initPipeline();
+    },
+
+    /**
+     * Sets the fill color and alpha for this Shape.
+     * 
+     * If you wish for the Shape to not be filled then call this method with no arguments, or just set `isFilled` to `false`.
+     * 
+     * Note that some Shapes do not support fill colors, such as the Line shape.
+     * 
+     * This call can be chained.
+     *
+     * @method Phaser.GameObjects.Shape#setFillStyle
+     * @since 3.13.0
+     * 
+     * @param {number} [color] - The color used to fill this shape. If not provided the Shape will not be filled.
+     * @param {number} [alpha=1] - The alpha value used when filling this shape, if a fill color is given.
+     *
+     * @return {this} This Game Object instance.
+     */
+    setFillStyle: function (color, alpha)
+    {
+        if (alpha === undefined) { alpha = 1; }
+
+        if (color === undefined)
+        {
+            this.isFilled = false;
+        }
+        else
+        {
+            this.fillColor = color;
+            this.fillAlpha = alpha;
+            this.isFilled = true;
+        }
+
+        return this;
+    },
