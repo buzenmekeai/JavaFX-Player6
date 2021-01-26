b46e34c3ae7c031fb23d163027c16630cc773346
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-01-26 20:50:36 +0000

    Added missing comma

--- js/phaser.js
+++ js/phaser.js
@@ -15548,3 +15548,218 @@ var LayerData = new Class({
          *
          * @name Phaser.Tilemaps.LayerData#indexes
          * @type {array}
+         * @since 3.0.0
+         */
+        this.indexes = GetFastValue(config, 'indexes', []);
+
+        /**
+         * [description]
+         *
+         * @name Phaser.Tilemaps.LayerData#collideIndexes
+         * @type {array}
+         * @since 3.0.0
+         */
+        this.collideIndexes = GetFastValue(config, 'collideIndexes', []);
+
+        /**
+         * [description]
+         *
+         * @name Phaser.Tilemaps.LayerData#callbacks
+         * @type {array}
+         * @since 3.0.0
+         */
+        this.callbacks = GetFastValue(config, 'callbacks', []);
+
+        /**
+         * [description]
+         *
+         * @name Phaser.Tilemaps.LayerData#bodies
+         * @type {array}
+         * @since 3.0.0
+         */
+        this.bodies = GetFastValue(config, 'bodies', []);
+
+        /**
+         * [description]
+         *
+         * @name Phaser.Tilemaps.LayerData#data
+         * @type {array}
+         * @since 3.0.0
+         */
+        this.data = GetFastValue(config, 'data', []);
+
+        /**
+         * [description]
+         *
+         * @name Phaser.Tilemaps.LayerData#tilemapLayer
+         * @type {(Phaser.Tilemaps.DynamicTilemapLayer|Phaser.Tilemaps.StaticTilemapLayer)}
+         * @since 3.0.0
+         */
+        this.tilemapLayer = GetFastValue(config, 'tilemapLayer', null);
+    }
+
+});
+
+module.exports = LayerData;
+
+
+/***/ }),
+/* 79 */
+/***/ (function(module, exports) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+/**
+ * Checks if the given tile coordinates are within the bounds of the layer.
+ *
+ * @function Phaser.Tilemaps.Components.IsInLayerBounds
+ * @private
+ * @since 3.0.0
+ *
+ * @param {integer} tileX - The x coordinate, in tiles, not pixels.
+ * @param {integer} tileY - The y coordinate, in tiles, not pixels.
+ * @param {Phaser.Tilemaps.LayerData} layer - The Tilemap Layer to act upon.
+ * 
+ * @return {boolean} `true` if the tile coordinates are within the bounds of the layer, otherwise `false`.
+ */
+var IsInLayerBounds = function (tileX, tileY, layer)
+{
+    return (tileX >= 0 && tileX < layer.width && tileY >= 0 && tileY < layer.height);
+};
+
+module.exports = IsInLayerBounds;
+
+
+/***/ }),
+/* 80 */
+/***/ (function(module, exports) {
+
+/**
+* The `Matter.Bounds` module contains methods for creating and manipulating axis-aligned bounding boxes (AABB).
+*
+* @class Bounds
+*/
+
+var Bounds = {};
+
+module.exports = Bounds;
+
+(function() {
+
+    /**
+     * Creates a new axis-aligned bounding box (AABB) for the given vertices.
+     * @method create
+     * @param {vertices} vertices
+     * @return {bounds} A new bounds object
+     */
+    Bounds.create = function(vertices) {
+        var bounds = { 
+            min: { x: 0, y: 0 }, 
+            max: { x: 0, y: 0 }
+        };
+
+        if (vertices)
+            Bounds.update(bounds, vertices);
+        
+        return bounds;
+    };
+
+    /**
+     * Updates bounds using the given vertices and extends the bounds given a velocity.
+     * @method update
+     * @param {bounds} bounds
+     * @param {vertices} vertices
+     * @param {vector} velocity
+     */
+    Bounds.update = function(bounds, vertices, velocity) {
+        bounds.min.x = Infinity;
+        bounds.max.x = -Infinity;
+        bounds.min.y = Infinity;
+        bounds.max.y = -Infinity;
+
+        for (var i = 0; i < vertices.length; i++) {
+            var vertex = vertices[i];
+            if (vertex.x > bounds.max.x) bounds.max.x = vertex.x;
+            if (vertex.x < bounds.min.x) bounds.min.x = vertex.x;
+            if (vertex.y > bounds.max.y) bounds.max.y = vertex.y;
+            if (vertex.y < bounds.min.y) bounds.min.y = vertex.y;
+        }
+        
+        if (velocity) {
+            if (velocity.x > 0) {
+                bounds.max.x += velocity.x;
+            } else {
+                bounds.min.x += velocity.x;
+            }
+            
+            if (velocity.y > 0) {
+                bounds.max.y += velocity.y;
+            } else {
+                bounds.min.y += velocity.y;
+            }
+        }
+    };
+
+    /**
+     * Returns true if the bounds contains the given point.
+     * @method contains
+     * @param {bounds} bounds
+     * @param {vector} point
+     * @return {boolean} True if the bounds contain the point, otherwise false
+     */
+    Bounds.contains = function(bounds, point) {
+        return point.x >= bounds.min.x && point.x <= bounds.max.x 
+               && point.y >= bounds.min.y && point.y <= bounds.max.y;
+    };
+
+    /**
+     * Returns true if the two bounds intersect.
+     * @method overlaps
+     * @param {bounds} boundsA
+     * @param {bounds} boundsB
+     * @return {boolean} True if the bounds overlap, otherwise false
+     */
+    Bounds.overlaps = function(boundsA, boundsB) {
+        return (boundsA.min.x <= boundsB.max.x && boundsA.max.x >= boundsB.min.x
+                && boundsA.max.y >= boundsB.min.y && boundsA.min.y <= boundsB.max.y);
+    };
+
+    /**
+     * Translates the bounds by the given vector.
+     * @method translate
+     * @param {bounds} bounds
+     * @param {vector} vector
+     */
+    Bounds.translate = function(bounds, vector) {
+        bounds.min.x += vector.x;
+        bounds.max.x += vector.x;
+        bounds.min.y += vector.y;
+        bounds.max.y += vector.y;
+    };
+
+    /**
+     * Shifts the bounds to the given position.
+     * @method shift
+     * @param {bounds} bounds
+     * @param {vector} position
+     */
+    Bounds.shift = function(bounds, position) {
+        var deltaX = bounds.max.x - bounds.min.x,
+            deltaY = bounds.max.y - bounds.min.y;
+            
+        bounds.min.x = position.x;
+        bounds.max.x = position.x + deltaX;
+        bounds.min.y = position.y;
+        bounds.max.y = position.y + deltaY;
+    };
+    
+})();
+
+
+/***/ }),
+/* 81 */
+/***/ (function(module, exports) {
