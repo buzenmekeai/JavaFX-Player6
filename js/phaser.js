12964b09b4b727b48021cd4790adc85107d3ae7c
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-02-01 08:54:44 +0000

    Update code.md

--- js/phaser.js
+++ js/phaser.js
@@ -19266,3 +19266,216 @@ var Tileset = new Class({
         /**
          * The number of tile columns in the tileset.
          *
+         * @name Phaser.Tilemaps.Tileset#columns
+         * @type {integer}
+         * @readonly
+         * @since 3.0.0
+         */
+        this.columns = 0;
+
+        /**
+         * The total number of tiles in the tileset.
+         *
+         * @name Phaser.Tilemaps.Tileset#total
+         * @type {integer}
+         * @readonly
+         * @since 3.0.0
+         */
+        this.total = 0;
+
+        /**
+         * The look-up table to specific tile image texture coordinates (UV in pixels). Each element
+         * contains the coordinates for a tile in an object of the form {x, y}.
+         *
+         * @name Phaser.Tilemaps.Tileset#texCoordinates
+         * @type {object[]}
+         * @readonly
+         * @since 3.0.0
+        */
+        this.texCoordinates = [];
+    },
+
+    /**
+     * Get a tiles properties that are stored in the Tileset. Returns null if tile index is not
+     * contained in this Tileset. This is typically defined in Tiled under the Tileset editor.
+     *
+     * @method Phaser.Tilemaps.Tileset#getTileProperties
+     * @since 3.0.0
+     *
+     * @param {integer} tileIndex - The unique id of the tile across all tilesets in the map.
+     *
+     * @return {?(object|undefined)}
+     */
+    getTileProperties: function (tileIndex)
+    {
+        if (!this.containsTileIndex(tileIndex)) { return null; }
+
+        return this.tileProperties[tileIndex - this.firstgid];
+    },
+
+    /**
+     * Get a tile's data that is stored in the Tileset. Returns null if tile index is not contained
+     * in this Tileset. This is typically defined in Tiled and will contain both Tileset collision
+     * info and terrain mapping.
+     *
+     * @method Phaser.Tilemaps.Tileset#getTileData
+     * @since 3.0.0
+     *
+     * @param {integer} tileIndex - The unique id of the tile across all tilesets in the map.
+     *
+     * @return {?object|undefined}
+     */
+    getTileData: function (tileIndex)
+    {
+        if (!this.containsTileIndex(tileIndex)) { return null; }
+
+        return this.tileData[tileIndex - this.firstgid];
+    },
+
+    /**
+     * Get a tile's collision group that is stored in the Tileset. Returns null if tile index is not
+     * contained in this Tileset. This is typically defined within Tiled's tileset collision editor.
+     *
+     * @method Phaser.Tilemaps.Tileset#getTileCollisionGroup
+     * @since 3.0.0
+     *
+     * @param {integer} tileIndex - The unique id of the tile across all tilesets in the map.
+     *
+     * @return {?object}
+     */
+    getTileCollisionGroup: function (tileIndex)
+    {
+        var data = this.getTileData(tileIndex);
+
+        return (data && data.objectgroup) ? data.objectgroup : null;
+    },
+
+    /**
+     * Returns true if and only if this Tileset contains the given tile index.
+     *
+     * @method Phaser.Tilemaps.Tileset#containsTileIndex
+     * @since 3.0.0
+     *
+     * @param {integer} tileIndex - The unique id of the tile across all tilesets in the map.
+     *
+     * @return {boolean}
+     */
+    containsTileIndex: function (tileIndex)
+    {
+        return (
+            tileIndex >= this.firstgid &&
+            tileIndex < (this.firstgid + this.total)
+        );
+    },
+
+    /**
+     * Returns the texture coordinates (UV in pixels) in the Tileset image for the given tile index.
+     * Returns null if tile index is not contained in this Tileset.
+     *
+     * @method Phaser.Tilemaps.Tileset#getTileTextureCoordinates
+     * @since 3.0.0
+     *
+     * @param {integer} tileIndex - The unique id of the tile across all tilesets in the map.
+     *
+     * @return {?object} Object in the form { x, y } representing the top-left UV coordinate
+     * within the Tileset image.
+     */
+    getTileTextureCoordinates: function (tileIndex)
+    {
+        if (!this.containsTileIndex(tileIndex)) { return null; }
+
+        return this.texCoordinates[tileIndex - this.firstgid];
+    },
+
+    /**
+     * Sets the image associated with this Tileset and updates the tile data (rows, columns, etc.).
+     *
+     * @method Phaser.Tilemaps.Tileset#setImage
+     * @since 3.0.0
+     *
+     * @param {Phaser.Textures.Texture} texture - The image that contains the tiles.
+     *
+     * @return {Phaser.Tilemaps.Tileset} This Tileset object.
+     */
+    setImage: function (texture)
+    {
+        this.image = texture;
+
+        this.glTexture = texture.get().source.glTexture;
+
+        this.updateTileData(this.image.source[0].width, this.image.source[0].height);
+
+        return this;
+    },
+
+    /**
+     * Sets the tile width & height and updates the tile data (rows, columns, etc.).
+     *
+     * @method Phaser.Tilemaps.Tileset#setTileSize
+     * @since 3.0.0
+     *
+     * @param {integer} [tileWidth] - The width of a tile in pixels.
+     * @param {integer} [tileHeight] - The height of a tile in pixels.
+     *
+     * @return {Phaser.Tilemaps.Tileset} This Tileset object.
+     */
+    setTileSize: function (tileWidth, tileHeight)
+    {
+        if (tileWidth !== undefined) { this.tileWidth = tileWidth; }
+        if (tileHeight !== undefined) { this.tileHeight = tileHeight; }
+
+        if (this.image)
+        {
+            this.updateTileData(this.image.source[0].width, this.image.source[0].height);
+        }
+
+        return this;
+    },
+
+    /**
+     * Sets the tile margin & spacing and updates the tile data (rows, columns, etc.).
+     *
+     * @method Phaser.Tilemaps.Tileset#setSpacing
+     * @since 3.0.0
+     *
+     * @param {integer} [margin] - The margin around the tiles in the sheet (in pixels).
+     * @param {integer} [spacing] - The spacing between the tiles in the sheet (in pixels).
+     *
+     * @return {Phaser.Tilemaps.Tileset} This Tileset object.
+     */
+    setSpacing: function (margin, spacing)
+    {
+        if (margin !== undefined) { this.tileMargin = margin; }
+        if (spacing !== undefined) { this.tileSpacing = spacing; }
+
+        if (this.image)
+        {
+            this.updateTileData(this.image.source[0].width, this.image.source[0].height);
+        }
+
+        return this;
+    },
+
+    /**
+     * Updates tile texture coordinates and tileset data.
+     *
+     * @method Phaser.Tilemaps.Tileset#updateTileData
+     * @since 3.0.0
+     *
+     * @param {integer} imageWidth - The (expected) width of the image to slice.
+     * @param {integer} imageHeight - The (expected) height of the image to slice.
+     *
+     * @return {Phaser.Tilemaps.Tileset} This Tileset object.
+     */
+    updateTileData: function (imageWidth, imageHeight)
+    {
+        var rowCount = (imageHeight - this.tileMargin * 2 + this.tileSpacing) / (this.tileHeight + this.tileSpacing);
+        var colCount = (imageWidth - this.tileMargin * 2 + this.tileSpacing) / (this.tileWidth + this.tileSpacing);
+
+        if (rowCount % 1 !== 0 || colCount % 1 !== 0)
+        {
+            console.warn('Image tile area not tile size multiple in: ' + this.name);
+        }
+
+        // In Tiled a tileset image that is not an even multiple of the tile dimensions is truncated
+        // - hence the floor when calculating the rows/columns.
