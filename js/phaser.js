5b4136406f05b03709ca785da2ea0474fb5b753e
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-05-06 13:48:43 +0000

    nfsd4: fix setclientid encode size

--- js/phaser.js
+++ js/phaser.js
@@ -36347,3 +36347,237 @@ var ParticleEmitterManager = new Class({
 
     /**
      * A NOOP method so you can pass an EmitterManager to a Container.
+     * Calling this method will do nothing. It is intentionally empty.
+     *
+     * @method Phaser.GameObjects.Particles.ParticleEmitterManager#setBlendMode
+     * @private
+     * @since 3.15.0
+     */
+    setBlendMode: function ()
+    {
+    }
+
+});
+
+module.exports = ParticleEmitterManager;
+
+
+/***/ }),
+/* 156 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Point = __webpack_require__(6);
+
+/**
+ * Returns a Point object containing the coordinates of a point on the circumference of the Ellipse based on the given angle.
+ *
+ * @function Phaser.Geom.Ellipse.CircumferencePoint
+ * @since 3.0.0
+ *
+ * @generic {Phaser.Geom.Point} O - [out,$return]
+ *
+ * @param {Phaser.Geom.Ellipse} ellipse - The Ellipse to get the circumference point on.
+ * @param {number} angle - The angle from the center of the Ellipse to the circumference to return the point from. Given in radians.
+ * @param {(Phaser.Geom.Point|object)} [out] - A Point, or point-like object, to store the results in. If not given a Point will be created.
+ *
+ * @return {(Phaser.Geom.Point|object)} A Point object where the `x` and `y` properties are the point on the circumference.
+ */
+var CircumferencePoint = function (ellipse, angle, out)
+{
+    if (out === undefined) { out = new Point(); }
+
+    var halfWidth = ellipse.width / 2;
+    var halfHeight = ellipse.height / 2;
+
+    out.x = ellipse.x + halfWidth * Math.cos(angle);
+    out.y = ellipse.y + halfHeight * Math.sin(angle);
+
+    return out;
+};
+
+module.exports = CircumferencePoint;
+
+
+/***/ }),
+/* 157 */
+/***/ (function(module, exports) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+/**
+ * @namespace Phaser.GameObjects.Graphics.Commands
+ */
+
+module.exports = {
+
+    ARC: 0,
+    BEGIN_PATH: 1,
+    CLOSE_PATH: 2,
+    FILL_RECT: 3,
+    LINE_TO: 4,
+    MOVE_TO: 5,
+    LINE_STYLE: 6,
+    FILL_STYLE: 7,
+    FILL_PATH: 8,
+    STROKE_PATH: 9,
+    FILL_TRIANGLE: 10,
+    STROKE_TRIANGLE: 11,
+    LINE_FX_TO: 12,
+    MOVE_FX_TO: 13,
+    SAVE: 14,
+    RESTORE: 15,
+    TRANSLATE: 16,
+    SCALE: 17,
+    ROTATE: 18,
+    SET_TEXTURE: 19,
+    CLEAR_TEXTURE: 20,
+    GRADIENT_FILL_STYLE: 21,
+    GRADIENT_LINE_STYLE: 22
+
+};
+
+
+/***/ }),
+/* 158 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var BaseCamera = __webpack_require__(121);
+var Class = __webpack_require__(0);
+var Commands = __webpack_require__(157);
+var ComponentsAlpha = __webpack_require__(401);
+var ComponentsBlendMode = __webpack_require__(400);
+var ComponentsDepth = __webpack_require__(399);
+var ComponentsMask = __webpack_require__(395);
+var ComponentsPipeline = __webpack_require__(186);
+var ComponentsTransform = __webpack_require__(390);
+var ComponentsVisible = __webpack_require__(389);
+var ComponentsScrollFactor = __webpack_require__(392);
+
+var Ellipse = __webpack_require__(90);
+var GameObject = __webpack_require__(19);
+var GetFastValue = __webpack_require__(2);
+var GetValue = __webpack_require__(4);
+var MATH_CONST = __webpack_require__(16);
+var Render = __webpack_require__(831);
+
+/**
+ * Graphics line style (or stroke style) settings.
+ *
+ * @typedef {object} GraphicsLineStyle
+ *
+ * @property {number} [width] - The stroke width.
+ * @property {number} [color] - The stroke color.
+ * @property {number} [alpha] - The stroke alpha.
+ */
+
+/**
+ * Graphics fill style settings.
+ *
+ * @typedef {object} GraphicsFillStyle
+ *
+ * @property {number} [color] - The fill color.
+ * @property {number} [alpha] - The fill alpha.
+ */
+
+/**
+ * Graphics style settings.
+ *
+ * @typedef {object} GraphicsStyles
+ *
+ * @property {GraphicsLineStyle} [lineStyle] - The style applied to shape outlines.
+ * @property {GraphicsFillStyle} [fillStyle] - The style applied to shape areas.
+ */
+
+/**
+ * Options for the Graphics game Object.
+ *
+ * @typedef {object} GraphicsOptions
+ * @extends GraphicsStyles
+ *
+ * @property {number} [x] - The x coordinate of the Graphics.
+ * @property {number} [y] - The y coordinate of the Graphics.
+ */
+
+/**
+ * @classdesc
+ * A Graphics object is a way to draw primitive shapes to your game. Primitives include forms of geometry, such as
+ * Rectangles, Circles, and Polygons. They also include lines, arcs and curves. When you initially create a Graphics
+ * object it will be empty.
+ *
+ * To draw to it you must first specify a line style or fill style (or both), draw shapes using paths, and finally
+ * fill or stroke them. For example:
+ *
+ * ```javascript
+ * graphics.lineStyle(5, 0xFF00FF, 1.0);
+ * graphics.beginPath();
+ * graphics.moveTo(100, 100);
+ * graphics.lineTo(200, 200);
+ * graphics.closePath();
+ * graphics.strokePath();
+ * ```
+ *
+ * There are also many helpful methods that draw and fill/stroke common shapes for you.
+ *
+ * ```javascript
+ * graphics.lineStyle(5, 0xFF00FF, 1.0);
+ * graphics.fillStyle(0xFFFFFF, 1.0);
+ * graphics.fillRect(50, 50, 400, 200);
+ * graphics.strokeRect(50, 50, 400, 200);
+ * ```
+ *
+ * When a Graphics object is rendered it will render differently based on if the game is running under Canvas or WebGL.
+ * Under Canvas it will use the HTML Canvas context drawing operations to draw the path.
+ * Under WebGL the graphics data is decomposed into polygons. Both of these are expensive processes, especially with
+ * complex shapes.
+ *
+ * If your Graphics object doesn't change much (or at all) once you've drawn your shape to it, then you will help
+ * performance by calling {@link Phaser.GameObjects.Graphics#generateTexture}. This will 'bake' the Graphics object into
+ * a Texture, and return it. You can then use this Texture for Sprites or other display objects. If your Graphics object
+ * updates frequently then you should avoid doing this, as it will constantly generate new textures, which will consume
+ * memory.
+ *
+ * As you can tell, Graphics objects are a bit of a trade-off. While they are extremely useful, you need to be careful
+ * in their complexity and quantity of them in your game.
+ *
+ * @class Graphics
+ * @extends Phaser.GameObjects.GameObject
+ * @memberof Phaser.GameObjects
+ * @constructor
+ * @since 3.0.0
+ *
+ * @extends Phaser.GameObjects.Components.Alpha
+ * @extends Phaser.GameObjects.Components.BlendMode
+ * @extends Phaser.GameObjects.Components.Depth
+ * @extends Phaser.GameObjects.Components.Mask
+ * @extends Phaser.GameObjects.Components.Pipeline
+ * @extends Phaser.GameObjects.Components.Transform
+ * @extends Phaser.GameObjects.Components.Visible
+ * @extends Phaser.GameObjects.Components.ScrollFactor
+ *
+ * @param {Phaser.Scene} scene - The Scene to which this Graphics object belongs.
+ * @param {GraphicsOptions} [options] - Options that set the position and default style of this Graphics object.
+ */
+var Graphics = new Class({
+
+    Extends: GameObject,
+
+    Mixins: [
+        ComponentsAlpha,
+        ComponentsBlendMode,
+        ComponentsDepth,
