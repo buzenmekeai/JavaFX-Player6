1634cfc5f40f17249ad7469eec349a0cb3d22fa1
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-05-21 11:03:03 +0000

    mm: thp: fix SMP race condition between THP page fault and MADV_DONTNEED

--- js/phaser.js
+++ js/phaser.js
@@ -38118,3 +38118,202 @@ var DynamicBitmapText = new Class({
          * A callback that alters how each character of the Bitmap Text is rendered.
          *
          * @name Phaser.GameObjects.DynamicBitmapText#displayCallback
+         * @type {DisplayCallback}
+         * @since 3.0.0
+         */
+        this.displayCallback;
+
+        /**
+         * The data object that is populated during rendering, then passed to the displayCallback.
+         * You should modify this object then return it back from the callback. It's updated values
+         * will be used to render the specific glyph.
+         * 
+         * Please note that if you need a reference to this object locally in your game code then you
+         * should shallow copy it, as it's updated and re-used for every glyph in the text.
+         *
+         * @name Phaser.GameObjects.DynamicBitmapText#callbackData
+         * @type {DisplayCallbackConfig}
+         * @since 3.11.0
+         */
+        this.callbackData = {
+            color: 0,
+            tint: {
+                topLeft: 0,
+                topRight: 0,
+                bottomLeft: 0,
+                bottomRight: 0
+            },
+            index: 0,
+            charCode: 0,
+            x: 0,
+            y: 0,
+            scale: 0,
+            rotation: 0,
+            data: 0
+        };
+    },
+
+    /**
+     * Set the crop size of this Bitmap Text.
+     *
+     * @method Phaser.GameObjects.DynamicBitmapText#setSize
+     * @since 3.0.0
+     *
+     * @param {number} width - The width of the crop.
+     * @param {number} height - The height of the crop.
+     *
+     * @return {Phaser.GameObjects.DynamicBitmapText} This Game Object.
+     */
+    setSize: function (width, height)
+    {
+        this.cropWidth = width;
+        this.cropHeight = height;
+
+        return this;
+    },
+
+    /**
+     * Set a callback that alters how each character of the Bitmap Text is rendered.
+     *
+     * The callback receives a {@link DisplayCallbackConfig} object that contains information about the character that's
+     * about to be rendered.
+     *
+     * It should return an object with `x`, `y`, `scale` and `rotation` properties that will be used instead of the
+     * usual values when rendering.
+     *
+     * @method Phaser.GameObjects.DynamicBitmapText#setDisplayCallback
+     * @since 3.0.0
+     *
+     * @param {DisplayCallback} callback - The display callback to set.
+     *
+     * @return {Phaser.GameObjects.DynamicBitmapText} This Game Object.
+     */
+    setDisplayCallback: function (callback)
+    {
+        this.displayCallback = callback;
+
+        return this;
+    },
+
+    /**
+     * Set the horizontal scroll position of this Bitmap Text.
+     *
+     * @method Phaser.GameObjects.DynamicBitmapText#setScrollX
+     * @since 3.0.0
+     *
+     * @param {number} value - The horizontal scroll position to set.
+     *
+     * @return {Phaser.GameObjects.DynamicBitmapText} This Game Object.
+     */
+    setScrollX: function (value)
+    {
+        this.scrollX = value;
+
+        return this;
+    },
+
+    /**
+     * Set the vertical scroll position of this Bitmap Text.
+     *
+     * @method Phaser.GameObjects.DynamicBitmapText#setScrollY
+     * @since 3.0.0
+     *
+     * @param {number} value - The vertical scroll position to set.
+     *
+     * @return {Phaser.GameObjects.DynamicBitmapText} This Game Object.
+     */
+    setScrollY: function (value)
+    {
+        this.scrollY = value;
+
+        return this;
+    }
+
+});
+
+module.exports = DynamicBitmapText;
+
+
+/***/ }),
+/* 160 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @author       Felipe Alfonso <@bitnenfer>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var ArrayUtils = __webpack_require__(164);
+var BlendModes = __webpack_require__(66);
+var Class = __webpack_require__(0);
+var Components = __webpack_require__(14);
+var GameObject = __webpack_require__(19);
+var Rectangle = __webpack_require__(9);
+var Render = __webpack_require__(837);
+var Union = __webpack_require__(309);
+var Vector2 = __webpack_require__(3);
+
+/**
+ * @classdesc
+ * A Container Game Object.
+ *
+ * A Container, as the name implies, can 'contain' other types of Game Object.
+ * When a Game Object is added to a Container, the Container becomes responsible for the rendering of it.
+ * By default it will be removed from the Display List and instead added to the Containers own internal list.
+ *
+ * The position of the Game Object automatically becomes relative to the position of the Container.
+ *
+ * When the Container is rendered, all of its children are rendered as well, in the order in which they exist
+ * within the Container. Container children can be repositioned using methods such as `MoveUp`, `MoveDown` and `SendToBack`.
+ *
+ * If you modify a transform property of the Container, such as `Container.x` or `Container.rotation` then it will
+ * automatically influence all children as well.
+ *
+ * Containers can include other Containers for deeply nested transforms.
+ *
+ * Containers can have masks set on them and can be used as a mask too. However, Container children cannot be masked.
+ * The masks do not 'stack up'. Only a Container on the root of the display list will use its mask.
+ *
+ * Containers can be enabled for input. Because they do not have a texture you need to provide a shape for them
+ * to use as their hit area. Container children can also be enabled for input, independent of the Container.
+ *
+ * Containers can be given a physics body for either Arcade Physics, Impact Physics or Matter Physics. However,
+ * if Container _children_ are enabled for physics you may get unexpected results, such as offset bodies,
+ * if the Container itself, or any of its ancestors, is positioned anywhere other than at 0 x 0. Container children
+ * with physics do not factor in the Container due to the excessive extra calculations needed. Please structure
+ * your game to work around this.
+ *
+ * It's important to understand the impact of using Containers. They add additional processing overhead into
+ * every one of their children. The deeper you nest them, the more the cost escalates. This is especially true
+ * for input events. You also loose the ability to set the display depth of Container children in the same
+ * flexible manner as those not within them. In short, don't use them for the sake of it. You pay a small cost
+ * every time you create one, try to structure your game around avoiding that where possible.
+ *
+ * @class Container
+ * @extends Phaser.GameObjects.GameObject
+ * @memberof Phaser.GameObjects
+ * @constructor
+ * @since 3.4.0
+ *
+ * @extends Phaser.GameObjects.Components.Alpha
+ * @extends Phaser.GameObjects.Components.BlendMode
+ * @extends Phaser.GameObjects.Components.ComputedSize
+ * @extends Phaser.GameObjects.Components.Depth
+ * @extends Phaser.GameObjects.Components.Mask
+ * @extends Phaser.GameObjects.Components.ScrollFactor
+ * @extends Phaser.GameObjects.Components.Transform
+ * @extends Phaser.GameObjects.Components.Visible
+ *
+ * @param {Phaser.Scene} scene - The Scene to which this Game Object belongs. A Game Object can only belong to one Scene at a time.
+ * @param {number} [x=0] - The horizontal position of this Game Object in the world.
+ * @param {number} [y=0] - The vertical position of this Game Object in the world.
+ * @param {Phaser.GameObjects.GameObject[]} [children] - An optional array of Game Objects to add to this Container.
+ */
+var Container = new Class({
+
+    Extends: GameObject,
+
+    Mixins: [
+        Components.Alpha,
