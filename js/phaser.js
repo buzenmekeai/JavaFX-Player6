ead85ddede7dea9b681c7e818a1f7bd314c32215
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-06-05 13:15:50 +0000

    clean slate

--- js/phaser.js
+++ js/phaser.js
@@ -41651,3 +41651,223 @@ var FromPoints = function (points, out)
         {
             px = p[0];
             py = p[1];
+        }
+        else
+        {
+            px = p.x;
+            py = p.y;
+        }
+
+        minX = Math.min(minX, px);
+        minY = Math.min(minY, py);
+
+        maxX = Math.max(maxX, px);
+        maxY = Math.max(maxY, py);
+    }
+
+    out.x = minX;
+    out.y = minY;
+    out.width = maxX - minX;
+    out.height = maxY - minY;
+
+    return out;
+};
+
+module.exports = FromPoints;
+
+
+/***/ }),
+/* 174 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Back = __webpack_require__(369);
+var Bounce = __webpack_require__(368);
+var Circular = __webpack_require__(367);
+var Cubic = __webpack_require__(366);
+var Elastic = __webpack_require__(365);
+var Expo = __webpack_require__(364);
+var Linear = __webpack_require__(363);
+var Quadratic = __webpack_require__(362);
+var Quartic = __webpack_require__(361);
+var Quintic = __webpack_require__(360);
+var Sine = __webpack_require__(359);
+var Stepped = __webpack_require__(358);
+
+//  EaseMap
+module.exports = {
+
+    Power0: Linear,
+    Power1: Quadratic.Out,
+    Power2: Cubic.Out,
+    Power3: Quartic.Out,
+    Power4: Quintic.Out,
+
+    Linear: Linear,
+    Quad: Quadratic.Out,
+    Cubic: Cubic.Out,
+    Quart: Quartic.Out,
+    Quint: Quintic.Out,
+    Sine: Sine.Out,
+    Expo: Expo.Out,
+    Circ: Circular.Out,
+    Elastic: Elastic.Out,
+    Back: Back.Out,
+    Bounce: Bounce.Out,
+    Stepped: Stepped,
+
+    'Quad.easeIn': Quadratic.In,
+    'Cubic.easeIn': Cubic.In,
+    'Quart.easeIn': Quartic.In,
+    'Quint.easeIn': Quintic.In,
+    'Sine.easeIn': Sine.In,
+    'Expo.easeIn': Expo.In,
+    'Circ.easeIn': Circular.In,
+    'Elastic.easeIn': Elastic.In,
+    'Back.easeIn': Back.In,
+    'Bounce.easeIn': Bounce.In,
+
+    'Quad.easeOut': Quadratic.Out,
+    'Cubic.easeOut': Cubic.Out,
+    'Quart.easeOut': Quartic.Out,
+    'Quint.easeOut': Quintic.Out,
+    'Sine.easeOut': Sine.Out,
+    'Expo.easeOut': Expo.Out,
+    'Circ.easeOut': Circular.Out,
+    'Elastic.easeOut': Elastic.Out,
+    'Back.easeOut': Back.Out,
+    'Bounce.easeOut': Bounce.Out,
+
+    'Quad.easeInOut': Quadratic.InOut,
+    'Cubic.easeInOut': Cubic.InOut,
+    'Quart.easeInOut': Quartic.InOut,
+    'Quint.easeInOut': Quintic.InOut,
+    'Sine.easeInOut': Sine.InOut,
+    'Expo.easeInOut': Expo.InOut,
+    'Circ.easeInOut': Circular.InOut,
+    'Elastic.easeInOut': Elastic.InOut,
+    'Back.easeInOut': Back.InOut,
+    'Bounce.easeInOut': Bounce.InOut
+
+};
+
+
+/***/ }),
+/* 175 */
+/***/ (function(module, exports) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+// Centers this Rectangle so that the center coordinates match the given x and y values.
+
+/**
+ * Moves the top-left corner of a Rectangle so that its center is at the given coordinates.
+ *
+ * @function Phaser.Geom.Rectangle.CenterOn
+ * @since 3.0.0
+ *
+ * @generic {Phaser.Geom.Rectangle} O - [rect,$return]
+ *
+ * @param {Phaser.Geom.Rectangle} rect - The Rectangle to be centered.
+ * @param {number} x - The X coordinate of the Rectangle's center.
+ * @param {number} y - The Y coordinate of the Rectangle's center.
+ *
+ * @return {Phaser.Geom.Rectangle} The centered rectangle.
+ */
+var CenterOn = function (rect, x, y)
+{
+    rect.x = x - (rect.width / 2);
+    rect.y = y - (rect.height / 2);
+
+    return rect;
+};
+
+module.exports = CenterOn;
+
+
+/***/ }),
+/* 176 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetColor = __webpack_require__(177);
+
+/**
+ * Converts an HSV (hue, saturation and value) color value to RGB.
+ * Conversion formula from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes HSV values are contained in the set [0, 1].
+ * Based on code by Michael Jackson (https://github.com/mjijackson)
+ *
+ * @function Phaser.Display.Color.HSVToRGB
+ * @since 3.0.0
+ *
+ * @param {number} h - The hue, in the range 0 - 1. This is the base color.
+ * @param {number} s - The saturation, in the range 0 - 1. This controls how much of the hue will be in the final color, where 1 is fully saturated and 0 will give you white.
+ * @param {number} v - The value, in the range 0 - 1. This controls how dark the color is. Where 1 is as bright as possible and 0 is black.
+ * @param {(ColorObject|Phaser.Display.Color)} [out] - A Color object to store the results in. If not given a new ColorObject will be created.
+ *
+ * @return {(ColorObject|Phaser.Display.Color)} An object with the red, green and blue values set in the r, g and b properties.
+ */
+var HSVToRGB = function (h, s, v, out)
+{
+    if (s === undefined) { s = 1; }
+    if (v === undefined) { v = 1; }
+
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+
+    var p = Math.floor((v * (1 - s)) * 255);
+    var q = Math.floor((v * (1 - f * s)) * 255);
+    var t = Math.floor((v * (1 - (1 - f) * s)) * 255);
+
+    v = Math.floor(v *= 255);
+
+    var r = v;
+    var g = v;
+    var b = v;
+
+    var c = i % 6;
+
+    if (c === 0)
+    {
+        g = t;
+        b = p;
+    }
+    else if (c === 1)
+    {
+        r = q;
+        b = p;
+    }
+    else if (c === 2)
+    {
+        r = p;
+        b = t;
+    }
+    else if (c === 3)
+    {
+        r = p;
+        g = q;
+    }
+    else if (c === 4)
+    {
+        r = t;
+        g = p;
+    }
+    else if (c === 5)
+    {
+        g = p;
+        b = q;
