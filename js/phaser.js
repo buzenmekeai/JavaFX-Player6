9268934001d896e135c7df5a9757c688c70ae0a7
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-07-30 06:08:44 +0000

    removing readme

--- js/phaser.js
+++ js/phaser.js
@@ -55991,3 +55991,219 @@ var ProcessQueue = new Class({
     }
 
 });
+
+module.exports = ProcessQueue;
+
+
+/***/ }),
+/* 229 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var CONST = __webpack_require__(35);
+
+/**
+ * [description]
+ *
+ * @function Phaser.Physics.Arcade.GetOverlapY
+ * @since 3.0.0
+ *
+ * @param {Phaser.Physics.Arcade.Body} body1 - [description]
+ * @param {Phaser.Physics.Arcade.Body} body2 - [description]
+ * @param {boolean} overlapOnly - [description]
+ * @param {number} bias - [description]
+ *
+ * @return {number} [description]
+ */
+var GetOverlapY = function (body1, body2, overlapOnly, bias)
+{
+    var overlap = 0;
+    var maxOverlap = body1.deltaAbsY() + body2.deltaAbsY() + bias;
+
+    if (body1._dy === 0 && body2._dy === 0)
+    {
+        //  They overlap but neither of them are moving
+        body1.embedded = true;
+        body2.embedded = true;
+    }
+    else if (body1._dy > body2._dy)
+    {
+        //  Body1 is moving down and/or Body2 is moving up
+        overlap = body1.bottom - body2.y;
+
+        if ((overlap > maxOverlap && !overlapOnly) || body1.checkCollision.down === false || body2.checkCollision.up === false)
+        {
+            overlap = 0;
+        }
+        else
+        {
+            body1.touching.none = false;
+            body1.touching.down = true;
+
+            body2.touching.none = false;
+            body2.touching.up = true;
+
+            if (body2.physicsType === CONST.STATIC_BODY)
+            {
+                body1.blocked.none = false;
+                body1.blocked.down = true;
+            }
+
+            if (body1.physicsType === CONST.STATIC_BODY)
+            {
+                body2.blocked.none = false;
+                body2.blocked.up = true;
+            }
+        }
+    }
+    else if (body1._dy < body2._dy)
+    {
+        //  Body1 is moving up and/or Body2 is moving down
+        overlap = body1.y - body2.bottom;
+
+        if ((-overlap > maxOverlap && !overlapOnly) || body1.checkCollision.up === false || body2.checkCollision.down === false)
+        {
+            overlap = 0;
+        }
+        else
+        {
+            body1.touching.none = false;
+            body1.touching.up = true;
+
+            body2.touching.none = false;
+            body2.touching.down = true;
+
+            if (body2.physicsType === CONST.STATIC_BODY)
+            {
+                body1.blocked.none = false;
+                body1.blocked.up = true;
+            }
+
+            if (body1.physicsType === CONST.STATIC_BODY)
+            {
+                body2.blocked.none = false;
+                body2.blocked.down = true;
+            }
+        }
+    }
+
+    //  Resets the overlapY to zero if there is no overlap, or to the actual pixel value if there is
+    body1.overlapY = overlap;
+    body2.overlapY = overlap;
+
+    return overlap;
+};
+
+module.exports = GetOverlapY;
+
+
+/***/ }),
+/* 230 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var CONST = __webpack_require__(35);
+
+/**
+ * [description]
+ *
+ * @function Phaser.Physics.Arcade.GetOverlapX
+ * @since 3.0.0
+ *
+ * @param {Phaser.Physics.Arcade.Body} body1 - [description]
+ * @param {Phaser.Physics.Arcade.Body} body2 - [description]
+ * @param {boolean} overlapOnly - [description]
+ * @param {number} bias - [description]
+ *
+ * @return {number} [description]
+ */
+var GetOverlapX = function (body1, body2, overlapOnly, bias)
+{
+    var overlap = 0;
+    var maxOverlap = body1.deltaAbsX() + body2.deltaAbsX() + bias;
+
+    if (body1._dx === 0 && body2._dx === 0)
+    {
+        //  They overlap but neither of them are moving
+        body1.embedded = true;
+        body2.embedded = true;
+    }
+    else if (body1._dx > body2._dx)
+    {
+        //  Body1 is moving right and / or Body2 is moving left
+        overlap = body1.right - body2.x;
+
+        if ((overlap > maxOverlap && !overlapOnly) || body1.checkCollision.right === false || body2.checkCollision.left === false)
+        {
+            overlap = 0;
+        }
+        else
+        {
+            body1.touching.none = false;
+            body1.touching.right = true;
+
+            body2.touching.none = false;
+            body2.touching.left = true;
+
+            if (body2.physicsType === CONST.STATIC_BODY)
+            {
+                body1.blocked.none = false;
+                body1.blocked.right = true;
+            }
+
+            if (body1.physicsType === CONST.STATIC_BODY)
+            {
+                body2.blocked.none = false;
+                body2.blocked.left = true;
+            }
+        }
+    }
+    else if (body1._dx < body2._dx)
+    {
+        //  Body1 is moving left and/or Body2 is moving right
+        overlap = body1.x - body2.width - body2.x;
+
+        if ((-overlap > maxOverlap && !overlapOnly) || body1.checkCollision.left === false || body2.checkCollision.right === false)
+        {
+            overlap = 0;
+        }
+        else
+        {
+            body1.touching.none = false;
+            body1.touching.left = true;
+
+            body2.touching.none = false;
+            body2.touching.right = true;
+
+            if (body2.physicsType === CONST.STATIC_BODY)
+            {
+                body1.blocked.none = false;
+                body1.blocked.left = true;
+            }
+
+            if (body1.physicsType === CONST.STATIC_BODY)
+            {
+                body2.blocked.none = false;
+                body2.blocked.right = true;
+            }
+        }
+    }
+
+    //  Resets the overlapX to zero if there is no overlap, or to the actual pixel value if there is
+    body1.overlapX = overlap;
+    body2.overlapX = overlap;
+
+    return overlap;
+};
+
+module.exports = GetOverlapX;
