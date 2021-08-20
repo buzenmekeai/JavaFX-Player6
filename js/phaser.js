71b8434061906a905e26d73b34bfcf75e1a18e3e
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-08-20 05:46:31 +0000

    working on custom banner

--- js/phaser.js
+++ js/phaser.js
@@ -59761,3 +59761,236 @@ var World = new Class({
 
         var speed = body.speed;
         var allowDrag = body.allowDrag;
+        var useDamping = body.useDamping;
+
+        if (body.allowGravity)
+        {
+            velocityX += (this.gravity.x + body.gravity.x) * delta;
+            velocityY += (this.gravity.y + body.gravity.y) * delta;
+        }
+
+        if (accelerationX)
+        {
+            velocityX += accelerationX * delta;
+        }
+        else if (allowDrag && dragX)
+        {
+            if (useDamping)
+            {
+                //  Damping based deceleration
+                velocityX *= dragX;
+
+                if (FuzzyEqual(speed, 0, 0.001))
+                {
+                    velocityX = 0;
+                }
+            }
+            else
+            {
+                //  Linear deceleration
+                dragX *= delta;
+
+                if (FuzzyGreaterThan(velocityX - dragX, 0, 0.01))
+                {
+                    velocityX -= dragX;
+                }
+                else if (FuzzyLessThan(velocityX + dragX, 0, 0.01))
+                {
+                    velocityX += dragX;
+                }
+                else
+                {
+                    velocityX = 0;
+                }
+            }
+        }
+
+        if (accelerationY)
+        {
+            velocityY += accelerationY * delta;
+        }
+        else if (allowDrag && dragY)
+        {
+            if (useDamping)
+            {
+                //  Damping based deceleration
+                velocityY *= dragY;
+
+                if (FuzzyEqual(speed, 0, 0.001))
+                {
+                    velocityY = 0;
+                }
+            }
+            else
+            {
+                //  Linear deceleration
+                dragY *= delta;
+
+                if (FuzzyGreaterThan(velocityY - dragY, 0, 0.01))
+                {
+                    velocityY -= dragY;
+                }
+                else if (FuzzyLessThan(velocityY + dragY, 0, 0.01))
+                {
+                    velocityY += dragY;
+                }
+                else
+                {
+                    velocityY = 0;
+                }
+            }
+        }
+
+        velocityX = Clamp(velocityX, -maxX, maxX);
+        velocityY = Clamp(velocityY, -maxY, maxY);
+
+        body.velocity.set(velocityX, velocityY);
+    },
+
+    /**
+     * Separates two Bodies.
+     *
+     * @method Phaser.Physics.Arcade.World#separate
+     * @fires Phaser.Physics.Arcade.World#collide
+     * @fires Phaser.Physics.Arcade.World#overlap
+     * @since 3.0.0
+     *
+     * @param {Phaser.Physics.Arcade.Body} body1 - The first Body to be separated.
+     * @param {Phaser.Physics.Arcade.Body} body2 - The second Body to be separated.
+     * @param {ArcadePhysicsCallback} [processCallback] - The process callback.
+     * @param {*} [callbackContext] - The context in which to invoke the callback.
+     * @param {boolean} [overlapOnly] - If this a collide or overlap check?
+     *
+     * @return {boolean} True if separation occurred, otherwise false.
+     */
+    separate: function (body1, body2, processCallback, callbackContext, overlapOnly)
+    {
+        if (
+            !body1.enable ||
+            !body2.enable ||
+            body1.checkCollision.none ||
+            body2.checkCollision.none ||
+            !this.intersects(body1, body2))
+        {
+            return false;
+        }
+
+        //  They overlap. Is there a custom process callback? If it returns true then we can carry on, otherwise we should abort.
+        if (processCallback && processCallback.call(callbackContext, body1.gameObject, body2.gameObject) === false)
+        {
+            return false;
+        }
+
+        //  Circle vs. Circle quick bail out
+        if (body1.isCircle && body2.isCircle)
+        {
+            return this.separateCircle(body1, body2, overlapOnly);
+        }
+
+        // We define the behavior of bodies in a collision circle and rectangle
+        // If a collision occurs in the corner points of the rectangle, the body behave like circles
+
+        //  Either body1 or body2 is a circle
+        if (body1.isCircle !== body2.isCircle)
+        {
+            var bodyRect = (body1.isCircle) ? body2 : body1;
+            var bodyCircle = (body1.isCircle) ? body1 : body2;
+
+            var rect = {
+                x: bodyRect.x,
+                y: bodyRect.y,
+                right: bodyRect.right,
+                bottom: bodyRect.bottom
+            };
+
+            var circle = bodyCircle.center;
+
+            if (circle.y < rect.y || circle.y > rect.bottom)
+            {
+                if (circle.x < rect.x || circle.x > rect.right)
+                {
+                    return this.separateCircle(body1, body2, overlapOnly);
+                }
+            }
+        }
+
+        var resultX = false;
+        var resultY = false;
+
+        //  Do we separate on x or y first?
+        if (this.forceX || Math.abs(this.gravity.y + body1.gravity.y) < Math.abs(this.gravity.x + body1.gravity.x))
+        {
+            resultX = SeparateX(body1, body2, overlapOnly, this.OVERLAP_BIAS);
+
+            //  Are they still intersecting? Let's do the other axis then
+            if (this.intersects(body1, body2))
+            {
+                resultY = SeparateY(body1, body2, overlapOnly, this.OVERLAP_BIAS);
+            }
+        }
+        else
+        {
+            resultY = SeparateY(body1, body2, overlapOnly, this.OVERLAP_BIAS);
+
+            //  Are they still intersecting? Let's do the other axis then
+            if (this.intersects(body1, body2))
+            {
+                resultX = SeparateX(body1, body2, overlapOnly, this.OVERLAP_BIAS);
+            }
+        }
+
+        var result = (resultX || resultY);
+
+        if (result)
+        {
+            if (overlapOnly && (body1.onOverlap || body2.onOverlap))
+            {
+                this.emit('overlap', body1.gameObject, body2.gameObject, body1, body2);
+            }
+            else
+            {
+                body1.postUpdate();
+                body2.postUpdate();
+
+                if (body1.onCollide || body2.onCollide)
+                {
+                    this.emit('collide', body1.gameObject, body2.gameObject, body1, body2);
+                }
+            }
+        }
+
+        return result;
+    },
+
+    /**
+     * Separates two Bodies, when both are circular.
+     *
+     * @method Phaser.Physics.Arcade.World#separateCircle
+     * @fires Phaser.Physics.Arcade.World#collide
+     * @fires Phaser.Physics.Arcade.World#overlap
+     * @since 3.0.0
+     *
+     * @param {Phaser.Physics.Arcade.Body} body1 - The first Body to be separated.
+     * @param {Phaser.Physics.Arcade.Body} body2 - The second Body to be separated.
+     * @param {boolean} [overlapOnly] - If this a collide or overlap check?
+     * @param {number} bias - A small value added to the calculations.
+     *
+     * @return {boolean} True if separation occurred, otherwise false.
+     */
+    separateCircle: function (body1, body2, overlapOnly, bias)
+    {
+        //  Set the bounding box overlap values into the bodies themselves (hence we don't use the return values here)
+        GetOverlapX(body1, body2, false, bias);
+        GetOverlapY(body1, body2, false, bias);
+
+        var dx = body2.center.x - body1.center.x;
+        var dy = body2.center.y - body1.center.y;
+
+        var angleCollision = Math.atan2(dy, dx);
+
+        var overlap = 0;
+
+        if (body1.isCircle !== body2.isCircle)
+        {
+            var rect = {
+                x: (body2.isCircle) ? body1.position.x : body2.position.x,
