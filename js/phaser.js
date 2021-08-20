4dda67fcbd9022fd67e5a771bddda6e62d200d51
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-08-20 12:05:03 +0000

    key: Fix resource leak

--- js/phaser.js
+++ js/phaser.js
@@ -59994,3 +59994,218 @@ var World = new Class({
         {
             var rect = {
                 x: (body2.isCircle) ? body1.position.x : body2.position.x,
+                y: (body2.isCircle) ? body1.position.y : body2.position.y,
+                right: (body2.isCircle) ? body1.right : body2.right,
+                bottom: (body2.isCircle) ? body1.bottom : body2.bottom
+            };
+
+            var circle = {
+                x: (body1.isCircle) ? body1.center.x : body2.center.x,
+                y: (body1.isCircle) ? body1.center.y : body2.center.y,
+                radius: (body1.isCircle) ? body1.halfWidth : body2.halfWidth
+            };
+
+            if (circle.y < rect.y)
+            {
+                if (circle.x < rect.x)
+                {
+                    overlap = DistanceBetween(circle.x, circle.y, rect.x, rect.y) - circle.radius;
+                }
+                else if (circle.x > rect.right)
+                {
+                    overlap = DistanceBetween(circle.x, circle.y, rect.right, rect.y) - circle.radius;
+                }
+            }
+            else if (circle.y > rect.bottom)
+            {
+                if (circle.x < rect.x)
+                {
+                    overlap = DistanceBetween(circle.x, circle.y, rect.x, rect.bottom) - circle.radius;
+                }
+                else if (circle.x > rect.right)
+                {
+                    overlap = DistanceBetween(circle.x, circle.y, rect.right, rect.bottom) - circle.radius;
+                }
+            }
+
+            overlap *= -1;
+        }
+        else
+        {
+            overlap = (body1.halfWidth + body2.halfWidth) - DistanceBetween(body1.center.x, body1.center.y, body2.center.x, body2.center.y);
+        }
+
+        //  Can't separate two immovable bodies, or a body with its own custom separation logic
+        if (overlapOnly || overlap === 0 || (body1.immovable && body2.immovable) || body1.customSeparateX || body2.customSeparateX)
+        {
+            if (overlap !== 0 && (body1.onOverlap || body2.onOverlap))
+            {
+                this.emit('overlap', body1.gameObject, body2.gameObject, body1, body2);
+            }
+
+            //  return true if there was some overlap, otherwise false
+            return (overlap !== 0);
+        }
+
+        // Transform the velocity vector to the coordinate system oriented along the direction of impact.
+        // This is done to eliminate the vertical component of the velocity
+
+        var b1vx = body1.velocity.x;
+        var b1vy = body1.velocity.y;
+        var b1mass = body1.mass;
+
+        var b2vx = body2.velocity.x;
+        var b2vy = body2.velocity.y;
+        var b2mass = body2.mass;
+
+        var v1 = {
+            x: b1vx * Math.cos(angleCollision) + b1vy * Math.sin(angleCollision),
+            y: b1vx * Math.sin(angleCollision) - b1vy * Math.cos(angleCollision)
+        };
+
+        var v2 = {
+            x: b2vx * Math.cos(angleCollision) + b2vy * Math.sin(angleCollision),
+            y: b2vx * Math.sin(angleCollision) - b2vy * Math.cos(angleCollision)
+        };
+
+        // We expect the new velocity after impact
+        var tempVel1 = ((b1mass - b2mass) * v1.x + 2 * b2mass * v2.x) / (b1mass + b2mass);
+        var tempVel2 = (2 * b1mass * v1.x + (b2mass - b1mass) * v2.x) / (b1mass + b2mass);
+
+        // We convert the vector to the original coordinate system and multiplied by factor of rebound
+        if (!body1.immovable)
+        {
+            body1.velocity.x = (tempVel1 * Math.cos(angleCollision) - v1.y * Math.sin(angleCollision)) * body1.bounce.x;
+            body1.velocity.y = (v1.y * Math.cos(angleCollision) + tempVel1 * Math.sin(angleCollision)) * body1.bounce.y;
+
+            //  Reset local var
+            b1vx = body1.velocity.x;
+            b1vy = body1.velocity.y;
+        }
+
+        if (!body2.immovable)
+        {
+            body2.velocity.x = (tempVel2 * Math.cos(angleCollision) - v2.y * Math.sin(angleCollision)) * body2.bounce.x;
+            body2.velocity.y = (v2.y * Math.cos(angleCollision) + tempVel2 * Math.sin(angleCollision)) * body2.bounce.y;
+
+            //  Reset local var
+            b2vx = body2.velocity.x;
+            b2vy = body2.velocity.y;
+        }
+
+        // When the collision angle is almost perpendicular to the total initial velocity vector
+        // (collision on a tangent) vector direction can be determined incorrectly.
+        // This code fixes the problem
+
+        if (Math.abs(angleCollision) < Math.PI / 2)
+        {
+            if ((b1vx > 0) && !body1.immovable && (b2vx > b1vx))
+            {
+                body1.velocity.x *= -1;
+            }
+            else if ((b2vx < 0) && !body2.immovable && (b1vx < b2vx))
+            {
+                body2.velocity.x *= -1;
+            }
+            else if ((b1vy > 0) && !body1.immovable && (b2vy > b1vy))
+            {
+                body1.velocity.y *= -1;
+            }
+            else if ((b2vy < 0) && !body2.immovable && (b1vy < b2vy))
+            {
+                body2.velocity.y *= -1;
+            }
+        }
+        else if (Math.abs(angleCollision) > Math.PI / 2)
+        {
+            if ((b1vx < 0) && !body1.immovable && (b2vx < b1vx))
+            {
+                body1.velocity.x *= -1;
+            }
+            else if ((b2vx > 0) && !body2.immovable && (b1vx > b2vx))
+            {
+                body2.velocity.x *= -1;
+            }
+            else if ((b1vy < 0) && !body1.immovable && (b2vy < b1vy))
+            {
+                body1.velocity.y *= -1;
+            }
+            else if ((b2vy > 0) && !body2.immovable && (b1vx > b2vy))
+            {
+                body2.velocity.y *= -1;
+            }
+        }
+
+        var delta = this._frameTime;
+
+        if (!body1.immovable)
+        {
+            body1.x += (body1.velocity.x * delta) - overlap * Math.cos(angleCollision);
+            body1.y += (body1.velocity.y * delta) - overlap * Math.sin(angleCollision);
+        }
+
+        if (!body2.immovable)
+        {
+            body2.x += (body2.velocity.x * delta) + overlap * Math.cos(angleCollision);
+            body2.y += (body2.velocity.y * delta) + overlap * Math.sin(angleCollision);
+        }
+
+        if (body1.onCollide || body2.onCollide)
+        {
+            this.emit('collide', body1.gameObject, body2.gameObject, body1, body2);
+        }
+
+        //  sync changes back to the bodies
+        body1.postUpdate();
+        body2.postUpdate();
+
+        return true;
+    },
+
+    /**
+     * Checks to see if two Bodies intersect at all.
+     *
+     * @method Phaser.Physics.Arcade.World#intersects
+     * @since 3.0.0
+     *
+     * @param {Phaser.Physics.Arcade.Body} body1 - The first body to check.
+     * @param {Phaser.Physics.Arcade.Body} body2 - The second body to check.
+     *
+     * @return {boolean} True if the two bodies intersect, otherwise false.
+     */
+    intersects: function (body1, body2)
+    {
+        if (body1 === body2)
+        {
+            return false;
+        }
+
+        if (!body1.isCircle && !body2.isCircle)
+        {
+            //  Rect vs. Rect
+            return !(
+                body1.right <= body2.position.x ||
+                body1.bottom <= body2.position.y ||
+                body1.position.x >= body2.right ||
+                body1.position.y >= body2.bottom
+            );
+        }
+        else if (body1.isCircle)
+        {
+            if (body2.isCircle)
+            {
+                //  Circle vs. Circle
+                return DistanceBetween(body1.center.x, body1.center.y, body2.center.x, body2.center.y) <= (body1.halfWidth + body2.halfWidth);
+            }
+            else
+            {
+                //  Circle vs. Rect
+                return this.circleBodyIntersects(body1, body2);
+            }
+        }
+        else
+        {
+            //  Rect vs. Circle
+            return this.circleBodyIntersects(body2, body1);
+        }
+    },
