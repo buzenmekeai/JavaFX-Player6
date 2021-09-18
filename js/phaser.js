1fde8f21a1504542c5ecf3a72a6e55d0f3e75af5
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-09-18 17:32:44 +0000

    added .gitignore.

--- js/phaser.js
+++ js/phaser.js
@@ -69371,3 +69371,242 @@ var Rectangle = __webpack_require__(9);
  *
  * @param {Phaser.Geom.Polygon} polygon - [description]
  * @param {(Phaser.Geom.Rectangle|object)} [out] - [description]
+ *
+ * @return {(Phaser.Geom.Rectangle|object)} [description]
+ */
+var GetAABB = function (polygon, out)
+{
+    if (out === undefined) { out = new Rectangle(); }
+
+    var minX = Infinity;
+    var minY = Infinity;
+    var maxX = -minX;
+    var maxY = -minY;
+    var p;
+
+    for (var i = 0; i < polygon.points.length; i++)
+    {
+        p = polygon.points[i];
+
+        minX = Math.min(minX, p.x);
+        minY = Math.min(minY, p.y);
+        maxX = Math.max(maxX, p.x);
+        maxY = Math.max(maxY, p.y);
+    }
+
+    out.x = minX;
+    out.y = minY;
+    out.width = maxX - minX;
+    out.height = maxY - minY;
+
+    return out;
+};
+
+module.exports = GetAABB;
+
+
+/***/ }),
+/* 286 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var PolygonRender = __webpack_require__(781);
+var Class = __webpack_require__(0);
+var Earcut = __webpack_require__(64);
+var GetAABB = __webpack_require__(285);
+var GeomPolygon = __webpack_require__(151);
+var Shape = __webpack_require__(27);
+var Smooth = __webpack_require__(282);
+
+/**
+ * @classdesc
+ * The Polygon Shape is a Game Object that can be added to a Scene, Group or Container. You can
+ * treat it like any other Game Object in your game, such as tweening it, scaling it, or enabling
+ * it for input or physics. It provides a quick and easy way for you to render this shape in your
+ * game without using a texture, while still taking advantage of being fully batched in WebGL.
+ * 
+ * This shape supports both fill and stroke colors.
+ * 
+ * The Polygon Shape is created by providing a list of points, which are then used to create an
+ * internal Polygon geometry object. The points can be set from a variety of formats:
+ *
+ * - A string containing paired values separated by a single space: `'40 0 40 20 100 20 100 80 40 80 40 100 0 50'`
+ * - An array of Point or Vector2 objects: `[new Phaser.Math.Vec2(x1, y1), ...]`
+ * - An array of objects with public x/y properties: `[obj1, obj2, ...]`
+ * - An array of paired numbers that represent point coordinates: `[x1,y1, x2,y2, ...]`
+ * - An array of arrays with two elements representing x/y coordinates: `[[x1, y1], [x2, y2], ...]`
+ * 
+ * By default the `x` and `y` coordinates of this Shape refer to the center of it. However, depending
+ * on the coordinates of the points provided, the final shape may be rendered offset from its origin.
+ *
+ * @class Polygon
+ * @extends Phaser.GameObjects.Shape
+ * @memberof Phaser.GameObjects
+ * @constructor
+ * @since 3.13.0
+ *
+ * @param {Phaser.Scene} scene - The Scene to which this Game Object belongs. A Game Object can only belong to one Scene at a time.
+ * @param {number} [x=0] - The horizontal position of this Game Object in the world.
+ * @param {number} [y=0] - The vertical position of this Game Object in the world.
+ * @param {any} [points] - The points that make up the polygon.
+ * @param {number} [fillColor] - The color the polygon will be filled with, i.e. 0xff0000 for red.
+ * @param {number} [fillAlpha] - The alpha the polygon will be filled with. You can also set the alpha of the overall Shape using its `alpha` property.
+ */
+var Polygon = new Class({
+
+    Extends: Shape,
+
+    Mixins: [
+        PolygonRender
+    ],
+
+    initialize:
+
+    function Polygon (scene, x, y, points, fillColor, fillAlpha)
+    {
+        if (x === undefined) { x = 0; }
+        if (y === undefined) { y = 0; }
+
+        Shape.call(this, scene, 'Polygon', new GeomPolygon(points));
+
+        var bounds = GetAABB(this.geom);
+
+        this.setPosition(x, y);
+        this.setSize(bounds.width, bounds.height);
+
+        if (fillColor !== undefined)
+        {
+            this.setFillStyle(fillColor, fillAlpha);
+        }
+
+        this.updateDisplayOrigin();
+        this.updateData();
+    },
+
+    /**
+     * Smooths the polygon over the number of iterations specified.
+     * The base polygon data will be updated and replaced with the smoothed values.
+     * This call can be chained.
+     *
+     * @method Phaser.GameObjects.Polygon#smooth
+     * @since 3.13.0
+     * 
+     * @param {integer} [iterations=1] - The number of times to apply the polygon smoothing.
+     *
+     * @return {this} This Game Object instance.
+     */
+    smooth: function (iterations)
+    {
+        if (iterations === undefined) { iterations = 1; }
+
+        for (var i = 0; i < iterations; i++)
+        {
+            Smooth(this.geom);
+        }
+
+        return this.updateData();
+    },
+
+    /**
+     * Internal method that updates the data and path values.
+     *
+     * @method Phaser.GameObjects.Polygon#updateData
+     * @private
+     * @since 3.13.0
+     *
+     * @return {this} This Game Object instance.
+     */
+    updateData: function ()
+    {
+        var path = [];
+        var points = this.geom.points;
+
+        for (var i = 0; i < points.length; i++)
+        {
+            path.push(points[i].x, points[i].y);
+        }
+
+        path.push(points[0].x, points[0].y);
+
+        this.pathIndexes = Earcut(path);
+        this.pathData = path;
+
+        return this;
+    }
+
+});
+
+module.exports = Polygon;
+
+
+/***/ }),
+/* 287 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Class = __webpack_require__(0);
+var Shape = __webpack_require__(27);
+var GeomLine = __webpack_require__(54);
+var LineRender = __webpack_require__(784);
+
+/**
+ * @classdesc
+ * The Line Shape is a Game Object that can be added to a Scene, Group or Container. You can
+ * treat it like any other Game Object in your game, such as tweening it, scaling it, or enabling
+ * it for input or physics. It provides a quick and easy way for you to render this shape in your
+ * game without using a texture, while still taking advantage of being fully batched in WebGL.
+ * 
+ * This shape supports only stroke colors and cannot be filled.
+ * 
+ * A Line Shape allows you to draw a line between two points in your game. You can control the
+ * stroke color and thickness of the line. In WebGL only you can also specify a different
+ * thickness for the start and end of the line, allowing you to render lines that taper-off.
+ * 
+ * If you need to draw multiple lines in a sequence you may wish to use the Polygon Shape instead.
+ *
+ * @class Line
+ * @extends Phaser.GameObjects.Shape
+ * @memberof Phaser.GameObjects
+ * @constructor
+ * @since 3.13.0
+ *
+ * @param {Phaser.Scene} scene - The Scene to which this Game Object belongs. A Game Object can only belong to one Scene at a time.
+ * @param {number} [x=0] - The horizontal position of this Game Object in the world.
+ * @param {number} [y=0] - The vertical position of this Game Object in the world.
+ * @param {number} [x1=0] - The horizontal position of the start of the line.
+ * @param {number} [y1=0] - The vertical position of the start of the line.
+ * @param {number} [x2=128] - The horizontal position of the end of the line.
+ * @param {number} [y2=0] - The vertical position of the end of the line.
+ * @param {number} [strokeColor] - The color the line will be drawn in, i.e. 0xff0000 for red.
+ * @param {number} [strokeAlpha] - The alpha the line will be drawn in. You can also set the alpha of the overall Shape using its `alpha` property.
+ */
+var Line = new Class({
+
+    Extends: Shape,
+
+    Mixins: [
+        LineRender
+    ],
+
+    initialize:
+
+    function Line (scene, x, y, x1, y1, x2, y2, strokeColor, strokeAlpha)
+    {
+        if (x === undefined) { x = 0; }
+        if (y === undefined) { y = 0; }
+        if (x1 === undefined) { x1 = 0; }
+        if (y1 === undefined) { y1 = 0; }
+        if (x2 === undefined) { x2 = 128; }
+        if (y2 === undefined) { y2 = 0; }
+
+        Shape.call(this, scene, 'Line', new GeomLine(x1, y1, x2, y2));
