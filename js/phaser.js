94d6214bae8807637c0bae1382c9ea2a6f7a9f95
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-09-19 08:06:10 +0000

    git gui: set GIT_ASKPASS=git-gui--askpass if not set yet

--- js/phaser.js
+++ js/phaser.js
@@ -69610,3 +69610,210 @@ var Line = new Class({
         if (y2 === undefined) { y2 = 0; }
 
         Shape.call(this, scene, 'Line', new GeomLine(x1, y1, x2, y2));
+
+        var width = this.geom.right - this.geom.left;
+        var height = this.geom.bottom - this.geom.top;
+
+        /**
+         * The width (or thickness) of the line.
+         * See the setLineWidth method for extra details on changing this on WebGL.
+         *
+         * @name Phaser.GameObjects.Line#lineWidth
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.lineWidth = 1;
+
+        /**
+         * Private internal value. Holds the start width of the line.
+         *
+         * @name Phaser.GameObjects.Line#_startWidth
+         * @type {number}
+         * @private
+         * @since 3.13.0
+         */
+        this._startWidth = 1;
+
+        /**
+         * Private internal value. Holds the end width of the line.
+         *
+         * @name Phaser.GameObjects.Line#_endWidth
+         * @type {number}
+         * @private
+         * @since 3.13.0
+         */
+        this._endWidth = 1;
+
+        this.setPosition(x, y);
+        this.setSize(width, height);
+
+        if (strokeColor !== undefined)
+        {
+            this.setStrokeStyle(1, strokeColor, strokeAlpha);
+        }
+
+        this.updateDisplayOrigin();
+    },
+
+    /**
+     * Sets the width of the line.
+     * 
+     * When using the WebGL renderer you can have different start and end widths.
+     * When using the Canvas renderer only the `startWidth` value is used. The `endWidth` is ignored.
+     * 
+     * This call can be chained.
+     *
+     * @method Phaser.GameObjects.Line#setLineWidth
+     * @since 3.13.0
+     * 
+     * @param {number} startWidth - The start width of the line.
+     * @param {number} [endWidth] - The end width of the line. Only used in WebGL.
+     *
+     * @return {this} This Game Object instance.
+     */
+    setLineWidth: function (startWidth, endWidth)
+    {
+        if (endWidth === undefined) { endWidth = startWidth; }
+
+        this._startWidth = startWidth;
+        this._endWidth = endWidth;
+
+        this.lineWidth = startWidth;
+
+        return this;
+    },
+
+    /**
+     * Sets the start and end coordinates of this Line.
+     *
+     * @method Phaser.GameObjects.Line#setTo
+     * @since 3.13.0
+     *
+     * @param {number} [x1=0] - The horizontal position of the start of the line.
+     * @param {number} [y1=0] - The vertical position of the start of the line.
+     * @param {number} [x2=0] - The horizontal position of the end of the line.
+     * @param {number} [y2=0] - The vertical position of the end of the line.
+     *
+     * @return {this} This Line object.
+     */
+    setTo: function (x1, y1, x2, y2)
+    {
+        this.geom.setTo(x1, y1, x2, y2);
+
+        return this;
+    }
+
+});
+
+module.exports = Line;
+
+
+/***/ }),
+/* 288 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Class = __webpack_require__(0);
+var IsoTriangleRender = __webpack_require__(787);
+var Shape = __webpack_require__(27);
+
+/**
+ * @classdesc
+ * The IsoTriangle Shape is a Game Object that can be added to a Scene, Group or Container. You can
+ * treat it like any other Game Object in your game, such as tweening it, scaling it, or enabling
+ * it for input or physics. It provides a quick and easy way for you to render this shape in your
+ * game without using a texture, while still taking advantage of being fully batched in WebGL.
+ * 
+ * This shape supports only fill colors and cannot be stroked.
+ * 
+ * An IsoTriangle is an 'isometric' triangle. Think of it like a pyramid. Each face has a different
+ * fill color. You can set the color of the top, left and right faces of the triangle respectively
+ * You can also choose which of the faces are rendered via the `showTop`, `showLeft` and `showRight` properties.
+ * 
+ * You cannot view an IsoTriangle from under-neath, however you can change the 'angle' by setting
+ * the `projection` property. The `reversed` property controls if the IsoTriangle is rendered upside
+ * down or not.
+ *
+ * @class IsoTriangle
+ * @extends Phaser.GameObjects.Shape
+ * @memberof Phaser.GameObjects
+ * @constructor
+ * @since 3.13.0
+ *
+ * @param {Phaser.Scene} scene - The Scene to which this Game Object belongs. A Game Object can only belong to one Scene at a time.
+ * @param {number} [x=0] - The horizontal position of this Game Object in the world.
+ * @param {number} [y=0] - The vertical position of this Game Object in the world.
+ * @param {number} [size=48] - The width of the iso triangle in pixels. The left and right faces will be exactly half this value.
+ * @param {number} [height=32] - The height of the iso triangle. The left and right faces will be this tall. The overall height of the iso triangle will be this value plus half the `size` value.
+ * @param {boolean} [reversed=false] - Is the iso triangle upside down?
+ * @param {number} [fillTop=0xeeeeee] - The fill color of the top face of the iso triangle.
+ * @param {number} [fillLeft=0x999999] - The fill color of the left face of the iso triangle.
+ * @param {number} [fillRight=0xcccccc] - The fill color of the right face of the iso triangle.
+ */
+var IsoTriangle = new Class({
+
+    Extends: Shape,
+
+    Mixins: [
+        IsoTriangleRender
+    ],
+
+    initialize:
+
+    function IsoTriangle (scene, x, y, size, height, reversed, fillTop, fillLeft, fillRight)
+    {
+        if (x === undefined) { x = 0; }
+        if (y === undefined) { y = 0; }
+        if (size === undefined) { size = 48; }
+        if (height === undefined) { height = 32; }
+        if (reversed === undefined) { reversed = false; }
+        if (fillTop === undefined) { fillTop = 0xeeeeee; }
+        if (fillLeft === undefined) { fillLeft = 0x999999; }
+        if (fillRight === undefined) { fillRight = 0xcccccc; }
+
+        Shape.call(this, scene, 'IsoTriangle', null);
+
+        /**
+         * The projection level of the iso box. Change this to change the 'angle' at which you are looking at the box.
+         *
+         * @name Phaser.GameObjects.IsoTriangle#projection
+         * @type {integer}
+         * @default 4
+         * @since 3.13.0
+         */
+        this.projection = 4;
+
+        /**
+         * The color used to fill in the top of the iso triangle. This is only used if the triangle is reversed.
+         *
+         * @name Phaser.GameObjects.IsoTriangle#fillTop
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.fillTop = fillTop;
+
+        /**
+         * The color used to fill in the left-facing side of the iso triangle.
+         *
+         * @name Phaser.GameObjects.IsoTriangle#fillLeft
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.fillLeft = fillLeft;
+
+        /**
+         * The color used to fill in the right-facing side of the iso triangle.
+         *
+         * @name Phaser.GameObjects.IsoTriangle#fillRight
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.fillRight = fillRight;
+
+        /**
+         * Controls if the top-face of the iso triangle be rendered.
