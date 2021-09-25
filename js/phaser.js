14f41043c303b11f3121f3c3b07b2ad822056da8
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-09-25 05:25:35 +0000

    small code cleanup

--- js/phaser.js
+++ js/phaser.js
@@ -71295,3 +71295,222 @@ module.exports = UUID;
 
 /**
  * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Class = __webpack_require__(0);
+var DegToRad = __webpack_require__(31);
+var GetBoolean = __webpack_require__(84);
+var GetValue = __webpack_require__(4);
+var Sprite = __webpack_require__(61);
+var TWEEN_CONST = __webpack_require__(83);
+var Vector2 = __webpack_require__(3);
+
+/**
+ * Settings for a PathFollower.
+ *
+ * @typedef {object} PathConfig
+ *
+ * @property {number} duration - The duration of the path follow.
+ * @property {number} from - The start position of the path follow, between 0 and 1.
+ * @property {number} to - The end position of the path follow, between 0 and 1.
+ * @property {boolean} [positionOnPath=false] - Whether to position the PathFollower on the Path using its path offset.
+ * @property {boolean} [rotateToPath=false] - Should the PathFollower automatically rotate to point in the direction of the Path?
+ * @property {number} [rotationOffset=0] - If the PathFollower is rotating to match the Path, this value is added to the rotation value. This allows you to rotate objects to a path but control the angle of the rotation as well.
+ * @property {boolean} [verticalAdjust=false] - [description]
+ */
+
+/**
+ * @classdesc
+ * A PathFollower Game Object.
+ *
+ * A PathFollower is a Sprite Game Object with some extra helpers to allow it to follow a Path automatically.
+ *
+ * Anything you can do with a standard Sprite can be done with this PathFollower, such as animate it, tint it,
+ * scale it and so on.
+ *
+ * PathFollowers are bound to a single Path at any one time and can traverse the length of the Path, from start
+ * to finish, forwards or backwards, or from any given point on the Path to its end. They can optionally rotate
+ * to face the direction of the path, be offset from the path coordinates or rotate independently of the Path.
+ *
+ * @class PathFollower
+ * @extends Phaser.GameObjects.Sprite
+ * @memberof Phaser.GameObjects
+ * @constructor
+ * @since 3.0.0
+ *
+ * @param {Phaser.Scene} scene - The Scene to which this PathFollower belongs.
+ * @param {Phaser.Curves.Path} path - The Path this PathFollower is following. It can only follow one Path at a time.
+ * @param {number} x - The horizontal position of this Game Object in the world.
+ * @param {number} y - The vertical position of this Game Object in the world.
+ * @param {string} texture - The key of the Texture this Game Object will use to render with, as stored in the Texture Manager.
+ * @param {(string|integer)} [frame] - An optional frame from the Texture this Game Object is rendering with.
+ */
+var PathFollower = new Class({
+
+    Extends: Sprite,
+
+    initialize:
+
+    function PathFollower (scene, path, x, y, texture, frame)
+    {
+        Sprite.call(this, scene, x, y, texture, frame);
+
+        /**
+         * The Path this PathFollower is following. It can only follow one Path at a time.
+         *
+         * @name Phaser.GameObjects.PathFollower#path
+         * @type {Phaser.Curves.Path}
+         * @since 3.0.0
+         */
+        this.path = path;
+
+        /**
+         * Should the PathFollower automatically rotate to point in the direction of the Path?
+         *
+         * @name Phaser.GameObjects.PathFollower#rotateToPath
+         * @type {boolean}
+         * @default false
+         * @since 3.0.0
+         */
+        this.rotateToPath = false;
+
+        /**
+         * [description]
+         *
+         * @name Phaser.GameObjects.PathFollower#pathRotationVerticalAdjust
+         * @type {boolean}
+         * @default false
+         * @since 3.0.0
+         */
+        this.pathRotationVerticalAdjust = false;
+
+        /**
+         * If the PathFollower is rotating to match the Path (@see Phaser.GameObjects.PathFollower#rotateToPath)
+         * this value is added to the rotation value. This allows you to rotate objects to a path but control
+         * the angle of the rotation as well.
+         *
+         * @name Phaser.GameObjects.PathFollower#pathRotationOffset
+         * @type {number}
+         * @default 0
+         * @since 3.0.0
+         */
+        this.pathRotationOffset = 0;
+
+        /**
+         * An additional vector to add to the PathFollowers position, allowing you to offset it from the
+         * Path coordinates.
+         *
+         * @name Phaser.GameObjects.PathFollower#pathOffset
+         * @type {Phaser.Math.Vector2}
+         * @since 3.0.0
+         */
+        this.pathOffset = new Vector2(x, y);
+
+        /**
+         * [description]
+         *
+         * @name Phaser.GameObjects.PathFollower#pathVector
+         * @type {Phaser.Math.Vector2}
+         * @since 3.0.0
+         */
+        this.pathVector = new Vector2();
+
+        /**
+         * The Tween used for following the Path.
+         *
+         * @name Phaser.GameObjects.PathFollower#pathTween
+         * @type {Phaser.Tweens.Tween}
+         * @since 3.0.0
+         */
+        this.pathTween;
+
+        /**
+         * Settings for the PathFollower.
+         *
+         * @name Phaser.GameObjects.PathFollower#pathConfig
+         * @type {?PathConfig}
+         * @default null
+         * @since 3.0.0
+         */
+        this.pathConfig = null;
+
+        /**
+         * Records the direction of the follower so it can change direction.
+         *
+         * @name Phaser.GameObjects.PathFollower#_prevDirection
+         * @type {integer}
+         * @private
+         * @since 3.0.0
+         */
+        this._prevDirection = TWEEN_CONST.PLAYING_FORWARD;
+    },
+
+    /**
+     * Set the Path that this PathFollower should follow.
+     *
+     * Optionally accepts {@link PathConfig} settings.
+     *
+     * @method Phaser.GameObjects.PathFollower#setPath
+     * @since 3.0.0
+     *
+     * @param {Phaser.Curves.Path} path - The Path this PathFollower is following. It can only follow one Path at a time.
+     * @param {PathConfig} [config] - Settings for the PathFollower.
+     *
+     * @return {Phaser.GameObjects.PathFollower} This Game Object.
+     */
+    setPath: function (path, config)
+    {
+        if (config === undefined) { config = this.pathConfig; }
+
+        var tween = this.pathTween;
+
+        if (tween && tween.isPlaying())
+        {
+            tween.stop();
+        }
+
+        this.path = path;
+
+        if (config)
+        {
+            this.startFollow(config);
+        }
+
+        return this;
+    },
+
+    /**
+     * Set whether the PathFollower should automatically rotate to point in the direction of the Path.
+     *
+     * @method Phaser.GameObjects.PathFollower#setRotateToPath
+     * @since 3.0.0
+     *
+     * @param {boolean} value - Whether the PathFollower should automatically rotate to point in the direction of the Path.
+     * @param {number} [offset=0] - Rotation offset in degrees.
+     * @param {boolean} [verticalAdjust=false] - [description]
+     *
+     * @return {Phaser.GameObjects.PathFollower} This Game Object.
+     */
+    setRotateToPath: function (value, offset, verticalAdjust)
+    {
+        if (offset === undefined) { offset = 0; }
+        if (verticalAdjust === undefined) { verticalAdjust = false; }
+
+        this.rotateToPath = value;
+
+        this.pathRotationOffset = offset;
+        this.pathRotationVerticalAdjust = verticalAdjust;
+
+        return this;
+    },
+
+    /**
+     * Is this PathFollower actively following a Path or not?
+     *
+     * To be considered as `isFollowing` it must be currently moving on a Path, and not paused.
+     *
+     * @method Phaser.GameObjects.PathFollower#isFollowing
+     * @since 3.0.0
+     *
