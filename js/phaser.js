57551eeab716321cc4cc4f13a694bd81e5dcaa89
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-10-26 11:18:33 +0000

    Prepare for release.

--- js/phaser.js
+++ js/phaser.js
@@ -78965,3 +78965,228 @@ var Extend = __webpack_require__(20);
  *
  * @class NoAudioSound
  * @extends Phaser.Sound.BaseSound
+ * @memberof Phaser.Sound
+ * @constructor
+ * @since 3.0.0
+ *
+ * @param {Phaser.Sound.NoAudioSoundManager} manager - Reference to the current sound manager instance.
+ * @param {string} key - Asset key for the sound.
+ * @param {SoundConfig} [config={}] - An optional config object containing default sound settings.
+ */
+var NoAudioSound = new Class({
+
+    Extends: EventEmitter,
+
+    initialize:
+
+    function NoAudioSound (manager, key, config)
+    {
+        if (config === void 0) { config = {}; }
+
+        EventEmitter.call(this);
+
+        this.manager = manager;
+        this.key = key;
+        this.isPlaying = false;
+        this.isPaused = false;
+        this.totalRate = 1;
+        this.duration = 0;
+        this.totalDuration = 0;
+
+        this.config = Extend({
+            mute: false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: false,
+            delay: 0
+        }, config);
+
+        this.currentConfig = this.config;
+        this.mute = false;
+        this.volume = 1;
+        this.rate = 1;
+        this.detune = 0;
+        this.seek = 0;
+        this.loop = false;
+        this.markers = {};
+        this.currentMarker = null;
+        this.pendingRemove = false;
+    },
+
+    // eslint-disable-next-line no-unused-vars
+    addMarker: function (marker)
+    {
+        return false;
+    },
+
+    // eslint-disable-next-line no-unused-vars
+    updateMarker: function (marker)
+    {
+        return false;
+    },
+
+    // eslint-disable-next-line no-unused-vars
+    removeMarker: function (markerName)
+    {
+        return null;
+    },
+
+    // eslint-disable-next-line no-unused-vars
+    play: function (markerName, config)
+    {
+        return false;
+    },
+
+    pause: function ()
+    {
+        return false;
+    },
+
+    resume: function ()
+    {
+        return false;
+    },
+
+    stop: function ()
+    {
+        return false;
+    },
+
+    destroy: function ()
+    {
+        this.manager.remove(this);
+
+        BaseSound.prototype.destroy.call(this);
+    }
+});
+
+module.exports = NoAudioSound;
+
+
+/***/ }),
+/* 322 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @author       Pavle Goloskokovic <pgoloskokovic@gmail.com> (http://prunegames.com)
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var BaseSoundManager = __webpack_require__(115);
+var Class = __webpack_require__(0);
+var EventEmitter = __webpack_require__(11);
+var NoAudioSound = __webpack_require__(321);
+var NOOP = __webpack_require__(1);
+
+/**
+ * @classdesc
+ * No audio implementation of the sound manager. It is used if audio has been
+ * disabled in the game config or the device doesn't support any audio.
+ *
+ * It represents a graceful degradation of sound manager logic that provides
+ * minimal functionality and prevents Phaser projects that use audio from
+ * breaking on devices that don't support any audio playback technologies.
+ *
+ * @class NoAudioSoundManager
+ * @extends Phaser.Sound.BaseSoundManager
+ * @memberof Phaser.Sound
+ * @constructor
+ * @since 3.0.0
+ *
+ * @param {Phaser.Game} game - Reference to the current game instance.
+ */
+var NoAudioSoundManager = new Class({
+
+    Extends: EventEmitter,
+
+    initialize:
+
+    function NoAudioSoundManager (game)
+    {
+        EventEmitter.call(this);
+
+        this.game = game;
+        this.sounds = [];
+        this.mute = false;
+        this.volume = 1;
+        this.rate = 1;
+        this.detune = 0;
+        this.pauseOnBlur = true;
+        this.locked = false;
+    },
+
+    add: function (key, config)
+    {
+        var sound = new NoAudioSound(this, key, config);
+
+        this.sounds.push(sound);
+
+        return sound;
+    },
+
+    addAudioSprite: function (key, config)
+    {
+        var sound = this.add(key, config);
+
+        sound.spritemap = {};
+
+        return sound;
+    },
+
+    // eslint-disable-next-line no-unused-vars
+    play: function (key, extra)
+    {
+        return false;
+    },
+
+    // eslint-disable-next-line no-unused-vars
+    playAudioSprite: function (key, spriteName, config)
+    {
+        return false;
+    },
+
+    remove: function (sound)
+    {
+        return BaseSoundManager.prototype.remove.call(this, sound);
+    },
+
+    removeByKey: function (key)
+    {
+        return BaseSoundManager.prototype.removeByKey.call(this, key);
+    },
+
+    pauseAll: NOOP,
+    resumeAll: NOOP,
+    stopAll: NOOP,
+    update: NOOP,
+    setRate: NOOP,
+    setDetune: NOOP,
+    setMute: NOOP,
+    setVolume: NOOP,
+
+    forEachActiveSound: function (callbackfn, scope)
+    {
+        BaseSoundManager.prototype.forEachActiveSound.call(this, callbackfn, scope);
+    },
+
+    destroy: function ()
+    {
+        BaseSoundManager.prototype.destroy.call(this);
+    }
+
+});
+
+module.exports = NoAudioSoundManager;
+
+
+/***/ }),
+/* 323 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @author       Pavle Goloskokovic <pgoloskokovic@gmail.com> (http://prunegames.com)
