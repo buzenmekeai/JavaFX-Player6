60f009782791eb8c15b5e42e402a883c4ebc3379
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-11-12 16:49:03 +0000

    woo

--- js/phaser.js
+++ js/phaser.js
@@ -83652,3 +83652,218 @@ module.exports = PluginManager;
 /***/ }),
 /* 332 */
 /***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Vector2 = __webpack_require__(3);
+
+/**
+ * Takes the `x` and `y` coordinates and transforms them into the same space as
+ * defined by the position, rotation and scale values.
+ *
+ * @function Phaser.Math.TransformXY
+ * @since 3.0.0
+ *
+ * @param {number} x - The x coordinate to be transformed.
+ * @param {number} y - The y coordinate to be transformed.
+ * @param {number} positionX - Horizontal position of the transform point.
+ * @param {number} positionY - Vertical position of the transform point.
+ * @param {number} rotation - Rotation of the transform point, in radians.
+ * @param {number} scaleX - Horizontal scale of the transform point.
+ * @param {number} scaleY - Vertical scale of the transform point.
+ * @param {(Phaser.Math.Vector2|Phaser.Geom.Point|object)} [output] - The output vector, point or object for the translated coordinates.
+ *
+ * @return {(Phaser.Math.Vector2|Phaser.Geom.Point|object)} The translated point.
+ */
+var TransformXY = function (x, y, positionX, positionY, rotation, scaleX, scaleY, output)
+{
+    if (output === undefined) { output = new Vector2(); }
+
+    var radianSin = Math.sin(rotation);
+    var radianCos = Math.cos(rotation);
+
+    // Rotate and Scale
+    var a = radianCos * scaleX;
+    var b = radianSin * scaleX;
+    var c = -radianSin * scaleY;
+    var d = radianCos * scaleY;
+
+    //  Invert
+    var id = 1 / ((a * d) + (c * -b));
+
+    output.x = (d * id * x) + (-c * id * y) + (((positionY * c) - (positionX * d)) * id);
+    output.y = (a * id * y) + (-b * id * x) + (((-positionY * a) + (positionX * b)) * id);
+
+    return output;
+};
+
+module.exports = TransformXY;
+
+
+/***/ }),
+/* 333 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Class = __webpack_require__(0);
+var NOOP = __webpack_require__(1);
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Touch_events
+// https://patrickhlauke.github.io/touch/tests/results/
+// https://www.html5rocks.com/en/mobile/touch/
+
+/**
+ * @classdesc
+ * The Touch Manager is a helper class that belongs to the Input Manager.
+ * 
+ * Its role is to listen for native DOM Touch Events and then pass them onto the Input Manager for further processing.
+ * 
+ * You do not need to create this class directly, the Input Manager will create an instance of it automatically.
+ *
+ * @class TouchManager
+ * @memberof Phaser.Input.Touch
+ * @constructor
+ * @since 3.0.0
+ *
+ * @param {Phaser.Input.InputManager} inputManager - A reference to the Input Manager.
+ */
+var TouchManager = new Class({
+
+    initialize:
+
+    function TouchManager (inputManager)
+    {
+        /**
+         * A reference to the Input Manager.
+         *
+         * @name Phaser.Input.Touch.TouchManager#manager
+         * @type {Phaser.Input.InputManager}
+         * @since 3.0.0
+         */
+        this.manager = inputManager;
+
+        /**
+         * If true the DOM events will have event.preventDefault applied to them, if false they will propagate fully.
+         *
+         * @name Phaser.Input.Touch.TouchManager#capture
+         * @type {boolean}
+         * @default true
+         * @since 3.0.0
+         */
+        this.capture = true;
+
+        /**
+         * A boolean that controls if the Touch Manager is enabled or not.
+         * Can be toggled on the fly.
+         *
+         * @name Phaser.Input.Touch.TouchManager#enabled
+         * @type {boolean}
+         * @default false
+         * @since 3.0.0
+         */
+        this.enabled = false;
+
+        /**
+         * The Touch Event target, as defined in the Game Config.
+         * Typically the canvas to which the game is rendering, but can be any interactive DOM element.
+         *
+         * @name Phaser.Input.Touch.TouchManager#target
+         * @type {any}
+         * @since 3.0.0
+         */
+        this.target;
+
+        /**
+         * The Touch Start event handler function.
+         * Initially empty and bound in the `startListeners` method.
+         *
+         * @name Phaser.Input.Touch.TouchManager#onTouchStart
+         * @type {function}
+         * @since 3.0.0
+         */
+        this.onTouchStart = NOOP;
+
+        /**
+         * The Touch Move event handler function.
+         * Initially empty and bound in the `startListeners` method.
+         *
+         * @name Phaser.Input.Touch.TouchManager#onTouchMove
+         * @type {function}
+         * @since 3.0.0
+         */
+        this.onTouchMove = NOOP;
+
+        /**
+         * The Touch End event handler function.
+         * Initially empty and bound in the `startListeners` method.
+         *
+         * @name Phaser.Input.Touch.TouchManager#onTouchEnd
+         * @type {function}
+         * @since 3.0.0
+         */
+        this.onTouchEnd = NOOP;
+
+        /**
+         * The Touch Cancel event handler function.
+         * Initially empty and bound in the `startListeners` method.
+         *
+         * @name Phaser.Input.Touch.TouchManager#onTouchCancel
+         * @type {function}
+         * @since 3.15.0
+         */
+        this.onTouchCancel = NOOP;
+
+        inputManager.events.once('boot', this.boot, this);
+    },
+
+    /**
+     * The Touch Manager boot process.
+     *
+     * @method Phaser.Input.Touch.TouchManager#boot
+     * @private
+     * @since 3.0.0
+     */
+    boot: function ()
+    {
+        var config = this.manager.config;
+
+        this.enabled = config.inputTouch;
+        this.target = config.inputTouchEventTarget;
+        this.capture = config.inputTouchCapture;
+
+        if (!this.target)
+        {
+            this.target = this.manager.game.canvas;
+        }
+
+        if (this.enabled && this.target)
+        {
+            this.startListeners();
+        }
+    },
+
+    /**
+     * Starts the Touch Event listeners running as long as an input target is set.
+     * 
+     * This method is called automatically if Touch Input is enabled in the game config,
+     * which it is by default. However, you can call it manually should you need to
+     * delay input capturing until later in the game.
+     *
+     * @method Phaser.Input.Touch.TouchManager#startListeners
+     * @since 3.0.0
+     */
+    startListeners: function ()
+    {
+        var _this = this;
+
+        this.onTouchStart = function (event)
+        {
