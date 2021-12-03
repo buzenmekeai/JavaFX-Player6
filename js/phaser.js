add5fd30d47e2df4f23f430d1204d42d2a0a77fd
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-12-03 07:14:17 +0000

    lab updates

--- js/phaser.js
+++ js/phaser.js
@@ -91364,3 +91364,237 @@ var BaseCache = new Class({
     {
         this.entries.clear();
         this.events.removeAllListeners();
+
+        this.entries = null;
+        this.events = null;
+    }
+
+});
+
+module.exports = BaseCache;
+
+
+/***/ }),
+/* 381 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Animation = __webpack_require__(384);
+var Class = __webpack_require__(0);
+var CustomMap = __webpack_require__(180);
+var EventEmitter = __webpack_require__(11);
+var GetValue = __webpack_require__(4);
+var Pad = __webpack_require__(179);
+
+/**
+ * @typedef {object} JSONAnimationManager
+ *
+ * @property {JSONAnimation[]} anims - [description]
+ * @property {number} globalTimeScale - [description]
+ */
+
+/**
+ * @classdesc
+ * The Animation Manager.
+ *
+ * Animations are managed by the global Animation Manager. This is a singleton class that is
+ * responsible for creating and delivering animations and their corresponding data to all Game Objects.
+ * Unlike plugins it is owned by the Game instance, not the Scene.
+ *
+ * Sprites and other Game Objects get the data they need from the AnimationManager.
+ *
+ * @class AnimationManager
+ * @extends Phaser.Events.EventEmitter
+ * @memberof Phaser.Animations
+ * @constructor
+ * @since 3.0.0
+ *
+ * @param {Phaser.Game} game - A reference to the Phaser.Game instance.
+ */
+var AnimationManager = new Class({
+
+    Extends: EventEmitter,
+
+    initialize:
+
+    function AnimationManager (game)
+    {
+        EventEmitter.call(this);
+
+        /**
+         * A reference to the Phaser.Game instance.
+         *
+         * @name Phaser.Animations.AnimationManager#game
+         * @type {Phaser.Game}
+         * @protected
+         * @since 3.0.0
+         */
+        this.game = game;
+
+        /**
+         * A reference to the Texture Manager.
+         *
+         * @name Phaser.Animations.AnimationManager#textureManager
+         * @type {Phaser.Textures.TextureManager}
+         * @protected
+         * @since 3.0.0
+         */
+        this.textureManager = null;
+
+        /**
+         * [description]
+         *
+         * @name Phaser.Animations.AnimationManager#globalTimeScale
+         * @type {number}
+         * @default 1
+         * @since 3.0.0
+         */
+        this.globalTimeScale = 1;
+
+        /**
+         * [description]
+         *
+         * @name Phaser.Animations.AnimationManager#anims
+         * @type {Phaser.Structs.Map.<string, Phaser.Animations.Animation>}
+         * @protected
+         * @since 3.0.0
+         */
+        this.anims = new CustomMap();
+
+        /**
+         * [description]
+         *
+         * @name Phaser.Animations.AnimationManager#paused
+         * @type {boolean}
+         * @default false
+         * @since 3.0.0
+         */
+        this.paused = false;
+
+        /**
+         * [description]
+         *
+         * @name Phaser.Animations.AnimationManager#name
+         * @type {string}
+         * @since 3.0.0
+         */
+        this.name = 'AnimationManager';
+
+        game.events.once('boot', this.boot, this);
+    },
+
+    /**
+     * [description]
+     *
+     * @method Phaser.Animations.AnimationManager#boot
+     * @since 3.0.0
+     */
+    boot: function ()
+    {
+        this.textureManager = this.game.textures;
+
+        this.game.events.once('destroy', this.destroy, this);
+    },
+
+    /**
+     * [description]
+     *
+     * @method Phaser.Animations.AnimationManager#add
+     * @fires AddAnimationEvent
+     * @since 3.0.0
+     *
+     * @param {string} key - [description]
+     * @param {Phaser.Animations.Animation} animation - [description]
+     *
+     * @return {Phaser.Animations.AnimationManager} This Animation Manager.
+     */
+    add: function (key, animation)
+    {
+        if (this.anims.has(key))
+        {
+            console.warn('Animation with key', key, 'already exists');
+            return;
+        }
+
+        animation.key = key;
+
+        this.anims.set(key, animation);
+
+        this.emit('add', key, animation);
+
+        return this;
+    },
+
+    /**
+     * [description]
+     *
+     * @method Phaser.Animations.AnimationManager#create
+     * @fires AddAnimationEvent
+     * @since 3.0.0
+     *
+     * @param {AnimationConfig} config - [description]
+     *
+     * @return {Phaser.Animations.Animation} The Animation that was created.
+     */
+    create: function (config)
+    {
+        var key = config.key;
+
+        if (!key || this.anims.has(key))
+        {
+            console.warn('Invalid Animation Key, or Key already in use: ' + key);
+            return;
+        }
+
+        var anim = new Animation(this, key, config);
+
+        this.anims.set(key, anim);
+
+        this.emit('add', key, anim);
+
+        return anim;
+    },
+
+    /**
+     * [description]
+     *
+     * @method Phaser.Animations.AnimationManager#fromJSON
+     * @since 3.0.0
+     *
+     * @param {(string|JSONAnimationManager|JSONAnimation)} data - [description]
+     * @param {boolean} [clearCurrentAnimations=false] - If set to `true`, the current animations will be removed (`anims.clear()`). If set to `false` (default), the animations in `data` will be added.
+     *
+     * @return {Phaser.Animations.Animation[]} An array containing all of the Animation objects that were created as a result of this call.
+     */
+    fromJSON: function (data, clearCurrentAnimations)
+    {
+        if (clearCurrentAnimations === undefined) { clearCurrentAnimations = false; }
+
+        if (clearCurrentAnimations)
+        {
+            this.anims.clear();
+        }
+
+        //  Do we have a String (i.e. from JSON, or an Object?)
+        if (typeof data === 'string')
+        {
+            data = JSON.parse(data);
+        }
+
+        var output = [];
+
+        //  Array of animations, or a single animation?
+        if (data.hasOwnProperty('anims') && Array.isArray(data.anims))
+        {
+            for (var i = 0; i < data.anims.length; i++)
+            {
+                output.push(this.create(data.anims[i]));
+            }
+
+            if (data.hasOwnProperty('globalTimeScale'))
+            {
