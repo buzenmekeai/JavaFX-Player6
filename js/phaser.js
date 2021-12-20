850b22fe9d4dd271e263592c749c7a087072dbfa
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-12-20 21:25:19 +0000

    //small fix

--- js/phaser.js
+++ js/phaser.js
@@ -96254,3 +96254,241 @@ module.exports = RightCenter;
 /***/ }),
 /* 410 */
 /***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetCenterY = __webpack_require__(72);
+var GetLeft = __webpack_require__(46);
+var SetCenterY = __webpack_require__(73);
+var SetLeft = __webpack_require__(45);
+
+/**
+ * Takes given Game Object and aligns it so that it is positioned in the left center of the other.
+ *
+ * @function Phaser.Display.Align.In.LeftCenter
+ * @since 3.0.0
+ *
+ * @generic {Phaser.GameObjects.GameObject} G - [gameObject,$return]
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object that will be positioned.
+ * @param {Phaser.GameObjects.GameObject} alignIn - The Game Object to base the alignment position on.
+ * @param {number} [offsetX=0] - Optional horizontal offset from the position.
+ * @param {number} [offsetY=0] - Optional vertical offset from the position.
+ *
+ * @return {Phaser.GameObjects.GameObject} The Game Object that was aligned.
+ */
+var LeftCenter = function (gameObject, alignIn, offsetX, offsetY)
+{
+    if (offsetX === undefined) { offsetX = 0; }
+    if (offsetY === undefined) { offsetY = 0; }
+
+    SetLeft(gameObject, GetLeft(alignIn) - offsetX);
+    SetCenterY(gameObject, GetCenterY(alignIn) + offsetY);
+
+    return gameObject;
+};
+
+module.exports = LeftCenter;
+
+
+/***/ }),
+/* 411 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var SetCenterX = __webpack_require__(74);
+var SetCenterY = __webpack_require__(73);
+
+/**
+ * Positions the Game Object so that it is centered on the given coordinates.
+ *
+ * @function Phaser.Display.Bounds.CenterOn
+ * @since 3.0.0
+ *
+ * @generic {Phaser.GameObjects.GameObject} G - [gameObject,$return]
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object that will be re-positioned.
+ * @param {number} x - The horizontal coordinate to position the Game Object on.
+ * @param {number} y - The vertical coordinate to position the Game Object on.
+ *
+ * @return {Phaser.GameObjects.GameObject} The Game Object that was positioned.
+ */
+var CenterOn = function (gameObject, x, y)
+{
+    SetCenterX(gameObject, x);
+
+    return SetCenterY(gameObject, y);
+};
+
+module.exports = CenterOn;
+
+
+/***/ }),
+/* 412 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var CenterOn = __webpack_require__(411);
+var GetCenterX = __webpack_require__(75);
+var GetCenterY = __webpack_require__(72);
+
+/**
+ * Takes given Game Object and aligns it so that it is positioned in the center of the other.
+ *
+ * @function Phaser.Display.Align.In.Center
+ * @since 3.0.0
+ *
+ * @generic {Phaser.GameObjects.GameObject} G - [gameObject,$return]
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object that will be positioned.
+ * @param {Phaser.GameObjects.GameObject} alignIn - The Game Object to base the alignment position on.
+ * @param {number} [offsetX=0] - Optional horizontal offset from the position.
+ * @param {number} [offsetY=0] - Optional vertical offset from the position.
+ *
+ * @return {Phaser.GameObjects.GameObject} The Game Object that was aligned.
+ */
+var Center = function (gameObject, alignIn, offsetX, offsetY)
+{
+    if (offsetX === undefined) { offsetX = 0; }
+    if (offsetY === undefined) { offsetY = 0; }
+
+    CenterOn(gameObject, GetCenterX(alignIn) + offsetX, GetCenterY(alignIn) + offsetY);
+
+    return gameObject;
+};
+
+module.exports = Center;
+
+
+/***/ }),
+/* 413 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetBottom = __webpack_require__(48);
+var GetRight = __webpack_require__(44);
+var SetBottom = __webpack_require__(47);
+var SetRight = __webpack_require__(43);
+
+/**
+ * Takes given Game Object and aligns it so that it is positioned in the bottom right of the other.
+ *
+ * @function Phaser.Display.Align.In.BottomRight
+ * @since 3.0.0
+ *
+ * @generic {Phaser.GameObjects.GameObject} G - [gameObject,$return]
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object that will be positioned.
+ * @param {Phaser.GameObjects.GameObject} alignIn - The Game Object to base the alignment position on.
+ * @param {number} [offsetX=0] - Optional horizontal offset from the position.
+ * @param {number} [offsetY=0] - Optional vertical offset from the position.
+ *
+ * @return {Phaser.GameObjects.GameObject} The Game Object that was aligned.
+ */
+var BottomRight = function (gameObject, alignIn, offsetX, offsetY)
+{
+    if (offsetX === undefined) { offsetX = 0; }
+    if (offsetY === undefined) { offsetY = 0; }
+
+    SetRight(gameObject, GetRight(alignIn) + offsetX);
+    SetBottom(gameObject, GetBottom(alignIn) + offsetY);
+
+    return gameObject;
+};
+
+module.exports = BottomRight;
+
+
+/***/ }),
+/* 414 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetBottom = __webpack_require__(48);
+var GetLeft = __webpack_require__(46);
+var SetBottom = __webpack_require__(47);
+var SetLeft = __webpack_require__(45);
+
+/**
+ * Takes given Game Object and aligns it so that it is positioned in the bottom left of the other.
+ *
+ * @function Phaser.Display.Align.In.BottomLeft
+ * @since 3.0.0
+ *
+ * @generic {Phaser.GameObjects.GameObject} G - [gameObject,$return]
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object that will be positioned.
+ * @param {Phaser.GameObjects.GameObject} alignIn - The Game Object to base the alignment position on.
+ * @param {number} [offsetX=0] - Optional horizontal offset from the position.
+ * @param {number} [offsetY=0] - Optional vertical offset from the position.
+ *
+ * @return {Phaser.GameObjects.GameObject} The Game Object that was aligned.
+ */
+var BottomLeft = function (gameObject, alignIn, offsetX, offsetY)
+{
+    if (offsetX === undefined) { offsetX = 0; }
+    if (offsetY === undefined) { offsetY = 0; }
+
+    SetLeft(gameObject, GetLeft(alignIn) - offsetX);
+    SetBottom(gameObject, GetBottom(alignIn) + offsetY);
+
+    return gameObject;
+};
+
+module.exports = BottomLeft;
+
+
+/***/ }),
+/* 415 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetBottom = __webpack_require__(48);
+var GetCenterX = __webpack_require__(75);
+var SetBottom = __webpack_require__(47);
+var SetCenterX = __webpack_require__(74);
+
+/**
+ * Takes given Game Object and aligns it so that it is positioned in the bottom center of the other.
+ *
+ * @function Phaser.Display.Align.In.BottomCenter
+ * @since 3.0.0
+ *
+ * @generic {Phaser.GameObjects.GameObject} G - [gameObject,$return]
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object that will be positioned.
+ * @param {Phaser.GameObjects.GameObject} alignIn - The Game Object to base the alignment position on.
+ * @param {number} [offsetX=0] - Optional horizontal offset from the position.
+ * @param {number} [offsetY=0] - Optional vertical offset from the position.
+ *
+ * @return {Phaser.GameObjects.GameObject} The Game Object that was aligned.
+ */
