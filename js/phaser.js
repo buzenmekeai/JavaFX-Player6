b9994d31c57e3801cc1dacce95169db78d00b9c7
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2021-12-20 13:18:39 +0000

    pyCMBS changes ==============

--- js/phaser.js
+++ js/phaser.js
@@ -96041,3 +96041,216 @@ module.exports = RandomDataGenerator;
  * @copyright    2018 Photon Storm Ltd.
  * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
  */
+
+var CircumferencePoint = __webpack_require__(192);
+var FromPercent = __webpack_require__(93);
+var MATH_CONST = __webpack_require__(16);
+var Point = __webpack_require__(6);
+
+/**
+ * Returns a Point object containing the coordinates of a point on the circumference of the Circle
+ * based on the given angle normalized to the range 0 to 1. I.e. a value of 0.5 will give the point
+ * at 180 degrees around the circle.
+ *
+ * @function Phaser.Geom.Circle.GetPoint
+ * @since 3.0.0
+ *
+ * @generic {Phaser.Geom.Point} O - [out,$return]
+ *
+ * @param {Phaser.Geom.Circle} circle - The Circle to get the circumference point on.
+ * @param {number} position - A value between 0 and 1, where 0 equals 0 degrees, 0.5 equals 180 degrees and 1 equals 360 around the circle.
+ * @param {(Phaser.Geom.Point|object)} [out] - An object to store the return values in. If not given a Point object will be created.
+ *
+ * @return {(Phaser.Geom.Point|object)} A Point, or point-like object, containing the coordinates of the point around the circle.
+ */
+var GetPoint = function (circle, position, out)
+{
+    if (out === undefined) { out = new Point(); }
+
+    var angle = FromPercent(position, 0, MATH_CONST.PI2);
+
+    return CircumferencePoint(circle, angle, out);
+};
+
+module.exports = GetPoint;
+
+
+/***/ }),
+/* 406 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetRight = __webpack_require__(44);
+var GetTop = __webpack_require__(42);
+var SetRight = __webpack_require__(43);
+var SetTop = __webpack_require__(41);
+
+/**
+ * Takes given Game Object and aligns it so that it is positioned in the top right of the other.
+ *
+ * @function Phaser.Display.Align.In.TopRight
+ * @since 3.0.0
+ *
+ * @generic {Phaser.GameObjects.GameObject} G - [gameObject,$return]
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object that will be positioned.
+ * @param {Phaser.GameObjects.GameObject} alignIn - The Game Object to base the alignment position on.
+ * @param {number} [offsetX=0] - Optional horizontal offset from the position.
+ * @param {number} [offsetY=0] - Optional vertical offset from the position.
+ *
+ * @return {Phaser.GameObjects.GameObject} The Game Object that was aligned.
+ */
+var TopRight = function (gameObject, alignIn, offsetX, offsetY)
+{
+    if (offsetX === undefined) { offsetX = 0; }
+    if (offsetY === undefined) { offsetY = 0; }
+
+    SetRight(gameObject, GetRight(alignIn) + offsetX);
+    SetTop(gameObject, GetTop(alignIn) - offsetY);
+
+    return gameObject;
+};
+
+module.exports = TopRight;
+
+
+/***/ }),
+/* 407 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetLeft = __webpack_require__(46);
+var GetTop = __webpack_require__(42);
+var SetLeft = __webpack_require__(45);
+var SetTop = __webpack_require__(41);
+
+/**
+ * Takes given Game Object and aligns it so that it is positioned in the top left of the other.
+ *
+ * @function Phaser.Display.Align.In.TopLeft
+ * @since 3.0.0
+ *
+ * @generic {Phaser.GameObjects.GameObject} G - [gameObject,$return]
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object that will be positioned.
+ * @param {Phaser.GameObjects.GameObject} alignIn - The Game Object to base the alignment position on.
+ * @param {number} [offsetX=0] - Optional horizontal offset from the position.
+ * @param {number} [offsetY=0] - Optional vertical offset from the position.
+ *
+ * @return {Phaser.GameObjects.GameObject} The Game Object that was aligned.
+ */
+var TopLeft = function (gameObject, alignIn, offsetX, offsetY)
+{
+    if (offsetX === undefined) { offsetX = 0; }
+    if (offsetY === undefined) { offsetY = 0; }
+
+    SetLeft(gameObject, GetLeft(alignIn) - offsetX);
+    SetTop(gameObject, GetTop(alignIn) - offsetY);
+
+    return gameObject;
+};
+
+module.exports = TopLeft;
+
+
+/***/ }),
+/* 408 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetCenterX = __webpack_require__(75);
+var GetTop = __webpack_require__(42);
+var SetCenterX = __webpack_require__(74);
+var SetTop = __webpack_require__(41);
+
+/**
+ * Takes given Game Object and aligns it so that it is positioned in the top center of the other.
+ *
+ * @function Phaser.Display.Align.In.TopCenter
+ * @since 3.0.0
+ *
+ * @generic {Phaser.GameObjects.GameObject} G - [gameObject,$return]
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object that will be positioned.
+ * @param {Phaser.GameObjects.GameObject} alignIn - The Game Object to base the alignment position on.
+ * @param {number} [offsetX=0] - Optional horizontal offset from the position.
+ * @param {number} [offsetY=0] - Optional vertical offset from the position.
+ *
+ * @return {Phaser.GameObjects.GameObject} The Game Object that was aligned.
+ */
+var TopCenter = function (gameObject, alignIn, offsetX, offsetY)
+{
+    if (offsetX === undefined) { offsetX = 0; }
+    if (offsetY === undefined) { offsetY = 0; }
+
+    SetCenterX(gameObject, GetCenterX(alignIn) + offsetX);
+    SetTop(gameObject, GetTop(alignIn) - offsetY);
+
+    return gameObject;
+};
+
+module.exports = TopCenter;
+
+
+/***/ }),
+/* 409 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetCenterY = __webpack_require__(72);
+var GetRight = __webpack_require__(44);
+var SetCenterY = __webpack_require__(73);
+var SetRight = __webpack_require__(43);
+
+/**
+ * Takes given Game Object and aligns it so that it is positioned in the right center of the other.
+ *
+ * @function Phaser.Display.Align.In.RightCenter
+ * @since 3.0.0
+ *
+ * @generic {Phaser.GameObjects.GameObject} G - [gameObject,$return]
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object that will be positioned.
+ * @param {Phaser.GameObjects.GameObject} alignIn - The Game Object to base the alignment position on.
+ * @param {number} [offsetX=0] - Optional horizontal offset from the position.
+ * @param {number} [offsetY=0] - Optional vertical offset from the position.
+ *
+ * @return {Phaser.GameObjects.GameObject} The Game Object that was aligned.
+ */
+var RightCenter = function (gameObject, alignIn, offsetX, offsetY)
+{
+    if (offsetX === undefined) { offsetX = 0; }
+    if (offsetY === undefined) { offsetY = 0; }
+
+    SetRight(gameObject, GetRight(alignIn) + offsetX);
+    SetCenterY(gameObject, GetCenterY(alignIn) + offsetY);
+
+    return gameObject;
+};
+
+module.exports = RightCenter;
+
+
+/***/ }),
+/* 410 */
+/***/ (function(module, exports, __webpack_require__) {
