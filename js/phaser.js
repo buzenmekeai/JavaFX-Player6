1ca0ef44ff7625fc6216c2fef8b6bcdd1b44e58a
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2022-02-09 17:41:09 +0000

    zram: support REQ_DISCARD

--- js/phaser.js
+++ js/phaser.js
@@ -106190,3 +106190,214 @@ var CullTiles = function (layer, camera, outputArray, renderOrder)
     }
     else if (renderOrder === 2)
     {
+        //  right-up
+
+        for (y = drawBottom; y >= drawTop; y--)
+        {
+            for (x = drawLeft; x < drawRight; x++)
+            {
+                tile = mapData[y][x];
+    
+                if (!tile || tile.index === -1 || !tile.visible || tile.alpha === 0)
+                {
+                    continue;
+                }
+    
+                outputArray.push(tile);
+            }
+        }
+    }
+    else if (renderOrder === 3)
+    {
+        //  left-up
+
+        for (y = drawBottom; y >= drawTop; y--)
+        {
+            for (x = drawRight; x >= drawLeft; x--)
+            {
+                tile = mapData[y][x];
+    
+                if (!tile || tile.index === -1 || !tile.visible || tile.alpha === 0)
+                {
+                    continue;
+                }
+    
+                outputArray.push(tile);
+            }
+        }
+    }
+
+    tilemapLayer.tilesDrawn = outputArray.length;
+    tilemapLayer.tilesTotal = mapWidth * mapHeight;
+
+    return outputArray;
+};
+
+module.exports = CullTiles;
+
+
+/***/ }),
+/* 488 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var TileToWorldX = __webpack_require__(101);
+var TileToWorldY = __webpack_require__(100);
+var GetTilesWithin = __webpack_require__(17);
+var ReplaceByIndex = __webpack_require__(220);
+
+/**
+ * Creates a Sprite for every object matching the given tile indexes in the layer. You can
+ * optionally specify if each tile will be replaced with a new tile after the Sprite has been
+ * created. This is useful if you want to lay down special tiles in a level that are converted to
+ * Sprites, but want to replace the tile itself with a floor tile or similar once converted.
+ *
+ * @function Phaser.Tilemaps.Components.CreateFromTiles
+ * @private
+ * @since 3.0.0
+ *
+ * @param {(integer|array)} indexes - The tile index, or array of indexes, to create Sprites from.
+ * @param {(integer|array)} replacements - The tile index, or array of indexes, to change a converted tile to. Set to `null` to leave the tiles unchanged. If an array is given, it is assumed to be a one-to-one mapping with the indexes array.
+ * @param {SpriteConfig} spriteConfig - The config object to pass into the Sprite creator (i.e. scene.make.sprite).
+ * @param {Phaser.Scene} [scene=scene the map is within] - The Scene to create the Sprites within.
+ * @param {Phaser.Cameras.Scene2D.Camera} [camera=main camera] - The Camera to use when determining the world XY
+ * @param {Phaser.Tilemaps.LayerData} layer - The Tilemap Layer to act upon.
+ *
+ * @return {Phaser.GameObjects.Sprite[]} An array of the Sprites that were created.
+ */
+var CreateFromTiles = function (indexes, replacements, spriteConfig, scene, camera, layer)
+{
+    if (spriteConfig === undefined) { spriteConfig = {}; }
+
+    if (!Array.isArray(indexes)) { indexes = [ indexes ]; }
+
+    var tilemapLayer = layer.tilemapLayer;
+    if (scene === undefined) { scene = tilemapLayer.scene; }
+    if (camera === undefined) { camera = scene.cameras.main; }
+
+    var tiles = GetTilesWithin(0, 0, layer.width, layer.height, null, layer);
+    var sprites = [];
+    var i;
+
+    for (i = 0; i < tiles.length; i++)
+    {
+        var tile = tiles[i];
+
+        if (indexes.indexOf(tile.index) !== -1)
+        {
+            spriteConfig.x = TileToWorldX(tile.x, camera, layer);
+            spriteConfig.y = TileToWorldY(tile.y, camera, layer);
+
+            var sprite = scene.make.sprite(spriteConfig);
+            sprites.push(sprite);
+        }
+    }
+
+    if (typeof replacements === 'number')
+    {
+        //  Assume 1 replacement for all types of tile given
+        for (i = 0; i < indexes.length; i++)
+        {
+            ReplaceByIndex(indexes[i], replacements, 0, 0, layer.width, layer.height, layer);
+        }
+    }
+    else if (Array.isArray(replacements))
+    {
+        //  Assume 1 to 1 mapping with indexes array
+        for (i = 0; i < indexes.length; i++)
+        {
+            ReplaceByIndex(indexes[i], replacements[i], 0, 0, layer.width, layer.height, layer);
+        }
+    }
+
+    return sprites;
+};
+
+module.exports = CreateFromTiles;
+
+
+/***/ }),
+/* 489 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetTilesWithin = __webpack_require__(17);
+var CalculateFacesWithin = __webpack_require__(34);
+
+/**
+ * Copies the tiles in the source rectangular area to a new destination (all specified in tile
+ * coordinates) within the layer. This copies all tile properties & recalculates collision
+ * information in the destination region.
+ *
+ * @function Phaser.Tilemaps.Components.Copy
+ * @private
+ * @since 3.0.0
+ *
+ * @param {integer} srcTileX - The x coordinate of the area to copy from, in tiles, not pixels.
+ * @param {integer} srcTileY - The y coordinate of the area to copy from, in tiles, not pixels.
+ * @param {integer} width - The width of the area to copy, in tiles, not pixels.
+ * @param {integer} height - The height of the area to copy, in tiles, not pixels.
+ * @param {integer} destTileX - The x coordinate of the area to copy to, in tiles, not pixels.
+ * @param {integer} destTileY - The y coordinate of the area to copy to, in tiles, not pixels.
+ * @param {boolean} [recalculateFaces=true] - `true` if the faces data should be recalculated.
+ * @param {Phaser.Tilemaps.LayerData} layer - The Tilemap Layer to act upon.
+ */
+var Copy = function (srcTileX, srcTileY, width, height, destTileX, destTileY, recalculateFaces, layer)
+{
+    if (srcTileX < 0) { srcTileX = 0; }
+    if (srcTileY < 0) { srcTileY = 0; }
+    if (recalculateFaces === undefined) { recalculateFaces = true; }
+
+    var srcTiles = GetTilesWithin(srcTileX, srcTileY, width, height, null, layer);
+
+    var offsetX = destTileX - srcTileX;
+    var offsetY = destTileY - srcTileY;
+
+    for (var i = 0; i < srcTiles.length; i++)
+    {
+        var tileX = srcTiles[i].x + offsetX;
+        var tileY = srcTiles[i].y + offsetY;
+        if (tileX >= 0 && tileX < layer.width && tileY >= 0 && tileY < layer.height)
+        {
+            if (layer.data[tileY][tileX])
+            {
+                layer.data[tileY][tileX].copy(srcTiles[i]);
+            }
+        }
+    }
+
+    if (recalculateFaces)
+    {
+        // Recalculate the faces within the destination area and neighboring tiles
+        CalculateFacesWithin(destTileX - 1, destTileY - 1, width + 2, height + 2, layer);
+    }
+};
+
+module.exports = Copy;
+
+
+/***/ }),
+/* 490 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+/**
+ * @namespace Phaser.Tilemaps
+ */
+
+module.exports = {
