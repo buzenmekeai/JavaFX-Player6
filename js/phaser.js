7901f48eb8d1714cb304390a204f69bc01a0f9b0
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2022-02-23 20:14:41 +0000

    sg_write()/bsg_write() is not fit to be called under KERNEL_DS

--- js/phaser.js
+++ js/phaser.js
@@ -109061,3 +109061,209 @@ var Common = __webpack_require__(33);
             axis.x = xx;
         }
     };
+
+})();
+
+
+/***/ }),
+/* 506 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+/**
+ * @namespace Phaser.Physics.Impact.Components
+ */
+
+module.exports = {
+
+    Acceleration: __webpack_require__(1121),
+    BodyScale: __webpack_require__(1120),
+    BodyType: __webpack_require__(1119),
+    Bounce: __webpack_require__(1118),
+    CheckAgainst: __webpack_require__(1117),
+    Collides: __webpack_require__(1116),
+    Debug: __webpack_require__(1115),
+    Friction: __webpack_require__(1114),
+    Gravity: __webpack_require__(1113),
+    Offset: __webpack_require__(1112),
+    SetGameObject: __webpack_require__(1111),
+    Velocity: __webpack_require__(1110)
+
+};
+
+
+/***/ }),
+/* 507 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetOverlapY = __webpack_require__(229);
+
+/**
+ * [description]
+ *
+ * @function Phaser.Physics.Arcade.SeparateY
+ * @since 3.0.0
+ *
+ * @param {Phaser.Physics.Arcade.Body} body1 - [description]
+ * @param {Phaser.Physics.Arcade.Body} body2 - [description]
+ * @param {boolean} overlapOnly - [description]
+ * @param {number} bias - [description]
+ *
+ * @return {boolean} [description]
+ */
+var SeparateY = function (body1, body2, overlapOnly, bias)
+{
+    var overlap = GetOverlapY(body1, body2, overlapOnly, bias);
+
+    //  Can't separate two immovable bodies, or a body with its own custom separation logic
+    if (overlapOnly || overlap === 0 || (body1.immovable && body2.immovable) || body1.customSeparateY || body2.customSeparateY)
+    {
+        //  return true if there was some overlap, otherwise false
+        return (overlap !== 0) || (body1.embedded && body2.embedded);
+    }
+
+    //  Adjust their positions and velocities accordingly (if there was any overlap)
+    var v1 = body1.velocity.y;
+    var v2 = body2.velocity.y;
+
+    if (!body1.immovable && !body2.immovable)
+    {
+        overlap *= 0.5;
+
+        body1.y -= overlap;
+        body2.y += overlap;
+
+        var nv1 = Math.sqrt((v2 * v2 * body2.mass) / body1.mass) * ((v2 > 0) ? 1 : -1);
+        var nv2 = Math.sqrt((v1 * v1 * body1.mass) / body2.mass) * ((v1 > 0) ? 1 : -1);
+        var avg = (nv1 + nv2) * 0.5;
+
+        nv1 -= avg;
+        nv2 -= avg;
+
+        body1.velocity.y = avg + nv1 * body1.bounce.y;
+        body2.velocity.y = avg + nv2 * body2.bounce.y;
+    }
+    else if (!body1.immovable)
+    {
+        body1.y -= overlap;
+        body1.velocity.y = v2 - v1 * body1.bounce.y;
+
+        //  This is special case code that handles things like horizontal moving platforms you can ride
+        if (body2.moves)
+        {
+            body1.x += (body2.x - body2.prev.x) * body2.friction.x;
+        }
+    }
+    else
+    {
+        body2.y += overlap;
+        body2.velocity.y = v1 - v2 * body2.bounce.y;
+
+        //  This is special case code that handles things like horizontal moving platforms you can ride
+        if (body1.moves)
+        {
+            body2.x += (body1.x - body1.prev.x) * body1.friction.x;
+        }
+    }
+
+    //  If we got this far then there WAS overlap, and separation is complete, so return true
+    return true;
+};
+
+module.exports = SeparateY;
+
+
+/***/ }),
+/* 508 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var GetOverlapX = __webpack_require__(230);
+
+/**
+ * [description]
+ *
+ * @function Phaser.Physics.Arcade.SeparateX
+ * @since 3.0.0
+ *
+ * @param {Phaser.Physics.Arcade.Body} body1 - [description]
+ * @param {Phaser.Physics.Arcade.Body} body2 - [description]
+ * @param {boolean} overlapOnly - [description]
+ * @param {number} bias - [description]
+ *
+ * @return {boolean} [description]
+ */
+var SeparateX = function (body1, body2, overlapOnly, bias)
+{
+    var overlap = GetOverlapX(body1, body2, overlapOnly, bias);
+
+    //  Can't separate two immovable bodies, or a body with its own custom separation logic
+    if (overlapOnly || overlap === 0 || (body1.immovable && body2.immovable) || body1.customSeparateX || body2.customSeparateX)
+    {
+        //  return true if there was some overlap, otherwise false
+        return (overlap !== 0) || (body1.embedded && body2.embedded);
+    }
+
+    //  Adjust their positions and velocities accordingly (if there was any overlap)
+    var v1 = body1.velocity.x;
+    var v2 = body2.velocity.x;
+
+    if (!body1.immovable && !body2.immovable)
+    {
+        overlap *= 0.5;
+
+        body1.x -= overlap;
+        body2.x += overlap;
+
+        var nv1 = Math.sqrt((v2 * v2 * body2.mass) / body1.mass) * ((v2 > 0) ? 1 : -1);
+        var nv2 = Math.sqrt((v1 * v1 * body1.mass) / body2.mass) * ((v1 > 0) ? 1 : -1);
+        var avg = (nv1 + nv2) * 0.5;
+
+        nv1 -= avg;
+        nv2 -= avg;
+
+        body1.velocity.x = avg + nv1 * body1.bounce.x;
+        body2.velocity.x = avg + nv2 * body2.bounce.x;
+    }
+    else if (!body1.immovable)
+    {
+        body1.x -= overlap;
+        body1.velocity.x = v2 - v1 * body1.bounce.x;
+
+        //  This is special case code that handles things like vertically moving platforms you can ride
+        if (body2.moves)
+        {
+            body1.y += (body2.y - body2.prev.y) * body2.friction.y;
+        }
+    }
+    else
+    {
+        body2.x += overlap;
+        body2.velocity.x = v1 - v2 * body2.bounce.x;
+
+        //  This is special case code that handles things like vertically moving platforms you can ride
+        if (body1.moves)
+        {
+            body2.y += (body1.y - body1.prev.y) * body1.friction.y;
+        }
+    }
+
+    //  If we got this far then there WAS overlap, and separation is complete, so return true
+    return true;
+};
