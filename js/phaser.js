fb3040af12d26e2473d8435c2a001a2a94377d21
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2022-03-03 16:09:06 +0000

    Update ffmpeg

--- js/phaser.js
+++ js/phaser.js
@@ -110979,3 +110979,217 @@ var ArcadePhysics = new Class({
                 closest = target;
                 min = distance;
             }
+        }
+
+        return closest;
+    },
+
+    /**
+     * Finds the Body farthest from a source point or object.
+     *
+     * @method Phaser.Physics.Arcade.ArcadePhysics#furthest
+     * @since 3.0.0
+     *
+     * @param {object} source - Any object with public `x` and `y` properties, such as a Game Object or Geometry object.
+     *
+     * @return {Phaser.Physics.Arcade.Body} The Body furthest from the given source point.
+     */
+    furthest: function (source)
+    {
+        var bodies = this.world.tree.all();
+
+        var max = -1;
+        var farthest = null;
+        var x = source.x;
+        var y = source.y;
+
+        for (var i = bodies.length - 1; i >= 0; i--)
+        {
+            var target = bodies[i];
+            var distance = DistanceSquared(x, y, target.x, target.y);
+
+            if (distance > max)
+            {
+                farthest = target;
+                max = distance;
+            }
+        }
+
+        return farthest;
+    },
+
+    /**
+     * Move the given display object towards the x/y coordinates at a steady velocity.
+     * If you specify a maxTime then it will adjust the speed (over-writing what you set) so it arrives at the destination in that number of seconds.
+     * Timings are approximate due to the way browser timers work. Allow for a variance of +- 50ms.
+     * Note: The display object does not continuously track the target. If the target changes location during transit the display object will not modify its course.
+     * Note: The display object doesn't stop moving once it reaches the destination coordinates.
+     * Note: Doesn't take into account acceleration, maxVelocity or drag (if you've set drag or acceleration too high this object may not move at all)
+     *
+     * @method Phaser.Physics.Arcade.ArcadePhysics#moveTo
+     * @since 3.0.0
+     *
+     * @param {Phaser.GameObjects.GameObject} gameObject - Any Game Object with an Arcade Physics body.
+     * @param {number} x - The x coordinate to move towards.
+     * @param {number} y - The y coordinate to move towards.
+     * @param {number} [speed=60] - The speed it will move, in pixels per second (default is 60 pixels/sec)
+     * @param {number} [maxTime=0] - Time given in milliseconds (1000 = 1 sec). If set the speed is adjusted so the object will arrive at destination in the given number of ms.
+     *
+     * @return {number} The angle (in radians) that the object should be visually set to in order to match its new velocity.
+     */
+    moveTo: function (gameObject, x, y, speed, maxTime)
+    {
+        if (speed === undefined) { speed = 60; }
+        if (maxTime === undefined) { maxTime = 0; }
+
+        var angle = Math.atan2(y - gameObject.y, x - gameObject.x);
+
+        if (maxTime > 0)
+        {
+            //  We know how many pixels we need to move, but how fast?
+            speed = DistanceBetween(gameObject.x, gameObject.y, x, y) / (maxTime / 1000);
+        }
+
+        gameObject.body.velocity.setToPolar(angle, speed);
+
+        return angle;
+    },
+
+    /**
+     * Move the given display object towards the destination object at a steady velocity.
+     * If you specify a maxTime then it will adjust the speed (overwriting what you set) so it arrives at the destination in that number of seconds.
+     * Timings are approximate due to the way browser timers work. Allow for a variance of +- 50ms.
+     * Note: The display object does not continuously track the target. If the target changes location during transit the display object will not modify its course.
+     * Note: The display object doesn't stop moving once it reaches the destination coordinates.
+     * Note: Doesn't take into account acceleration, maxVelocity or drag (if you've set drag or acceleration too high this object may not move at all)
+     *
+     * @method Phaser.Physics.Arcade.ArcadePhysics#moveToObject
+     * @since 3.0.0
+     *
+     * @param {Phaser.GameObjects.GameObject} gameObject - Any Game Object with an Arcade Physics body.
+     * @param {object} destination - Any object with public `x` and `y` properties, such as a Game Object or Geometry object.
+     * @param {number} [speed=60] - The speed it will move, in pixels per second (default is 60 pixels/sec)
+     * @param {number} [maxTime=0] - Time given in milliseconds (1000 = 1 sec). If set the speed is adjusted so the object will arrive at destination in the given number of ms.
+     *
+     * @return {number} The angle (in radians) that the object should be visually set to in order to match its new velocity.
+     */
+    moveToObject: function (gameObject, destination, speed, maxTime)
+    {
+        return this.moveTo(gameObject, destination.x, destination.y, speed, maxTime);
+    },
+
+    /**
+     * Given the angle (in degrees) and speed calculate the velocity and return it as a vector, or set it to the given vector object.
+     * One way to use this is: velocityFromAngle(angle, 200, sprite.body.velocity) which will set the values directly to the sprite's velocity and not create a new vector object.
+     *
+     * @method Phaser.Physics.Arcade.ArcadePhysics#velocityFromAngle
+     * @since 3.0.0
+     *
+     * @param {number} angle - The angle in degrees calculated in clockwise positive direction (down = 90 degrees positive, right = 0 degrees positive, up = 90 degrees negative)
+     * @param {number} [speed=60] - The speed it will move, in pixels per second squared.
+     * @param {Phaser.Math.Vector2} [vec2] - The Vector2 in which the x and y properties will be set to the calculated velocity.
+     *
+     * @return {Phaser.Math.Vector2} The Vector2 that stores the velocity.
+     */
+    velocityFromAngle: function (angle, speed, vec2)
+    {
+        if (speed === undefined) { speed = 60; }
+        if (vec2 === undefined) { vec2 = new Vector2(); }
+
+        return vec2.setToPolar(DegToRad(angle), speed);
+    },
+
+    /**
+     * Given the rotation (in radians) and speed calculate the velocity and return it as a vector, or set it to the given vector object.
+     * One way to use this is: velocityFromRotation(rotation, 200, sprite.body.velocity) which will set the values directly to the sprite's velocity and not create a new vector object.
+     *
+     * @method Phaser.Physics.Arcade.ArcadePhysics#velocityFromRotation
+     * @since 3.0.0
+     *
+     * @param {number} rotation - The angle in radians.
+     * @param {number} [speed=60] - The speed it will move, in pixels per second squared
+     * @param {Phaser.Math.Vector2} [vec2] - The Vector2 in which the x and y properties will be set to the calculated velocity.
+     *
+     * @return {Phaser.Math.Vector2} The Vector2 that stores the velocity.
+     */
+    velocityFromRotation: function (rotation, speed, vec2)
+    {
+        if (speed === undefined) { speed = 60; }
+        if (vec2 === undefined) { vec2 = new Vector2(); }
+
+        return vec2.setToPolar(rotation, speed);
+    },
+
+    /**
+     * The Scene that owns this plugin is shutting down.
+     * We need to kill and reset all internal properties as well as stop listening to Scene events.
+     *
+     * @method Phaser.Physics.Arcade.ArcadePhysics#shutdown
+     * @since 3.0.0
+     */
+    shutdown: function ()
+    {
+        if (!this.world)
+        {
+            //  Already destroyed
+            return;
+        }
+
+        var eventEmitter = this.systems.events;
+
+        eventEmitter.off('update', this.world.update, this.world);
+        eventEmitter.off('postupdate', this.world.postUpdate, this.world);
+        eventEmitter.off('shutdown', this.shutdown, this);
+
+        this.add.destroy();
+        this.world.destroy();
+
+        this.add = null;
+        this.world = null;
+    },
+
+    /**
+     * The Scene that owns this plugin is being destroyed.
+     * We need to shutdown and then kill off all external references.
+     *
+     * @method Phaser.Physics.Arcade.ArcadePhysics#destroy
+     * @since 3.0.0
+     */
+    destroy: function ()
+    {
+        this.shutdown();
+
+        this.scene.sys.events.off('start', this.start, this);
+
+        this.scene = null;
+        this.systems = null;
+    }
+
+});
+
+PluginCache.register('ArcadePhysics', ArcadePhysics, 'arcadePhysics');
+
+module.exports = ArcadePhysics;
+
+
+/***/ }),
+/* 528 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var CONST = __webpack_require__(35);
+var Extend = __webpack_require__(20);
+
+/**
+ * @callback ArcadePhysicsCallback
+ *
+ * @param {Phaser.GameObjects.GameObject} object1 - [description]
+ * @param {Phaser.GameObjects.GameObject} object2 - [description]
+ */
+
+/**
