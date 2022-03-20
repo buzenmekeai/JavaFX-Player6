878f81aa1fe8e1c738e846b0530dddd69c944173
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2022-03-20 17:00:38 +0000

    better syntax

--- js/phaser.js
+++ js/phaser.js
@@ -115408,3 +115408,244 @@ var SVGFile = new Class({
  * The file is **not** loaded right away. It is added to a queue ready to be loaded either when the loader starts,
  * or if it's already running, when the next free load slot becomes available. This happens automatically if you
  * are calling this from within the Scene's `preload` method, or a related callback. Because the file is queued
+ * it means you cannot use the file immediately after calling this method, but must wait for the file to complete.
+ * The typical flow for a Phaser Scene is that you load assets in the Scene's `preload` method and then when the
+ * Scene's `create` method is called you are guaranteed that all of those assets are ready for use and have been
+ * loaded.
+ *
+ * The key must be a unique String. It is used to add the file to the global Texture Manager upon a successful load.
+ * The key should be unique both in terms of files being loaded and files already present in the Texture Manager.
+ * Loading a file using a key that is already taken will result in a warning. If you wish to replace an existing file
+ * then remove it from the Texture Manager first, before loading a new one.
+ *
+ * Instead of passing arguments you can pass a configuration object, such as:
+ *
+ * ```javascript
+ * this.load.svg({
+ *     key: 'morty',
+ *     url: 'images/Morty.svg'
+ * });
+ * ```
+ *
+ * See the documentation for `Phaser.Loader.FileTypes.SVGFileConfig` for more details.
+ *
+ * Once the file has finished loading you can use it as a texture for a Game Object by referencing its key:
+ *
+ * ```javascript
+ * this.load.svg('morty', 'images/Morty.svg');
+ * // and later in your game ...
+ * this.add.image(x, y, 'morty');
+ * ```
+ *
+ * If you have specified a prefix in the loader, via `Loader.setPrefix` then this value will be prepended to this files
+ * key. For example, if the prefix was `MENU.` and the key was `Background` the final key will be `MENU.Background` and
+ * this is what you would use to retrieve the image from the Texture Manager.
+ *
+ * The URL can be relative or absolute. If the URL is relative the `Loader.baseURL` and `Loader.path` values will be prepended to it.
+ *
+ * If the URL isn't specified the Loader will take the key and create a filename from that. For example if the key is "alien"
+ * and no URL is given then the Loader will set the URL to be "alien.html". It will always add `.html` as the extension, although
+ * this can be overridden if using an object instead of method arguments. If you do not desire this action then provide a URL.
+ * 
+ * You can optionally pass an SVG Resize Configuration object when you load an SVG file. By default the SVG will be rendered to a texture
+ * at the same size defined in the SVG file attributes. However, this isn't always desirable. You may wish to resize the SVG (either down
+ * or up) to improve texture clarity, or reduce texture memory consumption. You can either specify an exact width and height to resize
+ * the SVG to:
+ * 
+ * ```javascript
+ * function preload ()
+ * {
+ *     this.load.svg('morty', 'images/Morty.svg', { width: 300, height: 600 });
+ * }
+ * ```
+ * 
+ * Or when using a configuration object:
+ * 
+ * ```javascript
+ * this.load.svg({
+ *     key: 'morty',
+ *     url: 'images/Morty.svg',
+ *     svgConfig: {
+ *         width: 300,
+ *         height: 600
+ *     }
+ * });
+ * ```
+ * 
+ * Alternatively, you can just provide a scale factor instead:
+ * 
+ * ```javascript
+ * function preload ()
+ * {
+ *     this.load.svg('morty', 'images/Morty.svg', { scale: 2.5 });
+ * }
+ * ```
+ * 
+ * Or when using a configuration object:
+ * 
+ * ```javascript
+ * this.load.svg({
+ *     key: 'morty',
+ *     url: 'images/Morty.svg',
+ *     svgConfig: {
+ *         scale: 2.5
+ *     }
+ * });
+ * ```
+ * 
+ * If scale, width and height values are all given, the scale has priority and the width and height values are ignored.
+ *
+ * Note: The ability to load this type of file will only be available if the SVG File type has been built into Phaser.
+ * It is available in the default build but can be excluded from custom builds.
+ *
+ * @method Phaser.Loader.LoaderPlugin#svg
+ * @fires Phaser.Loader.LoaderPlugin#addFileEvent
+ * @since 3.0.0
+ *
+ * @param {(string|Phaser.Loader.FileTypes.SVGFileConfig|Phaser.Loader.FileTypes.SVGFileConfig[])} key - The key to use for this file, or a file configuration object, or array of them.
+ * @param {string} [url] - The absolute or relative URL to load this file from. If undefined or `null` it will be set to `<key>.svg`, i.e. if `key` was "alien" then the URL will be "alien.svg".
+ * @param {Phaser.Loader.FileTypes.SVGSizeConfig} [svgConfig] - The svg size configuration object.
+ * @param {XHRSettingsObject} [xhrSettings] - An XHR Settings configuration object. Used in replacement of the Loaders default XHR Settings.
+ *
+ * @return {Phaser.Loader.LoaderPlugin} The Loader instance.
+ */
+FileTypesManager.register('svg', function (key, url, svgConfig, xhrSettings)
+{
+    if (Array.isArray(key))
+    {
+        for (var i = 0; i < key.length; i++)
+        {
+            //  If it's an array it has to be an array of Objects, so we get everything out of the 'key' object
+            this.addFile(new SVGFile(this, key[i]));
+        }
+    }
+    else
+    {
+        this.addFile(new SVGFile(this, key, url, svgConfig, xhrSettings));
+    }
+
+    return this;
+});
+
+module.exports = SVGFile;
+
+
+
+/***/ }),
+/* 577 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Class = __webpack_require__(0);
+var FileTypesManager = __webpack_require__(7);
+var ImageFile = __webpack_require__(58);
+
+/**
+ * @typedef {object} Phaser.Loader.FileTypes.SpriteSheetFileConfig
+ *
+ * @property {string} key - The key of the file. Must be unique within both the Loader and the Texture Manager.
+ * @property {string} [url] - The absolute or relative URL to load the file from.
+ * @property {string} [extension='png'] - The default file extension to use if no url is provided.
+ * @property {string} [normalMap] - The filename of an associated normal map. It uses the same path and url to load as the image.
+ * @property {Phaser.Loader.FileTypes.ImageFrameConfig} [frameConfig] - The frame configuration object.
+ * @property {XHRSettingsObject} [xhrSettings] - Extra XHR Settings specifically for this file.
+ */
+
+/**
+ * @classdesc
+ * A single Sprite Sheet Image File suitable for loading by the Loader.
+ *
+ * These are created when you use the Phaser.Loader.LoaderPlugin#spritesheet method and are not typically created directly.
+ * 
+ * For documentation about what all the arguments and configuration options mean please see Phaser.Loader.LoaderPlugin#spritesheet.
+ *
+ * @class SpriteSheetFile
+ * @extends Phaser.Loader.File
+ * @memberof Phaser.Loader.FileTypes
+ * @constructor
+ * @since 3.0.0
+ *
+ * @param {Phaser.Loader.LoaderPlugin} loader - A reference to the Loader that is responsible for this file.
+ * @param {(string|Phaser.Loader.FileTypes.SpriteSheetFileConfig)} key - The key to use for this file, or a file configuration object.
+ * @param {string|string[]} [url] - The absolute or relative URL to load this file from. If undefined or `null` it will be set to `<key>.png`, i.e. if `key` was "alien" then the URL will be "alien.png".
+ * @param {Phaser.Loader.FileTypes.ImageFrameConfig} [frameConfig] - The frame configuration object.
+ * @param {XHRSettingsObject} [xhrSettings] - Extra XHR Settings specifically for this file.
+ */
+var SpriteSheetFile = new Class({
+
+    Extends: ImageFile,
+
+    initialize:
+
+    function SpriteSheetFile (loader, key, url, frameConfig, xhrSettings)
+    {
+        ImageFile.call(this, loader, key, url, xhrSettings, frameConfig);
+
+        this.type = 'spritesheet';
+    },
+
+    /**
+     * Adds this file to its target cache upon successful loading and processing.
+     *
+     * @method Phaser.Loader.FileTypes.SpriteSheetFile#addToCache
+     * @since 3.7.0
+     */
+    addToCache: function ()
+    {
+        var texture = this.cache.addSpriteSheet(this.key, this.data, this.config);
+
+        this.pendingDestroy(texture);
+    }
+
+});
+
+/**
+ * Adds a Sprite Sheet Image, or array of Sprite Sheet Images, to the current load queue.
+ *
+ * The term 'Sprite Sheet' in Phaser means a fixed-size sheet. Where every frame in the sheet is the exact same size,
+ * and you reference those frames using numbers, not frame names. This is not the same thing as a Texture Atlas, where
+ * the frames are packed in a way where they take up the least amount of space, and are referenced by their names,
+ * not numbers. Some articles and software use the term 'Sprite Sheet' to mean Texture Atlas, so please be aware of
+ * what sort of file you're actually trying to load.
+ *
+ * You can call this method from within your Scene's `preload`, along with any other files you wish to load:
+ * 
+ * ```javascript
+ * function preload ()
+ * {
+ *     this.load.spritesheet('bot', 'images/robot.png', { frameWidth: 32, frameHeight: 38 });
+ * }
+ * ```
+ *
+ * The file is **not** loaded right away. It is added to a queue ready to be loaded either when the loader starts,
+ * or if it's already running, when the next free load slot becomes available. This happens automatically if you
+ * are calling this from within the Scene's `preload` method, or a related callback. Because the file is queued
+ * it means you cannot use the file immediately after calling this method, but must wait for the file to complete.
+ * The typical flow for a Phaser Scene is that you load assets in the Scene's `preload` method and then when the
+ * Scene's `create` method is called you are guaranteed that all of those assets are ready for use and have been
+ * loaded.
+ * 
+ * Phaser can load all common image types: png, jpg, gif and any other format the browser can natively handle.
+ * If you try to load an animated gif only the first frame will be rendered. Browsers do not natively support playback
+ * of animated gifs to Canvas elements.
+ *
+ * The key must be a unique String. It is used to add the file to the global Texture Manager upon a successful load.
+ * The key should be unique both in terms of files being loaded and files already present in the Texture Manager.
+ * Loading a file using a key that is already taken will result in a warning. If you wish to replace an existing file
+ * then remove it from the Texture Manager first, before loading a new one.
+ *
+ * Instead of passing arguments you can pass a configuration object, such as:
+ * 
+ * ```javascript
+ * this.load.spritesheet({
+ *     key: 'bot',
+ *     url: 'images/robot.png',
+ *     frameConfig: {
+ *         frameWidth: 32,
+ *         frameHeight: 38,
+ *         startFrame: 0,
