203e15faba43184fe9a29c715ef5e5419f034b74
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2022-03-24 15:47:24 +0000

    adding new files

--- js/phaser.js
+++ js/phaser.js
@@ -116562,3 +116562,235 @@ FileTypesManager.register('pack', function (key, url, packKey, xhrSettings)
         {
             this.addFile(new PackFile(this, key[i]));
         }
+    }
+    else
+    {
+        this.addFile(new PackFile(this, key, url, xhrSettings, packKey));
+    }
+
+    return this;
+});
+
+module.exports = PackFile;
+
+
+/***/ }),
+/* 582 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Class = __webpack_require__(0);
+var FileTypesManager = __webpack_require__(7);
+var GetFastValue = __webpack_require__(2);
+var ImageFile = __webpack_require__(58);
+var IsPlainObject = __webpack_require__(8);
+var JSONFile = __webpack_require__(51);
+var MultiFile = __webpack_require__(57);
+
+/**
+ * @typedef {object} Phaser.Loader.FileTypes.MultiAtlasFileConfig
+ *
+ * @property {string} key - The key of the file. Must be unique within both the Loader and the Texture Manager.
+ * @property {string} [atlasURL] - The absolute or relative URL to load the multi atlas json file from. Or, a well formed JSON object.
+ * @property {string} [atlasExtension='json'] - The default file extension to use for the atlas json if no url is provided.
+ * @property {XHRSettingsObject} [atlasXhrSettings] - Extra XHR Settings specifically for the atlas json file.
+ * @property {string} [path] - Optional path to use when loading the textures defined in the atlas data.
+ * @property {string} [baseURL] - Optional Base URL to use when loading the textures defined in the atlas data.
+ * @property {XHRSettingsObject} [textureXhrSettings] - Extra XHR Settings specifically for the texture files.
+ */
+
+/**
+ * @classdesc
+ * A single Multi Texture Atlas File suitable for loading by the Loader.
+ *
+ * These are created when you use the Phaser.Loader.LoaderPlugin#multiatlas method and are not typically created directly.
+ * 
+ * For documentation about what all the arguments and configuration options mean please see Phaser.Loader.LoaderPlugin#multiatlas.
+ *
+ * @class MultiAtlasFile
+ * @extends Phaser.Loader.MultiFile
+ * @memberof Phaser.Loader.FileTypes
+ * @constructor
+ * @since 3.7.0
+ *
+ * @param {Phaser.Loader.LoaderPlugin} loader - A reference to the Loader that is responsible for this file.
+ * @param {string} key - The key of the file. Must be unique within both the Loader and the Texture Manager.
+ * @param {string} [atlasURL] - The absolute or relative URL to load the multi atlas json file from.
+ * @param {string} [path] - Optional path to use when loading the textures defined in the atlas data.
+ * @param {string} [baseURL] - Optional Base URL to use when loading the textures defined in the atlas data.
+ * @param {XHRSettingsObject} [atlasXhrSettings] - Extra XHR Settings specifically for the atlas json file.
+ * @param {XHRSettingsObject} [textureXhrSettings] - Extra XHR Settings specifically for the texture files.
+ */
+var MultiAtlasFile = new Class({
+
+    Extends: MultiFile,
+
+    initialize:
+
+    function MultiAtlasFile (loader, key, atlasURL, path, baseURL, atlasXhrSettings, textureXhrSettings)
+    {
+        if (IsPlainObject(key))
+        {
+            var config = key;
+
+            key = GetFastValue(config, 'key');
+            atlasURL = GetFastValue(config, 'url');
+            atlasXhrSettings = GetFastValue(config, 'xhrSettings');
+            path = GetFastValue(config, 'path');
+            baseURL = GetFastValue(config, 'baseURL');
+            textureXhrSettings = GetFastValue(config, 'textureXhrSettings');
+        }
+
+        var data = new JSONFile(loader, key, atlasURL, atlasXhrSettings);
+
+        MultiFile.call(this, loader, 'multiatlas', key, [ data ]);
+
+        this.config.path = path;
+        this.config.baseURL = baseURL;
+        this.config.textureXhrSettings = textureXhrSettings;
+    },
+
+    /**
+     * Called by each File when it finishes loading.
+     *
+     * @method Phaser.Loader.MultiFile#onFileComplete
+     * @since 3.7.0
+     *
+     * @param {Phaser.Loader.File} file - The File that has completed processing.
+     */
+    onFileComplete: function (file)
+    {
+        var index = this.files.indexOf(file);
+
+        if (index !== -1)
+        {
+            this.pending--;
+
+            if (file.type === 'json' && file.data.hasOwnProperty('textures'))
+            {
+                //  Inspect the data for the files to now load
+                var textures = file.data.textures;
+
+                var config = this.config;
+                var loader = this.loader;
+
+                var currentBaseURL = loader.baseURL;
+                var currentPath = loader.path;
+                var currentPrefix = loader.prefix;
+
+                var baseURL = GetFastValue(config, 'baseURL', currentBaseURL);
+                var path = GetFastValue(config, 'path', currentPath);
+                var prefix = GetFastValue(config, 'prefix', currentPrefix);
+                var textureXhrSettings = GetFastValue(config, 'textureXhrSettings');
+
+                loader.setBaseURL(baseURL);
+                loader.setPath(path);
+                loader.setPrefix(prefix);
+
+                for (var i = 0; i < textures.length; i++)
+                {
+                    //  "image": "texture-packer-multi-atlas-0.png",
+                    var textureURL = textures[i].image;
+
+                    var key = '_MA_' + textureURL;
+
+                    var image = new ImageFile(loader, key, textureURL, textureXhrSettings);
+
+                    this.addToMultiFile(image);
+
+                    loader.addFile(image);
+
+                    //  "normalMap": "texture-packer-multi-atlas-0_n.png",
+                    if (textures[i].normalMap)
+                    {
+                        var normalMap = new ImageFile(loader, key, textures[i].normalMap, textureXhrSettings);
+
+                        normalMap.type = 'normalMap';
+
+                        image.setLink(normalMap);
+
+                        this.addToMultiFile(normalMap);
+
+                        loader.addFile(normalMap);
+                    }
+                }
+
+                //  Reset the loader settings
+                loader.setBaseURL(currentBaseURL);
+                loader.setPath(currentPath);
+                loader.setPrefix(currentPrefix);
+            }
+        }
+    },
+
+    /**
+     * Adds this file to its target cache upon successful loading and processing.
+     *
+     * @method Phaser.Loader.MultiFile#addToCache
+     * @since 3.7.0
+     */
+    addToCache: function ()
+    {
+        if (this.isReadyToProcess())
+        {
+            var fileJSON = this.files[0];
+
+            fileJSON.addToCache();
+
+            var data = [];
+            var images = [];
+            var normalMaps = [];
+
+            for (var i = 1; i < this.files.length; i++)
+            {
+                var file = this.files[i];
+
+                if (file.type === 'normalMap')
+                {
+                    continue;
+                }
+
+                var key = file.key.substr(4);
+                var image = file.data;
+
+                //  Now we need to find out which json entry this mapped to
+                for (var t = 0; t < fileJSON.data.textures.length; t++)
+                {
+                    var item = fileJSON.data.textures[t];
+
+                    if (item.image === key)
+                    {
+                        images.push(image);
+                        
+                        data.push(item);
+
+                        if (file.linkFile)
+                        {
+                            normalMaps.push(file.linkFile.data);
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            if (normalMaps.length === 0)
+            {
+                normalMaps = undefined;
+            }
+
+            this.loader.textureManager.addAtlasJSONArray(this.key, images, data, normalMaps);
+
+            this.complete = true;
+
+            for (i = 0; i < this.files.length; i++)
+            {
+                this.files[i].pendingDestroy();
+            }
+        }
+    }
