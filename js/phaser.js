5a0b1688c6c385f1409d873eb9bf8db9222fca4e
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2022-05-19 11:13:13 +0000

    update feature

--- js/phaser.js
+++ js/phaser.js
@@ -128614,3 +128614,230 @@ GameObjectCreator.register('tileSprite', function (config, addToScene)
 /***/ (function(module, exports, __webpack_require__) {
 
 /**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var BuildGameObject = __webpack_require__(28);
+var GameObjectCreator = __webpack_require__(13);
+var GetAdvancedValue = __webpack_require__(12);
+var Text = __webpack_require__(153);
+
+/**
+ * Creates a new Text Game Object and returns it.
+ *
+ * Note: This method will only be available if the Text Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectCreator#text
+ * @since 3.0.0
+ *
+ * @param {object} config - The configuration object this Game Object will use to create itself.
+ * @param {boolean} [addToScene] - Add this Game Object to the Scene after creating it? If set this argument overrides the `add` property in the config object.
+ *
+ * @return {Phaser.GameObjects.Text} The Game Object that was created.
+ */
+GameObjectCreator.register('text', function (config, addToScene)
+{
+    if (config === undefined) { config = {}; }
+
+    // style Object = {
+    //     font: [ 'font', '16px Courier' ],
+    //     backgroundColor: [ 'backgroundColor', null ],
+    //     fill: [ 'fill', '#fff' ],
+    //     stroke: [ 'stroke', '#fff' ],
+    //     strokeThickness: [ 'strokeThickness', 0 ],
+    //     shadowOffsetX: [ 'shadow.offsetX', 0 ],
+    //     shadowOffsetY: [ 'shadow.offsetY', 0 ],
+    //     shadowColor: [ 'shadow.color', '#000' ],
+    //     shadowBlur: [ 'shadow.blur', 0 ],
+    //     shadowStroke: [ 'shadow.stroke', false ],
+    //     shadowFill: [ 'shadow.fill', false ],
+    //     align: [ 'align', 'left' ],
+    //     maxLines: [ 'maxLines', 0 ],
+    //     fixedWidth: [ 'fixedWidth', false ],
+    //     fixedHeight: [ 'fixedHeight', false ],
+    //     rtl: [ 'rtl', false ]
+    // }
+
+    var content = GetAdvancedValue(config, 'text', '');
+    var style = GetAdvancedValue(config, 'style', null);
+
+    //  Padding
+    //      { padding: 2 }
+    //      { padding: { x: , y: }}
+    //      { padding: { left: , top: }}
+    //      { padding: { left: , right: , top: , bottom: }}
+
+    var padding = GetAdvancedValue(config, 'padding', null);
+
+    if (padding !== null)
+    {
+        style.padding = padding;
+    }
+
+    var text = new Text(this.scene, 0, 0, content, style);
+
+    if (addToScene !== undefined)
+    {
+        config.add = addToScene;
+    }
+
+    BuildGameObject(this.scene, text, config);
+
+    //  Text specific config options:
+
+    text.autoRound = GetAdvancedValue(config, 'autoRound', true);
+    text.resolution = GetAdvancedValue(config, 'resolution', 1);
+
+    return text;
+});
+
+//  When registering a factory function 'this' refers to the GameObjectCreator context.
+
+
+/***/ }),
+/* 735 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var BitmapText = __webpack_require__(109);
+var BuildGameObject = __webpack_require__(28);
+var GameObjectCreator = __webpack_require__(13);
+var GetAdvancedValue = __webpack_require__(12);
+var GetValue = __webpack_require__(4);
+
+/**
+ * Creates a new Bitmap Text Game Object and returns it.
+ *
+ * Note: This method will only be available if the Bitmap Text Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectCreator#bitmapText
+ * @since 3.0.0
+ *
+ * @param {BitmapTextConfig} config - The configuration object this Game Object will use to create itself.
+ * @param {boolean} [addToScene] - Add this Game Object to the Scene after creating it? If set this argument overrides the `add` property in the config object.
+ * 
+ * @return {Phaser.GameObjects.BitmapText} The Game Object that was created.
+ */
+GameObjectCreator.register('bitmapText', function (config, addToScene)
+{
+    if (config === undefined) { config = {}; }
+
+    var font = GetValue(config, 'font', '');
+    var text = GetAdvancedValue(config, 'text', '');
+    var size = GetAdvancedValue(config, 'size', false);
+    var align = GetValue(config, 'align', 0);
+
+    var bitmapText = new BitmapText(this.scene, 0, 0, font, text, size, align);
+
+    if (addToScene !== undefined)
+    {
+        config.add = addToScene;
+    }
+
+    BuildGameObject(this.scene, bitmapText, config);
+
+    return bitmapText;
+});
+
+//  When registering a factory function 'this' refers to the GameObjectCreator context.
+
+
+/***/ }),
+/* 736 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var BuildGameObject = __webpack_require__(28);
+var BuildGameObjectAnimation = __webpack_require__(311);
+var GameObjectCreator = __webpack_require__(13);
+var GetAdvancedValue = __webpack_require__(12);
+var Sprite = __webpack_require__(61);
+
+/**
+ * @typedef {object} SpriteConfig
+ * @extends GameObjectConfig
+ *
+ * @property {string} [key] - The key of the Texture this Game Object will use to render with, as stored in the Texture Manager.
+ * @property {(number|string)} [frame] - An optional frame from the Texture this Game Object is rendering with.
+ */
+
+/**
+ * Creates a new Sprite Game Object and returns it.
+ *
+ * Note: This method will only be available if the Sprite Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectCreator#sprite
+ * @since 3.0.0
+ *
+ * @param {SpriteConfig} config - The configuration object this Game Object will use to create itself.
+ * @param {boolean} [addToScene] - Add this Game Object to the Scene after creating it? If set this argument overrides the `add` property in the config object.
+ *
+ * @return {Phaser.GameObjects.Sprite} The Game Object that was created.
+ */
+GameObjectCreator.register('sprite', function (config, addToScene)
+{
+    if (config === undefined) { config = {}; }
+
+    var key = GetAdvancedValue(config, 'key', null);
+    var frame = GetAdvancedValue(config, 'frame', null);
+
+    var sprite = new Sprite(this.scene, 0, 0, key, frame);
+
+    if (addToScene !== undefined)
+    {
+        config.add = addToScene;
+    }
+
+    BuildGameObject(this.scene, sprite, config);
+
+    //  Sprite specific config options:
+
+    BuildGameObjectAnimation(sprite, config);
+
+    return sprite;
+});
+
+
+/***/ }),
+/* 737 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var BuildGameObject = __webpack_require__(28);
+var GameObjectCreator = __webpack_require__(13);
+var GetAdvancedValue = __webpack_require__(12);
+var RenderTexture = __webpack_require__(154);
+
+/**
+ * @typedef {object} RenderTextureConfig
+ *
+ * @property {number} [x=0] - The x coordinate of the RenderTexture's position.
+ * @property {number} [y=0] - The y coordinate of the RenderTexture's position.
+ * @property {number} [width=32] - The width of the RenderTexture.
+ * @property {number} [height=32] - The height of the RenderTexture.
+ */
+
+/**
+ * Creates a new Render Texture Game Object and returns it.
+ *
+ * Note: This method will only be available if the Render Texture Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectCreator#renderTexture
+ * @since 3.2.0
