9de6d0436ccb839889d1c114f633c6e0b816a046
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2022-05-24 14:12:06 +0000

    Update intro.md

--- js/phaser.js
+++ js/phaser.js
@@ -130194,3 +130194,209 @@ GameObjectFactory.register('particles', function (key, frame, emitters)
 //  this.scene - a reference to the Scene that owns the GameObjectFactory
 //  this.displayList - a reference to the Display List the Scene owns
 //  this.updateList - a reference to the Update List the Scene owns
+
+
+/***/ }),
+/* 764 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Image = __webpack_require__(87);
+var GameObjectFactory = __webpack_require__(5);
+
+/**
+ * Creates a new Image Game Object and adds it to the Scene.
+ *
+ * Note: This method will only be available if the Image Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectFactory#image
+ * @since 3.0.0
+ *
+ * @param {number} x - The horizontal position of this Game Object in the world.
+ * @param {number} y - The vertical position of this Game Object in the world.
+ * @param {string} texture - The key of the Texture this Game Object will use to render with, as stored in the Texture Manager.
+ * @param {(string|integer)} [frame] - An optional frame from the Texture this Game Object is rendering with.
+ *
+ * @return {Phaser.GameObjects.Image} The Game Object that was created.
+ */
+GameObjectFactory.register('image', function (x, y, key, frame)
+{
+    return this.displayList.add(new Image(this.scene, x, y, key, frame));
+});
+
+//  When registering a factory function 'this' refers to the GameObjectFactory context.
+//
+//  There are several properties available to use:
+//
+//  this.scene - a reference to the Scene that owns the GameObjectFactory
+//  this.displayList - a reference to the Display List the Scene owns
+//  this.updateList - a reference to the Update List the Scene owns
+
+
+/***/ }),
+/* 765 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Group = __webpack_require__(88);
+var GameObjectFactory = __webpack_require__(5);
+
+/**
+ * Creates a new Group Game Object and adds it to the Scene.
+ *
+ * Note: This method will only be available if the Group Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectFactory#group
+ * @since 3.0.0
+ *
+ * @param {(Phaser.GameObjects.GameObject[]|GroupConfig|GroupConfig[])} [children] - Game Objects to add to this Group; or the `config` argument.
+ * @param {GroupConfig} [config] - A Group Configuration object.
+ *
+ * @return {Phaser.GameObjects.Group} The Game Object that was created.
+ */
+GameObjectFactory.register('group', function (children, config)
+{
+    return this.updateList.add(new Group(this.scene, children, config));
+});
+
+
+/***/ }),
+/* 766 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Graphics = __webpack_require__(158);
+var GameObjectFactory = __webpack_require__(5);
+
+/**
+ * Creates a new Graphics Game Object and adds it to the Scene.
+ *
+ * Note: This method will only be available if the Graphics Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectFactory#graphics
+ * @since 3.0.0
+ *
+ * @param {GraphicsOptions} [config] - The Graphics configuration.
+ *
+ * @return {Phaser.GameObjects.Graphics} The Game Object that was created.
+ */
+GameObjectFactory.register('graphics', function (config)
+{
+    return this.displayList.add(new Graphics(this.scene, config));
+});
+
+//  When registering a factory function 'this' refers to the GameObjectFactory context.
+//
+//  There are several properties available to use:
+//
+//  this.scene - a reference to the Scene that owns the GameObjectFactory
+//  this.displayList - a reference to the Display List the Scene owns
+//  this.updateList - a reference to the Update List the Scene owns
+
+
+/***/ }),
+/* 767 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var DynamicBitmapText = __webpack_require__(159);
+var GameObjectFactory = __webpack_require__(5);
+
+/**
+ * Creates a new Dynamic Bitmap Text Game Object and adds it to the Scene.
+ * 
+ * BitmapText objects work by taking a texture file and an XML or JSON file that describes the font structure.
+ * 
+ * During rendering for each letter of the text is rendered to the display, proportionally spaced out and aligned to
+ * match the font structure.
+ * 
+ * Dynamic Bitmap Text objects are different from Static Bitmap Text in that they invoke a callback for each
+ * letter being rendered during the render pass. This callback allows you to manipulate the properties of
+ * each letter being rendered, such as its position, scale or tint, allowing you to create interesting effects
+ * like jiggling text, which can't be done with Static text. This means that Dynamic Text takes more processing
+ * time, so only use them if you require the callback ability they have.
+ *
+ * BitmapText objects are less flexible than Text objects, in that they have less features such as shadows, fills and the ability
+ * to use Web Fonts, however you trade this flexibility for rendering speed. You can also create visually compelling BitmapTexts by
+ * processing the font texture in an image editor, applying fills and any other effects required.
+ *
+ * To create multi-line text insert \r, \n or \r\n escape codes into the text string.
+ *
+ * To create a BitmapText data files you need a 3rd party app such as:
+ *
+ * BMFont (Windows, free): http://www.angelcode.com/products/bmfont/
+ * Glyph Designer (OS X, commercial): http://www.71squared.com/en/glyphdesigner
+ * Littera (Web-based, free): http://kvazars.com/littera/
+ *
+ * For most use cases it is recommended to use XML. If you wish to use JSON, the formatting should be equal to the result of
+ * converting a valid XML file through the popular X2JS library. An online tool for conversion can be found here: http://codebeautify.org/xmltojson
+ *
+ * Note: This method will only be available if the Dynamic Bitmap Text Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectFactory#dynamicBitmapText
+ * @since 3.0.0
+ *
+ * @param {number} x - The x position of the Game Object.
+ * @param {number} y - The y position of the Game Object.
+ * @param {string} font - The key of the font to use from the BitmapFont cache.
+ * @param {(string|string[])} [text] - The string, or array of strings, to be set as the content of this Bitmap Text.
+ * @param {number} [size] - The font size to set.
+ *
+ * @return {Phaser.GameObjects.DynamicBitmapText} The Game Object that was created.
+ */
+GameObjectFactory.register('dynamicBitmapText', function (x, y, font, text, size)
+{
+    return this.displayList.add(new DynamicBitmapText(this.scene, x, y, font, text, size));
+});
+
+//  When registering a factory function 'this' refers to the GameObjectFactory context.
+//
+//  There are several properties available to use:
+//
+//  this.scene - a reference to the Scene that owns the GameObjectFactory
+//  this.displayList - a reference to the Display List the Scene owns
+//  this.updateList - a reference to the Update List the Scene owns
+
+
+/***/ }),
+/* 768 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @author       Felipe Alfonso <@bitnenfer>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Container = __webpack_require__(160);
+var GameObjectFactory = __webpack_require__(5);
+
+/**
+ * Creates a new Container Game Object and adds it to the Scene.
+ *
+ * Note: This method will only be available if the Container Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectFactory#container
+ * @since 3.4.0
+ *
