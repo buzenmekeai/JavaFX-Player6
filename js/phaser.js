554742853bf3de4cdf753460176ba061bc77655c
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2022-05-30 09:02:32 +0000

    Updates gh-pages to mirror master.

--- js/phaser.js
+++ js/phaser.js
@@ -131291,3 +131291,207 @@ module.exports = LineCanvasRenderer;
 /***/ }),
 /* 783 */
 /***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Utils = __webpack_require__(10);
+
+/**
+ * Renders this Game Object with the WebGL Renderer to the given Camera.
+ * The object will not render if any of its renderFlags are set or it is being actively filtered out by the Camera.
+ * This method should not be called directly. It is a utility function of the Render module.
+ *
+ * @method Phaser.GameObjects.Line#renderWebGL
+ * @since 3.13.0
+ * @private
+ *
+ * @param {Phaser.Renderer.WebGL.WebGLRenderer} renderer - A reference to the current active WebGL renderer.
+ * @param {Phaser.GameObjects.Line} src - The Game Object being rendered in this call.
+ * @param {number} interpolationPercentage - Reserved for future use and custom pipelines.
+ * @param {Phaser.Cameras.Scene2D.Camera} camera - The Camera that is rendering the Game Object.
+ * @param {Phaser.GameObjects.Components.TransformMatrix} parentMatrix - This transform matrix is defined if the game object is nested
+ */
+var LineWebGLRenderer = function (renderer, src, interpolationPercentage, camera, parentMatrix)
+{
+    var pipeline = this.pipeline;
+
+    var camMatrix = pipeline._tempMatrix1;
+    var shapeMatrix = pipeline._tempMatrix2;
+
+    renderer.setPipeline(pipeline);
+
+    shapeMatrix.applyITRS(src.x, src.y, src.rotation, src.scaleX, src.scaleY);
+
+    camMatrix.copyFrom(camera.matrix);
+
+    if (parentMatrix)
+    {
+        //  Multiply the camera by the parent matrix
+        camMatrix.multiplyWithOffset(parentMatrix, -camera.scrollX * src.scrollFactorX, -camera.scrollY * src.scrollFactorY);
+
+        //  Undo the camera scroll
+        shapeMatrix.e = src.x;
+        shapeMatrix.f = src.y;
+    }
+    else
+    {
+        shapeMatrix.e -= camera.scrollX * src.scrollFactorX;
+        shapeMatrix.f -= camera.scrollY * src.scrollFactorY;
+    }
+
+    var dx = src._displayOriginX;
+    var dy = src._displayOriginY;
+    var alpha = camera.alpha * src.alpha;
+
+    if (src.isStroked)
+    {
+        var strokeTint = pipeline.strokeTint;
+        var color = Utils.getTintAppendFloatAlphaAndSwap(src.strokeColor, src.strokeAlpha * alpha);
+
+        strokeTint.TL = color;
+        strokeTint.TR = color;
+        strokeTint.BL = color;
+        strokeTint.BR = color;
+
+        var startWidth = src._startWidth;
+        var endWidth = src._endWidth;
+
+        pipeline.setTexture2D();
+
+        pipeline.batchLine(
+            src.geom.x1 - dx,
+            src.geom.y1 - dy,
+            src.geom.x2 - dx,
+            src.geom.y2 - dy,
+            startWidth,
+            endWidth,
+            1,
+            0,
+            false,
+            shapeMatrix,
+            camMatrix
+        );
+    }
+};
+
+module.exports = LineWebGLRenderer;
+
+
+/***/ }),
+/* 784 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var renderWebGL = __webpack_require__(1);
+var renderCanvas = __webpack_require__(1);
+
+if (true)
+{
+    renderWebGL = __webpack_require__(783);
+}
+
+if (true)
+{
+    renderCanvas = __webpack_require__(782);
+}
+
+module.exports = {
+
+    renderWebGL: renderWebGL,
+    renderCanvas: renderCanvas
+
+};
+
+
+/***/ }),
+/* 785 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var FillStyleCanvas = __webpack_require__(30);
+var SetTransform = __webpack_require__(22);
+
+/**
+ * Renders this Game Object with the Canvas Renderer to the given Camera.
+ * The object will not render if any of its renderFlags are set or it is being actively filtered out by the Camera.
+ * This method should not be called directly. It is a utility function of the Render module.
+ *
+ * @method Phaser.GameObjects.IsoTriangle#renderCanvas
+ * @since 3.13.0
+ * @private
+ *
+ * @param {Phaser.Renderer.Canvas.CanvasRenderer} renderer - A reference to the current active Canvas renderer.
+ * @param {Phaser.GameObjects.IsoTriangle} src - The Game Object being rendered in this call.
+ * @param {number} interpolationPercentage - Reserved for future use and custom pipelines.
+ * @param {Phaser.Cameras.Scene2D.Camera} camera - The Camera that is rendering the Game Object.
+ * @param {Phaser.GameObjects.Components.TransformMatrix} parentMatrix - This transform matrix is defined if the game object is nested
+ */
+var IsoTriangleCanvasRenderer = function (renderer, src, interpolationPercentage, camera, parentMatrix)
+{
+    var ctx = renderer.currentContext;
+
+    if (SetTransform(renderer, ctx, src, camera, parentMatrix) && src.isFilled)
+    {
+        var size = src.width;
+        var height = src.height;
+    
+        var sizeA = size / 2;
+        var sizeB = size / src.projection;
+
+        var reversed = src.isReversed;
+
+        //  Top Face
+
+        if (src.showTop && reversed)
+        {
+            FillStyleCanvas(ctx, src, src.fillTop);
+
+            ctx.beginPath();
+
+            ctx.moveTo(-sizeA, -height);
+            ctx.lineTo(0, -sizeB - height);
+            ctx.lineTo(sizeA, -height);
+            ctx.lineTo(0, sizeB - height);
+
+            ctx.fill();
+        }
+
+        //  Left Face
+
+        if (src.showLeft)
+        {
+            FillStyleCanvas(ctx, src, src.fillLeft);
+
+            ctx.beginPath();
+
+            if (reversed)
+            {
+                ctx.moveTo(-sizeA, -height);
+                ctx.lineTo(0, sizeB);
+                ctx.lineTo(0, sizeB - height);
+            }
+            else
+            {
+                ctx.moveTo(-sizeA, 0);
+                ctx.lineTo(0, sizeB);
+                ctx.lineTo(0, sizeB - height);
+            }
+
+            ctx.fill();
+        }
+
+        //  Right Face
