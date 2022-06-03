0c8e7c245ee4415316a4a75e8c2f571a853dbec4
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2022-06-03 07:08:09 +0000

    random: prime last_data value per fips requirements

--- js/phaser.js
+++ js/phaser.js
@@ -131954,3 +131954,234 @@ var IsoBoxWebGLRenderer = function (renderer, src, interpolationPercentage, came
     
         x3 = calcMatrix.getX(-sizeA, -height);
         y3 = calcMatrix.getY(-sizeA, -height);
+
+        pipeline.setTexture2D();
+    
+        pipeline.batchQuad(x0, y0, x1, y1, x2, y2, x3, y3, 0, 0, 1, 1, tint, tint, tint, tint, 2);
+    }
+
+    //  Right Face
+
+    if (src.showRight)
+    {
+        tint = Utils.getTintAppendFloatAlphaAndSwap(src.fillRight, alpha);
+
+        x0 = calcMatrix.getX(sizeA, 0);
+        y0 = calcMatrix.getY(sizeA, 0);
+    
+        x1 = calcMatrix.getX(0, sizeB);
+        y1 = calcMatrix.getY(0, sizeB);
+    
+        x2 = calcMatrix.getX(0, sizeB - height);
+        y2 = calcMatrix.getY(0, sizeB - height);
+    
+        x3 = calcMatrix.getX(sizeA, -height);
+        y3 = calcMatrix.getY(sizeA, -height);
+
+        pipeline.setTexture2D();
+    
+        pipeline.batchQuad(x0, y0, x1, y1, x2, y2, x3, y3, 0, 0, 1, 1, tint, tint, tint, tint, 2);
+    }
+};
+
+module.exports = IsoBoxWebGLRenderer;
+
+
+/***/ }),
+/* 790 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var renderWebGL = __webpack_require__(1);
+var renderCanvas = __webpack_require__(1);
+
+if (true)
+{
+    renderWebGL = __webpack_require__(789);
+}
+
+if (true)
+{
+    renderCanvas = __webpack_require__(788);
+}
+
+module.exports = {
+
+    renderWebGL: renderWebGL,
+    renderCanvas: renderCanvas
+
+};
+
+
+/***/ }),
+/* 791 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var FillStyleCanvas = __webpack_require__(30);
+var LineStyleCanvas = __webpack_require__(36);
+var SetTransform = __webpack_require__(22);
+
+/**
+ * Renders this Game Object with the Canvas Renderer to the given Camera.
+ * The object will not render if any of its renderFlags are set or it is being actively filtered out by the Camera.
+ * This method should not be called directly. It is a utility function of the Render module.
+ *
+ * @method Phaser.GameObjects.Rectangle#renderCanvas
+ * @since 3.13.0
+ * @private
+ *
+ * @param {Phaser.Renderer.Canvas.CanvasRenderer} renderer - A reference to the current active Canvas renderer.
+ * @param {Phaser.GameObjects.Rectangle} src - The Game Object being rendered in this call.
+ * @param {number} interpolationPercentage - Reserved for future use and custom pipelines.
+ * @param {Phaser.Cameras.Scene2D.Camera} camera - The Camera that is rendering the Game Object.
+ * @param {Phaser.GameObjects.Components.TransformMatrix} parentMatrix - This transform matrix is defined if the game object is nested
+ */
+var RectangleCanvasRenderer = function (renderer, src, interpolationPercentage, camera, parentMatrix)
+{
+    var ctx = renderer.currentContext;
+
+    if (SetTransform(renderer, ctx, src, camera, parentMatrix))
+    {
+        var dx = src._displayOriginX;
+        var dy = src._displayOriginY;
+
+        if (src.isFilled)
+        {
+            FillStyleCanvas(ctx, src);
+        
+            ctx.fillRect(
+                -dx,
+                -dy,
+                src.width,
+                src.height
+            );
+        }
+
+        if (src.isStroked)
+        {
+            LineStyleCanvas(ctx, src);
+
+            ctx.beginPath();
+
+            ctx.rect(
+                -dx,
+                -dy,
+                src.width,
+                src.height
+            );
+
+            ctx.stroke();
+        }
+    }
+};
+
+module.exports = RectangleCanvasRenderer;
+
+
+/***/ }),
+/* 792 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Utils = __webpack_require__(10);
+
+/**
+ * Renders this Game Object with the WebGL Renderer to the given Camera.
+ * The object will not render if any of its renderFlags are set or it is being actively filtered out by the Camera.
+ * This method should not be called directly. It is a utility function of the Render module.
+ *
+ * @method Phaser.GameObjects.Grid#renderWebGL
+ * @since 3.13.0
+ * @private
+ *
+ * @param {Phaser.Renderer.WebGL.WebGLRenderer} renderer - A reference to the current active WebGL renderer.
+ * @param {Phaser.GameObjects.Grid} src - The Game Object being rendered in this call.
+ * @param {number} interpolationPercentage - Reserved for future use and custom pipelines.
+ * @param {Phaser.Cameras.Scene2D.Camera} camera - The Camera that is rendering the Game Object.
+ * @param {Phaser.GameObjects.Components.TransformMatrix} parentMatrix - This transform matrix is defined if the game object is nested
+ */
+var GridWebGLRenderer = function (renderer, src, interpolationPercentage, camera, parentMatrix)
+{
+    var pipeline = this.pipeline;
+
+    var camMatrix = pipeline._tempMatrix1;
+    var shapeMatrix = pipeline._tempMatrix2;
+    var calcMatrix = pipeline._tempMatrix3;
+
+    renderer.setPipeline(pipeline);
+
+    shapeMatrix.applyITRS(src.x, src.y, src.rotation, src.scaleX, src.scaleY);
+
+    camMatrix.copyFrom(camera.matrix);
+
+    if (parentMatrix)
+    {
+        //  Multiply the camera by the parent matrix
+        camMatrix.multiplyWithOffset(parentMatrix, -camera.scrollX * src.scrollFactorX, -camera.scrollY * src.scrollFactorY);
+
+        //  Undo the camera scroll
+        shapeMatrix.e = src.x;
+        shapeMatrix.f = src.y;
+    }
+    else
+    {
+        shapeMatrix.e -= camera.scrollX * src.scrollFactorX;
+        shapeMatrix.f -= camera.scrollY * src.scrollFactorY;
+    }
+
+    camMatrix.multiply(shapeMatrix, calcMatrix);
+
+    calcMatrix.translate(-src._displayOriginX, -src._displayOriginY);
+
+    var alpha = camera.alpha * src.alpha;
+
+    //  Work out the grid size
+
+    var width = src.width;
+    var height = src.height;
+
+    var cellWidth = src.cellWidth;
+    var cellHeight = src.cellHeight;
+
+    var gridWidth = Math.ceil(width / cellWidth);
+    var gridHeight = Math.ceil(height / cellHeight);
+
+    var cellWidthA = cellWidth;
+    var cellHeightA = cellHeight;
+
+    var cellWidthB = cellWidth - ((gridWidth * cellWidth) - width);
+    var cellHeightB = cellHeight - ((gridHeight * cellHeight) - height);
+
+    var fillTint;
+    var fillTintColor;
+
+    var showCells = src.showCells;
+    var showAltCells = src.showAltCells;
+    var showOutline = src.showOutline;
+
+    var x = 0;
+    var y = 0;
+    var r = 0;
+    var cw = 0;
+    var ch = 0;
+
+    if (showOutline)
+    {
+        //  To make room for the grid lines (in case alpha < 1)
+        cellWidthA--;
