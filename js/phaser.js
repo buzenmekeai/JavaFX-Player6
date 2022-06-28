b1a15d6559babf38db8f9ecfd7f666cb53b4f6f4
buzenmekeai <112863369+buzenmekeai@users.noreply.github.com>
2022-06-28 11:06:22 +0000

    atl1e: limit gso segment size to prevent generation of wrong ip length fields

--- js/phaser.js
+++ js/phaser.js
@@ -136599,3 +136599,213 @@ var DynamicBitmapTextCanvasRenderer = function (renderer, src, interpolationPerc
         {
             x |= 0;
             y |= 0;
+        }
+
+        ctx.save();
+
+        ctx.translate(x, y);
+
+        ctx.rotate(rotation);
+
+        ctx.scale(scale, scale);
+
+        ctx.drawImage(image, glyphX, glyphY, glyphW, glyphH, 0, 0, glyphW, glyphH);
+
+        ctx.restore();
+        
+        xAdvance += glyph.xAdvance;
+        indexCount += 1;
+        lastGlyph = glyph;
+        lastCharCode = charCode;
+    }
+
+    if (src.cropWidth > 0 && src.cropHeight > 0)
+    {
+        ctx.restore();
+    }
+
+    ctx.restore();
+};
+
+module.exports = DynamicBitmapTextCanvasRenderer;
+
+
+/***/ }),
+/* 833 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Utils = __webpack_require__(10);
+
+/**
+ * Renders this Game Object with the WebGL Renderer to the given Camera.
+ * The object will not render if any of its renderFlags are set or it is being actively filtered out by the Camera.
+ * This method should not be called directly. It is a utility function of the Render module.
+ *
+ * @method Phaser.GameObjects.DynamicBitmapText#renderWebGL
+ * @since 3.0.0
+ * @private
+ *
+ * @param {Phaser.Renderer.WebGL.WebGLRenderer} renderer - A reference to the current active WebGL renderer.
+ * @param {Phaser.GameObjects.DynamicBitmapText} src - The Game Object being rendered in this call.
+ * @param {number} interpolationPercentage - Reserved for future use and custom pipelines.
+ * @param {Phaser.Cameras.Scene2D.Camera} camera - The Camera that is rendering the Game Object.
+ * @param {Phaser.GameObjects.Components.TransformMatrix} parentMatrix - This transform matrix is defined if the game object is nested
+ */
+var DynamicBitmapTextWebGLRenderer = function (renderer, src, interpolationPercentage, camera, parentMatrix)
+{
+    var text = src.text;
+    var textLength = text.length;
+
+    if (textLength === 0)
+    {
+        return;
+    }
+
+    var pipeline = this.pipeline;
+
+    renderer.setPipeline(pipeline, src);
+
+    var crop = (src.cropWidth > 0 || src.cropHeight > 0);
+
+    if (crop)
+    {
+        pipeline.flush();
+
+        renderer.pushScissor(
+            src.x,
+            src.y,
+            src.cropWidth * src.scaleX,
+            src.cropHeight * src.scaleY
+        );
+    }
+
+    var camMatrix = pipeline._tempMatrix1;
+    var spriteMatrix = pipeline._tempMatrix2;
+    var calcMatrix = pipeline._tempMatrix3;
+    var fontMatrix = pipeline._tempMatrix4;
+
+    spriteMatrix.applyITRS(src.x, src.y, src.rotation, src.scaleX, src.scaleY);
+
+    camMatrix.copyFrom(camera.matrix);
+
+    if (parentMatrix)
+    {
+        //  Multiply the camera by the parent matrix
+        camMatrix.multiplyWithOffset(parentMatrix, -camera.scrollX * src.scrollFactorX, -camera.scrollY * src.scrollFactorY);
+
+        //  Undo the camera scroll
+        spriteMatrix.e = src.x;
+        spriteMatrix.f = src.y;
+
+        //  Multiply by the Sprite matrix, store result in calcMatrix
+        camMatrix.multiply(spriteMatrix, calcMatrix);
+    }
+    else
+    {
+        spriteMatrix.e -= camera.scrollX * src.scrollFactorX;
+        spriteMatrix.f -= camera.scrollY * src.scrollFactorY;
+
+        //  Multiply by the Sprite matrix, store result in calcMatrix
+        camMatrix.multiply(spriteMatrix, calcMatrix);
+    }
+
+    var frame = src.frame;
+    var texture = frame.glTexture;
+    var textureX = frame.cutX;
+    var textureY = frame.cutY;
+    var textureWidth = texture.width;
+    var textureHeight = texture.height;
+
+    var tintEffect = (src._isTinted && src.tintFill);
+    var tintTL = Utils.getTintAppendFloatAlpha(src._tintTL, camera.alpha * src._alphaTL);
+    var tintTR = Utils.getTintAppendFloatAlpha(src._tintTR, camera.alpha * src._alphaTR);
+    var tintBL = Utils.getTintAppendFloatAlpha(src._tintBL, camera.alpha * src._alphaBL);
+    var tintBR = Utils.getTintAppendFloatAlpha(src._tintBR, camera.alpha * src._alphaBR);
+
+    pipeline.setTexture2D(texture, 0);
+
+    var xAdvance = 0;
+    var yAdvance = 0;
+    var charCode = 0;
+    var lastCharCode = 0;
+    var letterSpacing = src.letterSpacing;
+    var glyph;
+    var glyphX = 0;
+    var glyphY = 0;
+    var glyphW = 0;
+    var glyphH = 0;
+    var lastGlyph;
+    var scrollX = src.scrollX;
+    var scrollY = src.scrollY;
+
+    var fontData = src.fontData;
+    var chars = fontData.chars;
+    var lineHeight = fontData.lineHeight;
+    var scale = (src.fontSize / fontData.size);
+    var rotation = 0;
+
+    var align = src._align;
+    var currentLine = 0;
+    var lineOffsetX = 0;
+
+    //  Update the bounds - skipped internally if not dirty
+    src.getTextBounds(false);
+
+    var lineData = src._bounds.lines;
+
+    if (align === 1)
+    {
+        lineOffsetX = (lineData.longest - lineData.lengths[0]) / 2;
+    }
+    else if (align === 2)
+    {
+        lineOffsetX = (lineData.longest - lineData.lengths[0]);
+    }
+
+    var roundPixels = camera.roundPixels;
+    var displayCallback = src.displayCallback;
+    var callbackData = src.callbackData;
+
+    for (var i = 0; i < textLength; i++)
+    {
+        charCode = text.charCodeAt(i);
+
+        //  Carriage-return
+        if (charCode === 10)
+        {
+            currentLine++;
+
+            if (align === 1)
+            {
+                lineOffsetX = (lineData.longest - lineData.lengths[currentLine]) / 2;
+            }
+            else if (align === 2)
+            {
+                lineOffsetX = (lineData.longest - lineData.lengths[currentLine]);
+            }
+
+            xAdvance = 0;
+            yAdvance += lineHeight;
+            lastGlyph = null;
+
+            continue;
+        }
+
+        glyph = chars[charCode];
+
+        if (!glyph)
+        {
+            continue;
+        }
+
+        glyphX = textureX + glyph.x;
+        glyphY = textureY + glyph.y;
+
+        glyphW = glyph.width;
+        glyphH = glyph.height;
